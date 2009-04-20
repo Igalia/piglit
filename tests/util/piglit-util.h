51fb996eea4069a6706c2ce6bb00477bff17bd45
Brian Paul <brianp@vmware.com>
2009-04-20 07:23:23 -0600

    function pointer changes for MacOS

--- tests/util/piglit-util.h
+++ tests/util/piglit-util.h
@@ -35,6 +35,17 @@ void piglit_require_extension(const char *name);
 int piglit_probe_pixel_rgb(int x, int y, const float* expected);
 int piglit_probe_pixel_rgba(int x, int y, const float* expected);
 
+#if defined(__APPLE__)
+extern void (*pglGenProgramsARB)(GLsizei n, GLuint *programs);
+extern void (*pglProgramStringARB)(GLenum target, GLenum format, GLsizei len, const GLvoid *string);
+extern void (*pglBindProgramARB)(GLenum target, GLuint program);
+extern GLboolean (*pglIsProgramARB)(GLuint program);
+extern void (*pglDeleteProgramsARB)(GLsizei n, const GLuint *programs);
+extern void (*pglProgramLocalParameter4fvARB)(GLenum target, GLuint index, const GLfloat *params);
+extern void (*pglProgramLocalParameter4dARB)(GLenum target, GLuint index, GLdouble x, GLdouble y, GLdouble z, GLdouble w);
+extern void (*pglGetProgramivARB)(GLenum target, GLenum pname, GLint *params);
+extern void (*pglGetProgramLocalParameterdvARB)(GLenum target, GLuint index, GLdouble *params);
+#else
 extern PFNGLGENPROGRAMSARBPROC pglGenProgramsARB;
 extern PFNGLPROGRAMSTRINGARBPROC pglProgramStringARB;
 extern PFNGLBINDPROGRAMARBPROC pglBindProgramARB;
@@ -44,6 +55,7 @@ extern PFNGLPROGRAMLOCALPARAMETER4FVARBPROC pglProgramLocalParameter4fvARB;
 extern PFNGLPROGRAMLOCALPARAMETER4DARBPROC pglProgramLocalParameter4dARB;
 extern PFNGLGETPROGRAMLOCALPARAMETERDVARBPROC pglGetProgramLocalParameterdvARB;
 extern PFNGLGETPROGRAMIVARBPROC pglGetProgramivARB;
+#endif
 
 int piglit_use_fragment_program();
 int piglit_use_vertex_program();
