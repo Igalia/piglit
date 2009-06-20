0ea704aedd86986a4af66ca7e556ffc84eb634b5
Eric Anholt <eric@anholt.net>
2009-06-19 22:13:30 -0700

    read-front: New test to catch a regression with DRI2 front buffer reading.

---
+++ tests/general/read-front.c
@@ -0,0 +1,130 @@
+/*
+ * Copyright © 2009 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *
+ */
+
+/** @file read-front.c
+ *
+ * Tests that reading the front buffer after a draw to back and swap
+ * works correctly.
+ *
+ * This catches a regression in the Intel driver with DRI2, where the
+ * read buffer didn't have an actual buffer present if it hadn't been
+ * used as a draw buffer.
+ */
+
+#include "GL/glut.h"
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include "piglit-util.h"
+
+#define WIN_WIDTH 100
+#define WIN_HEIGHT 100
+
+static GLboolean Automatic = GL_FALSE;
+static int win_width, win_height;
+
+static void display()
+{
+	GLboolean pass = GL_TRUE;
+	int x, y;
+	static float red[]   = {1.0, 0.0, 0.0, 0.0};
+	static float green[] = {0.0, 1.0, 0.0, 0.0};
+
+	glClearColor(1.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor4fv(green);
+	piglit_draw_rect(win_width / 2, 0, win_width / 2, win_height);
+
+	glutSwapBuffers();
+	glReadBuffer(GL_FRONT);
+
+	for (y = 0; y < win_height; y++) {
+		for (x = 0; x < win_width; x++) {
+			float *expected;
+
+			if (x >= win_width / 2)
+				expected = green;
+			else
+				expected = red;
+
+			pass &= piglit_probe_pixel_rgb(x, y, expected);
+		}
+	}
+
+	glReadBuffer(GL_BACK);
+
+	if (Automatic) {
+		piglit_report_result(pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE);
+	}
+}
+
+
+static void reshape(int width, int height)
+{
+	win_width = width;
+	win_height = height;
+
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(0.0, width, 0.0, height, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+static void
+init(void)
+{
+	reshape(WIN_WIDTH, WIN_HEIGHT);
+}
+
+int main(int argc, char**argv)
+{
+	int i;
+	glutInit(&argc, argv);
+
+	for(i = 1; i < argc; ++i) {
+		if (!strcmp(argv[i], "-auto"))
+			Automatic = 1;
+		else
+			printf("Unknown option: %s\n", argv[i]);
+	}
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_STENCIL);
+	glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("read-front");
+	glutKeyboardFunc(piglit_escape_exit_key);
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
