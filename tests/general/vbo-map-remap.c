3b821e8c56b54339f5375048bd2e024b688809e7
Eric Anholt <eric@anholt.net>
2009-07-02 17:49:11 -0700

    vbo-map-remap: Clean up test (camelcase, whitespace, naming).

--- tests/general/vbo-map-remap.c
+++ tests/general/vbo-map-remap.c
@@ -19,10 +19,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Ben Holmes <shranzel@hotmail.com>
  */
-
-// author: Ben Holmes
-
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -37,138 +37,107 @@
 #include "piglit-util.h"
 
 static GLboolean Automatic = GL_FALSE;
-static GLuint vBuffer;
+static GLuint vbo;
 
 static void
-Init()
+init()
 {
 
-        glewInit();
-        glMatrixMode(GL_PROJECTION);
-        glPushMatrix();
-        glLoadIdentity();
-        glOrtho(0, 400, 0, 300, -1, 1);
+	glewInit();
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glOrtho(0, 400, 0, 300, -1, 1);
 
-        glMatrixMode(GL_MODELVIEW);
-        glPushMatrix();
-        glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
 
-        glEnable(GL_VERTEX_ARRAY);
+	glEnable(GL_VERTEX_ARRAY);
+
+	glGenBuffersARB(1, &vbo);
+	glBindBufferARB(GL_ARRAY_BUFFER_ARB, vbo);
+	glBufferDataARB(GL_ARRAY_BUFFER_ARB, 12 * sizeof(GLfloat),
+			NULL, GL_DYNAMIC_DRAW);
 }
 
 static void
-vboMap(GLfloat *vArray, int count)
+vbo_write_floats_mapped(float *varray, int count)
 {
+	float *ptr = glMapBufferARB(GL_ARRAY_BUFFER_ARB, GL_WRITE_ONLY_ARB);
+	int i;
 
-
-	float *ptr = (float*)glMapBufferARB(GL_ARRAY_BUFFER_ARB, 
-						GL_READ_WRITE_ARB);
-
-	int i=0;
-	for (i;i<count;++i) {
-		*ptr = vArray[i];
+	for (i = 0; i < count; ++i) {
+		*ptr = varray[i];
 		++ptr;
 	}
 
-	if(!glUnmapBufferARB(GL_ARRAY_BUFFER_ARB))
+	if (!glUnmapBufferARB(GL_ARRAY_BUFFER_ARB))
 		piglit_report_result(PIGLIT_FAILURE);
-
-}
-
-static void
-vboInit()
-{
-        glGenBuffersARB(1, &vBuffer);
-        glBindBufferARB(GL_ARRAY_BUFFER_ARB, vBuffer);
-	glBufferDataARB(GL_ARRAY_BUFFER_ARB, 12*sizeof(GLfloat), 
-			NULL, GL_DYNAMIC_DRAW);
-
 }
 
 static void
 display()
 {
+	GLfloat white[4] = {1.0, 1.0, 1.0, 0.0};
+	GLboolean pass = GL_TRUE;
+	GLfloat varray1[12] = {175, 125, 0,
+			       175, 175, 0,
+			       125, 125, 0,
+			       125, 175, 0};
+	GLfloat varray2[12] = {275, 125, 0,
+			       275, 175, 0,
+			       225, 125, 0,
+			       225, 175, 0};
+	GLenum err;
 
-	GLfloat vArray[12] = {  175, 125, 0,
-				175, 175, 0,
-				125, 125, 0,
-				125, 175, 0};
-
-	glBindBufferARB(GL_ARRAY_BUFFER_ARB, vBuffer);
-
+	glBindBufferARB(GL_ARRAY_BUFFER_ARB, vbo);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(3, GL_FLOAT, 0, 0);
+	glVertexPointer(3, GL_FLOAT, 0, 0);
 
-	vboMap(vArray, 12);
+	vbo_write_floats_mapped(varray1, 12);
 
        	glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
+	vbo_write_floats_mapped(varray2, 12);
 
-	GLfloat vArray1[12] = { 275, 125, 0,
-				275, 175, 0,
-				225, 125, 0,
-				225, 175, 0};
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-	vboMap(vArray1, 12);
-
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-
-	glutSwapBuffers();
-	glFlush();
-
-
-	glDisableClientState(GL_VERTEX_ARRAY);
-
-	GLfloat white[4] = {1.0, 1.0, 1.0, 0.0};
-	GLboolean pass;
-
-	if(glGetError() == 0)
-		pass = GL_TRUE;
-	else
+	if ((err = glGetError()) != 0) {
+		printf("gl error: 0x%08x\n", err);
 		pass = GL_FALSE;
+	}
 
 	pass = pass && piglit_probe_pixel_rgb(250, 150, white);
 	pass = pass && piglit_probe_pixel_rgb(150, 150, white);
 
-	if(Automatic)
+	glutSwapBuffers();
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+	if (Automatic)
 		piglit_report_result(pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE);
-
 }
-
-static void Key(unsigned char key, int x, int y)
-{
-        (void) x;
-        (void) y;
-        switch (key) {
-        case 27:
-                exit(0);
-                break;
-        }
-        glutPostRedisplay();
-}
-
 
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
-	if(argc==2 && !strncmp(argv[1],"-auto", 5))
-                Automatic = GL_TRUE;
+	if (argc == 2 && !strcmp(argv[1],"-auto"))
+		Automatic = GL_TRUE;
 
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-        glutInitWindowSize(400, 300);
-        glutCreateWindow("VBO map remap");
-        glutDisplayFunc(display);
-	glutKeyboardFunc(Key);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(400, 300);
+	glutCreateWindow("VBO map remap");
+	glutDisplayFunc(display);
+	glutKeyboardFunc(piglit_escape_exit_key);
 
-        Init();
-        vboInit();
+	init();
 
-        glutMainLoop();
+	glutMainLoop();
 
-	glDeleteBuffersARB(1, &vBuffer);
-        return 0;
+	glDeleteBuffersARB(1, &vbo);
 
+	return 0;
 }
