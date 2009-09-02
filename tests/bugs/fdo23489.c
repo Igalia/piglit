1c1b74d5f344966d93481e99a6b66e0be5cc514e
Vinson Lee <vlee@vmware.com>
2009-09-02 15:25:59 -0700

    fdo23489: Fix up and build as a piglit test.

--- tests/bugs/fdo23489.c
+++ tests/bugs/fdo23489.c
@@ -1,45 +1,56 @@
-#include <GL/gl.h>
+/*
+ * Test case from fdo bug #23489
+ * http://bugs.freedesktop.org/show_bug.cgi?id=23489
+ */
+
+#include <string.h>
+
+#if defined(__APPLE__)
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
+#endif
+
+#include "piglit-util.h"
+
+static int Automatic = 0;
 
 void display(void)
 {
-    int i;
+	int i;
 
-    for (i=0; i < 100; i++) {
-	glBegin(GL_LINES);
-	glEnd();
-    }
-    glFlush ();
+	for (i = 0; i < 100; i++) {
+		glBegin(GL_LINES);
+		glEnd();
+	}
+	glFlush();
+
+	if (Automatic) {
+		piglit_report_result(PIGLIT_SUCCESS);
+	}
 }
 
-void init (void) 
+void init(void)
 {
-/*  select clearing (background) color       */
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-
-/*  initialize viewing values  */
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
-/* 
- *  Declare initial window size, position, and display mode
- *  (single buffer and RGBA).  Open window with "hello"
- *  in its title bar.  Call initialization routines.
- *  Register callback function to display graphics.
- *  Enter main loop and process events.
- */
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (250, 250); 
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow ("hello");
-    init ();
-    glutDisplayFunc(display); 
-    glutMainLoop();
-    return 0;   /* ISO C requires main to return int. */
+	glutInit(&argc, argv);
+	if (argc == 2 && !strcmp(argv[1], "-auto"))
+		Automatic = 1;
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("fdo23489");
+	glutKeyboardFunc(piglit_escape_exit_key);
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
 }
 
