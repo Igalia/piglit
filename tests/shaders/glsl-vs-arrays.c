4654669f713b19c02f01aedf099ab7a9174bd9ee
Eric Anholt <eric@anholt.net>
2009-11-12 12:39:16 -0800

    glsl-vs-arrays: New test for showing off 965 breakage with arrays in VS.
    (a glean glsl1 testcase also covers it, but it's mixed in with a million
    other testcases)

---
+++ tests/shaders/glsl-vs-arrays.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright © 2009 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *
+ */
+
+/** @file glsl-vs-arrays.c
+ *
+ * Tests use of a temporary array as a source/destination in the VS.
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+static GLint prog;
+static int index1_location, index2_location;
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	static const float green[] = {0.0, 1.0, 0.0, 0.0};
+
+	glClearColor(0.5, 0.5, 0.5, 0.5);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUniform1i(index1_location, 3);
+	glUniform1i(index2_location, 2);
+	piglit_draw_rect(10, 10, 10, 10);
+
+	pass &= piglit_probe_pixel_rgb(15, 15, green);
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+void piglit_init(int argc, char **argv)
+{
+	GLint vs, fs;
+
+	if (!GLEW_VERSION_2_0) {
+		printf("Requires OpenGL 2.0\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
+	vs = piglit_compile_shader(GL_VERTEX_SHADER,
+				   SOURCE_DIR "tests/shaders/glsl-vs-arrays.vert");
+	fs = piglit_compile_shader(GL_FRAGMENT_SHADER,
+				   SOURCE_DIR "tests/shaders/glsl-vs-arrays.frag");
+
+	prog = piglit_link_simple_program(vs, fs);
+
+	index1_location = glGetUniformLocation(prog, "index1");
+	index2_location = glGetUniformLocation(prog, "index2");
+
+	glUseProgram(prog);
+}
