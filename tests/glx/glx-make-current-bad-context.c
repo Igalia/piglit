55ba75c6aeef8879ff8ba94640aa133af753f60f
Eric Anholt <eric@anholt.net>
2009-11-25 09:15:40 -0800

    glx-multithread-makecurrent-*: New tests for MESA_multithread_makecurrent

---
+++ tests/glx/glx-make-current-bad-context.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/** @file glx-make-current-bad-context.c
+ *
+ * Test that glXMakeCurrent() with a bad context correctly returns
+ * GLXBadContext.
+ */
+
+#include "piglit-util.h"
+#include "piglit-glx-util.h"
+
+int piglit_width = 50, piglit_height = 50;
+
+bool found_badvalue = false;
+int expect_badvalue(Display *dpy, XErrorEvent *e)
+{
+	if (e->error_code == BadValue)
+		found_badvalue = true;
+
+	return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+	Display *dpy;
+	Window win;
+	XVisualInfo *visinfo;
+	int i;
+	int (*old_handler)(Display *, XErrorEvent *);
+	GLXContext ctx;
+	int attrib[] = {
+		GLX_RGBA,
+		GLX_RED_SIZE, 1,
+		GLX_GREEN_SIZE, 1,
+		GLX_BLUE_SIZE, 1,
+		GLX_DOUBLEBUFFER,
+		None
+	};
+	GLXFBConfig config, *configs;
+	int nconfigs;
+
+	for(i = 1; i < argc; ++i) {
+		if (!strcmp(argv[i], "-auto"))
+			piglit_automatic = 1;
+		else
+			fprintf(stderr, "Unknown option: %s\n", argv[i]);
+	}
+
+	dpy = XOpenDisplay(NULL);
+	if (dpy == NULL) {
+		fprintf(stderr, "couldn't open display\n");
+		piglit_report_result(PIGLIT_FAILURE);
+	}
+	visinfo = piglit_get_glx_visual(dpy);
+	win = piglit_get_glx_window(dpy, visinfo);
+
+	configs = glXChooseFBConfig(dpy, DefaultScreen(dpy),
+				    attrib, &nconfigs);
+	assert(nconfigs > 0);
+	config = configs[0];
+	XFree(configs);
+
+	old_handler = XSetErrorHandler(expect_badvalue);
+	ctx = glXCreateNewContext(dpy, config, 0x1010, NULL, True);
+	XSync(dpy, 0);
+	XSetErrorHandler(old_handler);
+
+	if (!found_badvalue) {
+		printf("Failed to get BadValue from glXCreateContext().\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	if (ctx != NULL) {
+		glXMakeCurrent(dpy, win, ctx);
+		piglit_report_result(PIGLIT_PASS);
+	} else {
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	return 0;
+}
---
+++ tests/glx/glx-multithread-makecurrent-4.c
@@ -0,0 +1,174 @@
+/*
+ * Copyright © 2009-2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/** @file glx-multithread-makecurrent-4.c
+ *
+ * Test of issue #3 of GLX_MESA_multithread_makecurrent: Bind one
+ * context into multiple threads, do rendering from both, then destroy
+ * the context in one and keep using it to render from the other.
+ */
+
+#include <unistd.h>
+#include "piglit-util.h"
+#include "piglit-glx-util.h"
+#include "pthread.h"
+
+int piglit_width = 70, piglit_height = 30;
+static Display *dpy;
+static Window win;
+static GLXContext ctx;
+static pthread_mutex_t mutex;
+static XVisualInfo *visinfo;
+static int current_step = 1;
+
+static void
+get_lock_for_step(int step)
+{
+	while (true) {
+		pthread_mutex_lock(&mutex);
+		if (step == current_step) {
+			current_step++;
+			return;
+		}
+		pthread_mutex_unlock(&mutex);
+		usleep(1);
+	}
+}
+
+static void *
+thread1_func(void *arg)
+{
+	get_lock_for_step(1);
+	glXMakeCurrent(dpy, win, ctx);
+	glColor4f(0.0, 1.0, 0.0, 0.0);
+	piglit_draw_rect(10, 10, 10, 10);
+	pthread_mutex_unlock(&mutex);
+
+	get_lock_for_step(3);
+	glXMakeCurrent(dpy, None, None);
+	glXDestroyContext(dpy, ctx);
+	pthread_mutex_unlock(&mutex);
+
+	return NULL;
+}
+
+static void *
+thread2_func(void *arg)
+{
+	get_lock_for_step(2);
+	glXMakeCurrent(dpy, win, ctx);
+	glColor4f(0.0, 1.0, 0.0, 0.0);
+	piglit_draw_rect(30, 10, 10, 10);
+	pthread_mutex_unlock(&mutex);
+
+	get_lock_for_step(4);
+	piglit_draw_rect(50, 10, 10, 10);
+	pthread_mutex_unlock(&mutex);
+
+	return NULL;
+}
+
+enum piglit_result
+draw(Display *dpy)
+{
+	GLboolean pass = GL_TRUE;
+	float green[] = {0.0, 1.0, 0.0, 1.0};
+	float gray[] = {0.5, 0.5, 0.5, 1.0};
+	pthread_t thread1, thread2;
+	void *retval;
+	int ret;
+
+	ctx = piglit_get_glx_context(dpy, visinfo);
+	glXMakeCurrent(dpy, win, ctx);
+
+	piglit_require_glx_extension(dpy, "MESA_multithread_makecurrent");
+
+	/* Clear background to gray */
+	glClearColor(0.5, 0.5, 0.5, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
+	pthread_mutex_init(&mutex, NULL);
+
+	glFlush();
+	/* Now, spawn some threads that do some drawing, both with this
+	 * context
+	 */
+	pthread_create(&thread1, NULL, thread1_func, NULL);
+	pthread_create(&thread2, NULL, thread2_func, NULL);
+
+	ret = pthread_join(thread1, &retval);
+	assert(ret == 0);
+	ret = pthread_join(thread2, &retval);
+	assert(ret == 0);
+
+	pthread_mutex_destroy(&mutex);
+	glFlush();
+
+	pass &= piglit_probe_rect_rgba( 0, 10, 10, 10, gray);
+	pass &= piglit_probe_rect_rgba(10, 10, 10, 10, green);
+	pass &= piglit_probe_rect_rgba(20, 10, 10, 10, gray);
+	pass &= piglit_probe_rect_rgba(30, 10, 10, 10, green);
+	pass &= piglit_probe_rect_rgba(40, 10, 10, 10, gray);
+	pass &= piglit_probe_rect_rgba(50, 10, 10, 10, green);
+	pass &= piglit_probe_rect_rgba(60, 10, 10, 10, gray);
+
+	pass &= piglit_probe_rect_rgba(0, 0, piglit_width, 10, gray);
+	pass &= piglit_probe_rect_rgba(0, 20, piglit_width, 10, gray);
+
+	glXSwapBuffers(dpy, win);
+
+	glXMakeCurrent(dpy, None, None);
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+int
+main(int argc, char **argv)
+{
+	int i;
+
+	for(i = 1; i < argc; ++i) {
+		if (!strcmp(argv[i], "-auto"))
+			piglit_automatic = 1;
+		else
+			fprintf(stderr, "Unknown option: %s\n", argv[i]);
+	}
+
+	dpy = XOpenDisplay(NULL);
+	if (dpy == NULL) {
+		fprintf(stderr, "couldn't open display\n");
+		piglit_report_result(PIGLIT_FAILURE);
+	}
+	visinfo = piglit_get_glx_visual(dpy);
+	win = piglit_get_glx_window(dpy, visinfo);
+
+	XFree(visinfo);
+
+	XMapWindow(dpy, win);
+
+	piglit_glx_event_loop(dpy, draw);
+
+	return 0;
+}
