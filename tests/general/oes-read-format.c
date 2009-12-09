c9d0bcf8cca97e1a833ed1511afe969ac1133765
Eric Anholt <eric@anholt.net>
2009-12-09 10:23:24 -0800

    oes-read-format: New test to make sure that basic OES_read_format works.

---
+++ tests/general/oes-read-format.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2009 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *
+ */
+
+/** @file oes-read-format.c
+ *
+ * Tests that GL_OES_read_format works and returns a set of parameters valid
+ * for ReadPixels.
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	static float green[] = {0.0, 1.0, 0.0, 0.0};
+	uint32_t vals[4];
+	GLint format, type;
+
+	glColor4fv(green);
+	piglit_draw_rect(0, 0, piglit_width, piglit_height);
+
+	glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_FORMAT_OES, &format);
+	glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_TYPE_OES, &type);
+
+	glReadPixels(1, 1, 1, 1, format, type, vals);
+
+	pass = glGetError() == 0;
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+
+static void reshape(int width, int height)
+{
+	piglit_width = width;
+	piglit_height = height;
+
+	piglit_ortho_projection(width, height, GL_FALSE);
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	reshape(piglit_width, piglit_height);
+	piglit_require_extension("GL_OES_read_format");
+}
