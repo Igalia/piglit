b18a779c9501cd5a5eca53b77f253e129fcfa30d
Eric Anholt <eric@anholt.net>
2009-12-09 11:26:41 -0800

    fbo-flushing: New test for texture cache flushing with FBOs.
    Bug #25377

---
+++ tests/fbo/fbo-flushing.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright © 2009 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *
+ */
+
+/** @file fbo-flushing.c
+ *
+ * Tests that rendering to a texture then texturing from it gets correct
+ * results.
+ *
+ * This caught a bug where the texture cache wasn't flushed appropriately
+ * on the Intel drivers once additional batchbuffer flushing had been
+ * removed.
+ */
+
+#include "piglit-util.h"
+#include "piglit-framework.h"
+
+#define TEX_WIDTH 128
+#define TEX_HEIGHT 128
+
+int piglit_width = 300;
+int piglit_height = 300;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	int y, probe_x, probe_y, size;
+	GLuint tex, fb;
+	const float red[] =   {1, 0, 0, 0};
+	const float green[] = {0, 1, 0, 0};
+
+	glClearColor(0.5, 0.5, 0.5, 0.5);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+		     TEX_WIDTH, TEX_HEIGHT, 0,
+		     GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glGenFramebuffersEXT(1, &fb);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fb);
+	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT,
+				  GL_COLOR_ATTACHMENT0_EXT,
+				  GL_TEXTURE_2D,
+				  tex,
+				  0);
+	assert(glGetError() == 0);
+
+	assert(glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) ==
+	       GL_FRAMEBUFFER_COMPLETE_EXT);
+
+	/* For each power of two size we test, draw red to it, draw it to
+	 * the framebuffer, then draw green to it and draw it to the
+	 * framebuffer.
+	 *
+	 * Hopefully between these we'll catch any flushing fail.
+	 */
+	y = 0;
+	for (size = TEX_WIDTH; size > 0; size /= 2) {
+		glDisable(GL_TEXTURE_2D);
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fb);
+		glColor4fv(red);
+		piglit_ortho_projection(TEX_WIDTH, TEX_HEIGHT, GL_FALSE);
+		piglit_draw_rect(0, y, size, size);
+
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+		glEnable(GL_TEXTURE_2D);
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+ 		piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+		piglit_draw_rect_tex(0, y, size, size, 0, 0, 1, 1);
+
+		glDisable(GL_TEXTURE_2D);
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fb);
+		glColor4fv(green);
+		piglit_ortho_projection(TEX_WIDTH, TEX_HEIGHT, GL_FALSE);
+		piglit_draw_rect(0, y, size, size);
+
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+		glEnable(GL_TEXTURE_2D);
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+ 		piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+		piglit_draw_rect_tex(0, y, size, size, 0, 0, 1, 1);
+		y += size + 5;
+	}
+
+	glDeleteFramebuffersEXT(1, &fb);
+	glDeleteTextures(1, &tex);
+
+	y = 0;
+	for (size = TEX_WIDTH; size > 0; size /= 2) {
+		for (probe_y = 0; probe_y < size; probe_y++) {
+			for (probe_x = 0; probe_x < size; probe_x++) {
+				pass = pass && piglit_probe_pixel_rgb(probe_x,
+								      probe_y,
+								      green);
+			}
+		}
+		y += size + 5;
+	}
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	piglit_require_extension("GL_EXT_framebuffer_object");
+}
