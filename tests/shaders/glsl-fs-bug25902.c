06a16c5ea898bf9b2b8aaa6aab76685ce4116d23
Eric Anholt <eric@anholt.net>
2010-03-10 10:06:10 -0800

    glsl-fs-bug25902: Add a new test for fd.o bug #25902 (965 driver hang).

---
+++ tests/shaders/glsl-fs-bug25902.c
@@ -0,0 +1,98 @@
+/*
+ * Copyright © 2010 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *
+ */
+
+/** @file glsl-fs-bug25902.c
+ *
+ * Tests for a hang in the i965 driver with a masked texture sample operation
+ * in the GLSL path.
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+static int args_location, tex_location;
+static GLint prog;
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	static const float args[4] = {0.0, 1.0, 0.0, 1.0};
+	float black[4] = {0.0, 0.0, 0.0, 0.0};
+
+	glClearColor(0.5, 0.5, 0.5, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUniform4fv(args_location, 1, args);
+	glUniform1i(tex_location, 0);
+	glColor4f(1.0, 0.0, 0.0, 0.0);
+	piglit_draw_rect_tex(10, 10, 10, 10,
+			     0, 0, 1, 1);
+
+	pass &= piglit_probe_pixel_rgb(12, 12, black);
+	pass &= piglit_probe_pixel_rgb(17, 12, args);
+	pass &= piglit_probe_pixel_rgb(12, 17, args);
+	pass &= piglit_probe_pixel_rgb(17, 17, black);
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLint vs, fs;
+	float black[4] = {0.0, 0.0, 0.0, 0.0};
+	float white[4] = {1.0, 1.0, 1.0, 0.0};
+
+	if (!GLEW_VERSION_2_0) {
+		printf("Requires OpenGL 2.0\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
+	vs = piglit_compile_shader(GL_VERTEX_SHADER,
+				   "shaders/glsl-tex-mvp.vert");
+	fs = piglit_compile_shader(GL_FRAGMENT_SHADER,
+				   "shaders/glsl-fs-bug25902.frag");
+
+	prog = piglit_link_simple_program(vs, fs);
+
+	glUseProgram(prog);
+
+	piglit_checkerboard_texture(0, 0,
+				    2, 2,
+				    1, 1,
+				    black, white);
+
+	args_location = glGetUniformLocation(prog, "args");
+	tex_location = glGetUniformLocation(prog, "sampler");
+}
