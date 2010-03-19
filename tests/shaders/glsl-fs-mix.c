aa73714713dafa3645abe23901f72ca4cca2b950
Eric Anholt <eric@anholt.net>
2010-03-19 12:11:57 -0700

    glsl-fs-mix: Simple test for mix() behavior in the FS.

---
+++ tests/shaders/glsl-fs-mix.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2010 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *
+ */
+
+/** @file glsl-fs-mix.c
+ *
+ * Tests that mix() produces the expected output in a fragment shader.
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE | GLUT_ALPHA;
+
+static int args1_location, args2_location, args3_location;
+static GLint prog;
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	/* result = mix(args1, args2, args3) */
+	static const float args1[4] = {1.0, 0.0, 2.0, 0.0};
+	static const float args2[4] = {0.0, 1.0, 0.0, 2.0};
+	static const float args3[4] = {0.5, 0.5, 0.75, 0.25};
+	static const float gray[] = {0.5, 0.5, 0.5, 0.5};
+
+	glClearColor(0.0, 1.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUniform4fv(args1_location, 1, args1);
+	glUniform4fv(args2_location, 1, args2);
+	glUniform4fv(args3_location, 1, args3);
+	piglit_draw_rect(10, 10, 10, 10);
+
+	pass &= piglit_probe_pixel_rgba(15, 15, gray);
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLint vs, fs;
+
+	if (!GLEW_VERSION_2_0) {
+		printf("Requires OpenGL 2.0\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
+	vs = piglit_compile_shader(GL_VERTEX_SHADER,
+				   "shaders/glsl-mvp.vert");
+	fs = piglit_compile_shader(GL_FRAGMENT_SHADER,
+				   "shaders/glsl-fs-mix.frag");
+
+	prog = piglit_link_simple_program(vs, fs);
+
+	glUseProgram(prog);
+
+	args1_location = glGetUniformLocation(prog, "args1");
+	args2_location = glGetUniformLocation(prog, "args2");
+	args3_location = glGetUniformLocation(prog, "args3");
+}
---
+++ tests/shaders/glsl-fs-mix.frag
@@ -0,0 +1,6 @@
+uniform vec4 args1, args2, args3;
+
+void main()
+{
+	gl_FragColor = mix(args1, args2, args3);
+}
