3a34a2379e15e826139e7a2b179fbb49fa2f5ab0
Xavier Chantry <chantry.xavier@gmail.com>
2010-04-12 14:32:02 -0700

    fdo25614-genmipmap: Require OpenGL 1.3.
    glActiveTexture is available only if the GL version is 1.3 or greater.
    
    Signed-off-by: Xavier Chantry <chantry.xavier@gmail.com>
    Signed-off-by: Brian Paul <brianp@vmware.com>

--- tests/bugs/fdo25614-genmipmap.c
+++ tests/bugs/fdo25614-genmipmap.c
@@ -57,6 +57,11 @@ piglit_init(int argc, char **argv)
 {
 	GLint alpha_bits;
 
+	if (!GLEW_VERSION_1_3) {
+		printf("Requires OpenGL 1.3\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
 	loadTex();
 
 	glGetIntegerv(GL_ALPHA_BITS, &alpha_bits);
