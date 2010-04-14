dab3798f02259f04f3fa4e0adf6b0e31c1ebe58f
Neil Roberts <neil@linux.intel.com>
2010-04-14 18:23:32 +0100

    offscreen-scissor: New test for setting a scissor that is fully offscreen
    The test sets a zero-width and height scissor that is off the edge of
    the window. This should clip everything. It then tries to fill the
    window with red triangles. These do not currently get clipped on i965.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

---
+++ tests/general/scissor-offscreen.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2010 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Neil Roberts <neil@linux.intel.com>
+ *
+ */
+
+/** @file scissor-offscreen.c
+ *
+ * Test case for setting a scissor that is entirely offscreen. This
+ * should clip everything but under current Mesa master with i965 it
+ * clips nothing
+ *
+ * Bug #27643
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 64;
+int piglit_height = 64;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+enum piglit_result
+piglit_display(void)
+{
+	static const GLfloat window_color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLboolean pass = GL_TRUE;
+
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
+	/* Clear to white */
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* Set a fully offscreen scissor. This should clip everything */
+	glEnable(GL_SCISSOR_TEST);
+	glScissor(0, piglit_height, 0, 0);
+
+	/* Fill the window with red */
+	glColor3f (1.0f, 0.0f, 0.0f);
+	piglit_draw_rect(0, 0, piglit_width, piglit_height);
+
+	glDisable(GL_SCISSOR_TEST);
+
+	/* Verify some pixels around the window */
+	pass = pass && piglit_probe_pixel_rgb(0, 0, window_color);
+	pass = pass && piglit_probe_pixel_rgb(piglit_width / 2, piglit_height / 2,
+				      window_color) && pass;
+	pass = pass && piglit_probe_pixel_rgb(piglit_width - 1, piglit_height - 1,
+				      window_color) && pass;
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+void
+piglit_init(int argc, char**argv)
+{
+}
