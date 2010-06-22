2fcfd22f1294a7db32ccbb4c4c4ccf0fdb411c74
Eric Anholt <eric@anholt.net>
2010-06-22 11:18:19 -0700

    Split the glslparsertests out so they can be run with a standalone compiler.
    ln -s ~/src/glsl2/glsl tests/glslparsertest/glslcompiler
    ./piglit-run.py tests/external-glslparser.tests results/glsl

---
+++ tests/glslparsertest/external-glslparsertest.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# This is the wrapper script for using the standalone GLSL2 compiler
+# for the glslparsertest.  Drop a link named "glslcompiler" in this
+# directory pointing at the compiler binary.
+
+if test "x$2" != "xpass" -a "x$2" != "xfail"; then
+    echo "usage: external-glslparsertest.sh filename [pass|fail]"
+    exit 1;
+fi
+
+result=$(./tests/glslparsertest/glslcompiler $type $1)
+status=$?
+
+if test "x$2" = xpass; then
+    if test "$status" = "0"; then
+	echo PIGLIT: {\'result\': \'pass\' }
+    else
+	echo "shader source:"
+	cat $1
+	echo "compiler result:"
+	echo "$result"
+	echo PIGLIT: {\'result\': \'fail\' }
+    fi
+else
+    if test "$status" != "1"; then
+	echo "shader should have failed:"
+	cat $1
+	echo PIGLIT: {\'result\': \'fail\' }
+    else
+	echo PIGLIT: {\'result\': \'pass\' }
+    fi
+fi
