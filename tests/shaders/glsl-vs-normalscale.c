362b953c94ec1e5bf4c8ecd5c7a845eacef397a5
Eric Anholt <eric@anholt.net>
2010-09-07 16:20:29 -0700

    glsl-vs-normalscale: New test for gl_NormalScale builtin uniform.
    Both 7.8 and master failed on this.

---
+++ tests/shaders/glsl-vs-normalscale.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2010 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *
+ */
+
+/** @file glsl-vs-normalscale.c
+ *
+ * Tests that gl_NormalScale provides a correct value.
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_ALPHA | GLUT_DOUBLE;
+
+static GLint prog;
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	static const float gray[]   = {0.5, 0.5, 0.5, 0.5};
+
+	/* Go ahead and draw with one matrix (gl_NormalScale = 1.0)
+	 * first. Don't bother testing this one.
+	 */
+	piglit_ortho_projection(1, 1, GL_FALSE);
+	piglit_draw_rect(0, 0, 1, 1);
+
+	/* Tweak the modelview and draw over it with another matrix. */
+	glMatrixMode(GL_MODELVIEW);
+	glScalef(1.0, 1.0, 2.0);
+	piglit_draw_rect(0, 0, 1, 1);
+
+	pass = piglit_probe_rect_rgba(0, 0, piglit_width, piglit_height,
+				      gray);
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+void piglit_init(int argc, char **argv)
+{
+	GLint vs, fs;
+	int loc;
+
+	if (!GLEW_VERSION_2_0) {
+		printf("Requires OpenGL 2.0\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	vs = piglit_compile_shader(GL_VERTEX_SHADER,
+				   "shaders/glsl-vs-normalscale.vert");
+	fs = piglit_compile_shader(GL_FRAGMENT_SHADER,
+				   "shaders/glsl-color.frag");
+
+	prog = piglit_link_simple_program(vs, fs);
+
+	glUseProgram(prog);
+
+	loc = glGetUniformLocation(prog, "sampler");
+	glUniform1i(loc, 1);
+}
