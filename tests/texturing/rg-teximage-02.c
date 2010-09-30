59d83d49bd70b035917449bf7175cb8fbe061612
Ian Romanick <ian.d.romanick@intel.com>
2010-09-30 13:39:41 -0700

    Add serveral simple GL_ARB_texture_rg tests

---
+++ tests/texturing/rg-teximage-02.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright © 2010 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * \file rg-teximage-02.c
+ * Verify setting and getting image data for RED or RG formats
+ *
+ * Specify an RGBA image with a RG internal format.  Read the image back as
+ * RGBA.  Verify the red and green components read back match the source image
+ * and the blue and alpha components are 0 and 1, repsectively.
+ *
+ * \author Ian Romanick <ian.d.romanick@intel.com>
+ */
+
+#include "piglit-framework.h"
+#include "piglit-util.h"
+#include "rg-teximage-common.h"
+
+#define WIDTH  256
+#define HEIGHT 256
+static float rgba_image[4 * WIDTH * HEIGHT];
+
+static const GLenum internal_formats[] = {
+	GL_RG,
+	GL_RG8,
+	GL_RG16,
+	GL_RGBA,
+};
+
+GLuint tex[Elements(internal_formats)];
+const unsigned num_tex = Elements(tex);
+GLboolean pass = GL_TRUE;
+
+int piglit_width = WIDTH * Elements(tex);
+int piglit_height = HEIGHT;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+void
+piglit_init(int argc, char **argv)
+{
+	unsigned i;
+	GLfloat result[4 * WIDTH * HEIGHT];
+
+	piglit_require_extension("GL_ARB_texture_rg");
+
+	generate_rainbow_texture_data(WIDTH, HEIGHT, rgba_image);
+
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(Elements(tex), tex);
+
+	for (i = 0; i < Elements(internal_formats); i++) {
+		GLenum err;
+
+		glBindTexture(GL_TEXTURE_2D, tex[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, internal_formats[i],
+			     WIDTH, HEIGHT, 0, GL_RGBA,
+			     GL_FLOAT, rgba_image);
+		err = glGetError();
+		if (err) {
+			fprintf(stderr, "glTexImage2D(internalFormat = 0x%04x) "
+				"generated GL error 0x%04x\n",
+				internal_formats[i], err);
+			pass = GL_FALSE;
+		}
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+				GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+				GL_NEAREST);
+
+		if (internal_formats[i] != GL_RGBA) {
+			glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_FLOAT,
+				      result);
+			pass = compare_texture(rgba_image, result,
+					       internal_formats[i], GL_RGBA,
+					       (WIDTH * HEIGHT), GL_TRUE)
+				&& pass;
+		}
+	}
+
+	if (piglit_automatic)
+		piglit_report_result(pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE);
+}
---
+++ tests/texturing/rg-teximage-common.c
@@ -0,0 +1,141 @@
+/*
+ * Copyright © 2010 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * \file rg-teximage-01.c
+ * Verify setting and getting image data for RED or RG formats
+ *
+ * Specify an RGBA image with a RED internal format.  Read the image back as
+ * RGBA.  Verify the red components read back match the source image and the
+ * green, blue, and alpha components are 0, 0, and 1, repsectively.
+ *
+ * \author Ian Romanick <ian.d.romanick@intel.com>
+ */
+
+#include "piglit-framework.h"
+#include "piglit-util.h"
+#include "rg-teximage-common.h"
+
+/**
+ * Map an integer from [0, maximum] to a float on [0, 2*PI].
+ */
+#define NORMALIZE_TO_RADIANS(x, maximum)		\
+	(((float) x / (float) maximum) * 2.0 * M_PI)
+
+#define EPSILON (1.0 / 255.0)
+
+enum piglit_result
+piglit_display(void)
+{
+	unsigned i;
+
+	for (i = 0; i < num_tex; i++) {
+		glBindTexture(GL_TEXTURE_2D, tex[i]);
+
+		piglit_draw_rect_tex(-1.0 + ((float) (2 * i) / num_tex), -1.0,
+				     2.0 / num_tex, 2.0,
+				     0.0, 0.0, 1.0, 1.0);
+	}
+
+	glutSwapBuffers();
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
+
+
+GLboolean
+compare_texture(const GLfloat *orig, const GLfloat *copy,
+		GLenum orig_fmt, GLenum copy_fmt, unsigned num_pix,
+		GLboolean has_green)
+{
+	GLboolean logged = GL_FALSE;
+	GLboolean pass = GL_TRUE;
+	unsigned i;
+
+	for (i = 0; i < num_pix; i++) {
+		if (fabs(orig[0] - copy[0]) > EPSILON) {
+			if (!logged) {
+				fprintf(stderr,
+					"Got bad R channel reading back "
+					"0x%04x as 0x%04x\n",
+					orig_fmt, copy_fmt);
+				logged = GL_TRUE;
+			}
+
+			pass = GL_FALSE;
+		}
+
+		if (has_green && fabs(orig[0] - copy[0]) > EPSILON) {
+			if (!logged) {
+				fprintf(stderr,
+					"Got bad G channel reading back "
+					"0x%04x as 0x%04x\n",
+					orig_fmt, copy_fmt);
+				logged = GL_TRUE;
+			}
+
+			pass = GL_FALSE;
+		}
+
+		if ((!has_green && copy[1] != 0.0)
+		    || copy[2] != 0.0
+		    || copy[3] != 1.0) {
+			if (!logged) {
+				fprintf(stderr,
+					"Got bad %s channel reading back "
+					"0x%04x as 0x%04x\n",
+					has_green ? "B/A" : "G/B/A",
+					orig_fmt, copy_fmt);
+				logged = GL_TRUE;
+			}
+
+			pass = GL_FALSE;
+		}
+
+		orig += 4;
+		copy += 4;
+	}
+
+	return pass;
+}
+
+
+void
+generate_rainbow_texture_data(unsigned width, unsigned height, float *img)
+{
+	unsigned i;
+	unsigned j;
+
+	for (i = 0; i < height; i++) {
+		const float bias = NORMALIZE_TO_RADIANS(i, height);
+
+		for (j = 0; j < width; j++) {
+			const float angle = NORMALIZE_TO_RADIANS(j, width);
+
+			img[0] = (cos(angle + bias) + 1.0) * 0.5;
+			img[1] = (sin(angle - bias) + 1.0) * 0.5;
+			img[2] = (cos(bias) + 1.0) * 0.5;
+			img[3] = (sin(bias) + 1.0) * 0.5;
+			img += 4;
+		}
+	}
+}
