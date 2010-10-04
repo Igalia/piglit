83d11a7cf54d88d70be49c792b31d6271171f0bb
Vinson Lee <vlee@vmware.com>
2010-10-04 12:21:35 -0700

    glsl-link-bug30552: Add test case to reproduce bug #30552.

---
+++ tests/shaders/glsl-link-bug30552.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010 VMware, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * \file glsl-link-bug30552.c
+ *
+ * \author Vinson Lee <vlee@vmware.com>
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+enum piglit_result
+piglit_display(void)
+{
+	return PIGLIT_FAILURE;
+}
+
+void piglit_init(int argc, char **argv)
+{
+	GLint vert;
+	GLint prog;
+	GLboolean ok;
+
+	if (!GLEW_VERSION_2_0) {
+		printf("Requires OpenGL 2.0\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	vert =
+		piglit_compile_shader(GL_VERTEX_SHADER,
+				      "shaders/glsl-link-bug30552.vert");
+	prog = glCreateProgram();
+	glAttachShader(prog, vert);
+	glLinkProgram(prog);
+
+	ok = piglit_link_check_status(prog);
+	piglit_report_result(ok ? PIGLIT_SUCCESS : PIGLIT_FAILURE);
+}
