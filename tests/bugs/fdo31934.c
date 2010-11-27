82b519e6f53c65e80302d08a5df61808d8fc2ff7
Vinson Lee <vlee@vmware.com>
2010-11-26 19:53:55 -0800

    fdo31934: Convert to piglit test.

--- tests/bugs/fdo31934.c
+++ tests/bugs/fdo31934.c
@@ -1,17 +1,27 @@
-#include <stdlib.h>
+#include "piglit-util.h"
 
-#define GL_GLEXT_PROTOTYPES
-#include <GL/gl.h>
-#include <GL/glut.h>
+static int Automatic = 0;
 
 int main (int argc, char *argv[])
 {
+    GLuint id;
+
     glutInit(&argc, argv);
+    if (argc == 2 && !strcmp(argv[1], "-auto"))
+        Automatic = 1;
     glutInitDisplayMode(GLUT_RGBA);
     glutInitWindowSize(512,512);
-    glutCreateWindow("");
+    glutCreateWindow("fdo31934");
 
-    GLuint id;
+    glewInit();
+
+    if (!GLEW_VERSION_1_5) {
+        printf("Requires OpenGL 1.5\n");
+        piglit_report_result(PIGLIT_SKIP);
+    }
+
+    piglit_require_extension("GL_ARB_vertex_buffer_object");
+
     glGenBuffersARB(1, &id);
     glBindBufferARB(GL_ARRAY_BUFFER_ARB, id);
     glBufferDataARB(GL_ARRAY_BUFFER_ARB, 0, NULL, GL_STATIC_DRAW);
@@ -20,6 +30,9 @@ int main (int argc, char *argv[])
     glBindBufferARB(GL_ARRAY_BUFFER_ARB, id);
     glMapBuffer(GL_ARRAY_BUFFER_ARB, GL_READ_WRITE_ARB); /* CRASH! */
     glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
+
+    if (Automatic)
+        printf("PIGLIT: {'result': 'pass' }\n");
 
     return 0;
 }
