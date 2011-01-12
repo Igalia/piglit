d4925237117bdc16d7844c0e7a00805bfa2b8ac6
Eric Anholt <eric@anholt.net>
2011-01-12 13:10:15 -0800

    glsl-fs-flat-color: New test for a bug in the i965 driver.
    With the new FS backend, it was trying to do full interpolation on the
    flat interpolation parameters, which don't have the deltas zeroed.

---
+++ tests/shaders/glsl-fs-flat-color.c
@@ -0,0 +1,119 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * \file glsl-fs-flat-color.c
+ * Test that gl_Color works correctly with GL_SHADE_MODEL of GL_FLAT
+ */
+#include "piglit-util.h"
+
+int piglit_width = 30, piglit_height = 30;
+int piglit_window_mode = GLUT_RGB | GLUT_ALPHA | GLUT_DOUBLE;
+
+GLuint prog;
+
+GLvoid
+draw_rect(float x, float y, float w, float h, float *color)
+{
+	float verts[4][4];
+	float colors[16];
+
+	verts[0][0] = x;
+	verts[0][1] = y;
+	verts[0][2] = 0.0;
+	verts[0][3] = 1.0;
+	verts[1][0] = x + w;
+	verts[1][1] = y;
+	verts[1][2] = 0.0;
+	verts[1][3] = 1.0;
+	verts[2][0] = x + w;
+	verts[2][1] = y + h;
+	verts[2][2] = 0.0;
+	verts[2][3] = 1.0;
+	verts[3][0] = x;
+	verts[3][1] = y + h;
+	verts[3][2] = 0.0;
+	verts[3][3] = 1.0;
+
+	/* Fill in just the first color.  We're flat shading, after all. */
+	memset(&colors, 0, sizeof(colors));
+	memcpy(colors, color, 4 * sizeof(float));
+
+	glVertexPointer(4, GL_FLOAT, 0, verts);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glColorPointer(4, GL_FLOAT, 0, colors);
+	glEnableClientState(GL_COLOR_ARRAY);
+
+	glDrawArrays(GL_POLYGON, 0, 4);
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
+}
+
+enum piglit_result
+piglit_display(void)
+{
+	float green[4] = { 0.0, 1.0, 0.0, 1.0 };
+	enum piglit_result result = PIGLIT_SUCCESS;
+
+	glShadeModel(GL_FLAT);
+
+	draw_rect(-1, -1, 2, 2, green);
+
+	if (!piglit_probe_rect_rgba(0, 0, piglit_width, piglit_height, green))
+		result = PIGLIT_FAIL;
+
+	if (!piglit_automatic)
+		glutSwapBuffers();
+
+	return result;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLuint vs, fs;
+
+	if (!GLEW_VERSION_2_0) {
+		printf("Requires OpenGL 2.0\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	vs = piglit_compile_shader(GL_VERTEX_SHADER,
+				   "shaders/glsl-color-mvp.vert");
+	fs = piglit_compile_shader(GL_FRAGMENT_SHADER,
+				   "shaders/glsl-color.frag");
+
+	prog = piglit_link_simple_program(vs, fs);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	/* Don't try to render if the program failed to link, and linking
+	 * had better succeed!
+	 */
+	if (!piglit_link_check_status(prog))
+		piglit_report_result(PIGLIT_FAILURE);
+
+	glUseProgram(prog);
+}
