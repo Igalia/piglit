f9e57798da95f9e53344f8467cb973ef8431ed9a
Marek Olšák <maraeo@gmail.com>
2011-03-04 01:29:31 +0100

    tex-miplevel-selection: test base/max level, min/max lod, lod bias, etc.

---
+++ tests/texturing/tex-miplevel-selection.c
@@ -0,0 +1,222 @@
+/*
+ * Copyright © 2010 Marek Olšák <maraeo@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+/** @file tex-miplevel-selection.c
+ *
+ * Tests the interaction between GL_TEXTURE_BASE/MAX_LEVEL
+ * GL_TEXTURE_MIN/MAX_LOD, TEXTURE_LOD_BIAS, mipmap
+ * filtering on/off, and variable texture scaling.
+ */
+
+#include "piglit-util.h"
+
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+int piglit_width = 900;
+int piglit_height = 600;
+
+#define TEX_WIDTH 32
+#define TEX_HEIGHT 32
+#define LAST_LEVEL 5
+
+static const float colors[][3] = {
+	{1.0, 0.0, 0.0},
+	{0.0, 1.0, 0.0},
+	{0.0, 0.0, 1.0},
+	{1.0, 1.0, 0.0},
+	{0.0, 1.0, 1.0},
+	{1.0, 0.0, 1.0},
+};
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLuint tex, fb;
+	GLenum status;
+	int i, dim;
+
+	piglit_require_extension("GL_EXT_framebuffer_object");
+	if (!GLEW_VERSION_1_4)
+		piglit_report_result(PIGLIT_SKIP);
+
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+
+	for (i = 0, dim = TEX_WIDTH; dim >0; i++, dim /= 2) {
+		glTexImage2D(GL_TEXTURE_2D, i, GL_RGBA,
+			     dim, dim,
+			     0,
+			     GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	}
+	assert(glGetError() == 0);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_TEXTURE_2D);
+
+	glGenFramebuffersEXT(1, &fb);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fb);
+
+	for (i = 0, dim = TEX_WIDTH; dim >0; i++, dim /= 2) {
+		glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT,
+					  GL_COLOR_ATTACHMENT0_EXT,
+					  GL_TEXTURE_2D,
+					  tex,
+					  i);
+
+
+		status = glCheckFramebufferStatusEXT (GL_FRAMEBUFFER_EXT);
+		if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+			fprintf(stderr, "FBO incomplete\n");
+			piglit_report_result(PIGLIT_SKIP);
+		}
+
+		glClearColor(colors[i][0],
+			     colors[i][1],
+			     colors[i][2],
+			     0.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		assert(glGetError() == 0);
+	}
+
+	glDeleteFramebuffersEXT(1, &fb);
+	glBindTexture(GL_TEXTURE_2D, tex);
+
+	glViewport(0, 0, piglit_width, piglit_height);
+
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+}
+
+static void
+draw_quad(int x, int y, int w, int h, int level)
+{
+	float s = (w/(float)TEX_WIDTH) * (1<<level);
+	float t = (h/(float)TEX_HEIGHT) * (1<<level);
+
+	glBegin(GL_QUADS);
+
+	glTexCoord2f(0, 0);
+	glVertex2f(x, y);
+	glTexCoord2f(s, 0);
+	glVertex2f(x + w, y);
+	glTexCoord2f(s, t);
+	glVertex2f(x + w, y + h);
+	glTexCoord2f(0, t);
+	glVertex2f(x, y + h);
+
+	glEnd();
+}
+
+#define CLAMP( X, MIN, MAX )  ( (X)<(MIN) ? (MIN) : ((X)>(MAX) ? (MAX) : (X)) )
+#define MIN2( A, B )   ( (A)<(B) ? (A) : (B) )
+#define MAX2( A, B )   ( (A)>(B) ? (A) : (B) )
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	int scale_to_level, baselevel, maxlevel, minlod, maxlod, bias, mipfilter, expected_level, x, y, i, c;
+	unsigned char *pix, *p;
+
+	glClearColor(0.5, 0.5, 0.5, 0.5);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	i = 0;
+	for (scale_to_level = 0; scale_to_level <= LAST_LEVEL; scale_to_level++)
+		for (baselevel = 0; baselevel <= LAST_LEVEL; baselevel++)
+			for (maxlevel = baselevel; maxlevel <= LAST_LEVEL; maxlevel++)
+				for (minlod = 0; minlod <= LAST_LEVEL; minlod++)
+					for (maxlod = minlod; maxlod <= LAST_LEVEL; maxlod++)
+						for (bias = -LAST_LEVEL; bias <= LAST_LEVEL; bias++)
+							for (mipfilter = 0; mipfilter < 2; mipfilter++) {
+								glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, baselevel);
+								glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, maxlevel);
+								glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_LOD, minlod);
+								glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LOD, maxlod);
+								glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, bias);
+								glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+										mipfilter ? GL_NEAREST_MIPMAP_NEAREST : GL_NEAREST);
+
+								x = (i % (piglit_width/3)) * 3;
+								y = (i / (piglit_width/3)) * 3;
+								draw_quad(x, y, 3, 3, scale_to_level);
+
+								i++;
+							}
+
+	pix = malloc(piglit_width * piglit_height * 4);
+	glReadPixels(0, 0, piglit_width, piglit_height, GL_RGBA, GL_UNSIGNED_BYTE, pix);
+
+	i = 0;
+	for (scale_to_level = 0; scale_to_level <= LAST_LEVEL; scale_to_level++)
+		for (baselevel = 0; baselevel <= LAST_LEVEL; baselevel++)
+			for (maxlevel = baselevel; maxlevel <= LAST_LEVEL; maxlevel++)
+				for (minlod = 0; minlod <= LAST_LEVEL; minlod++)
+					for (maxlod = minlod; maxlod <= LAST_LEVEL; maxlod++)
+						for (bias = -LAST_LEVEL; bias <= LAST_LEVEL; bias++)
+							for (mipfilter = 0; mipfilter < 2; mipfilter++) {
+								if (mipfilter) {
+									expected_level = CLAMP(scale_to_level + bias,
+											       MIN2(baselevel + minlod, maxlevel),
+											       MIN2(baselevel + maxlod, maxlevel));
+								} else {
+									expected_level = baselevel;
+								}
+								assert(expected_level <= 5);
+
+								x = (i % (piglit_width/3)) * 3;
+								y = (i / (piglit_width/3)) * 3;
+								p = pix + (y*piglit_width + x)*4;
+
+								for (c = 0; c < 3; c++) {
+									if (fabs(colors[expected_level][c] - (p[c]/255.0)) > 0.01) {
+										pass = GL_FALSE;
+
+										printf("Probe at (%i,%i)\n", x, y);
+										printf("  Expected mipmap level: %i\n", expected_level);
+										printf("  Expected: %f %f %f\n", colors[expected_level][0],
+										       colors[expected_level][1], colors[expected_level][2]);
+										printf("  Observed: %f %f %f\n", p[0]/255.0, p[1]/255.0, p[2]/255.0);
+										printf("  Scale to level: %i, baselevel: %i, maxlevel: %i, "
+										       "minlod: %i, maxlod: %i, bias: %i, mipfilter: %s\n",
+										       scale_to_level, baselevel, maxlevel, minlod,
+										       maxlod, bias, mipfilter ? "yes" : "no");
+										break;
+									}
+								}
+								i++;
+							}
+
+	free(pix);
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_SUCCESS : PIGLIT_FAILURE;
+}
