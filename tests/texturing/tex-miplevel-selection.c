57fcd108cee25852e2994655f992ac5f0c95ffe4
Marek Olšák <maraeo@gmail.com>
2011-05-07 02:42:35 +0200

    tex-miplevel-selection: test texture2DLod from ARB_shader_texture_lod

--- tests/texturing/tex-miplevel-selection.c
+++ tests/texturing/tex-miplevel-selection.c
@@ -27,6 +27,7 @@
  * Tests the interaction between GL_TEXTURE_BASE/MAX_LEVEL
  * GL_TEXTURE_MIN/MAX_LOD, TEXTURE_LOD_BIAS, mipmap
  * filtering on/off, and variable texture scaling.
+ * Also tests ARB_shader_texture_lod if requested.
  */
 
 #include "piglit-util.h"
@@ -47,7 +48,16 @@ static const float colors[][3] = {
 	{0.0, 1.0, 1.0},
 	{1.0, 0.0, 1.0},
 };
-static GLboolean in_place_probing, no_bias, no_lod;
+static GLboolean in_place_probing, no_bias, no_lod, ARB_shader_texture_lod;
+static GLuint loc_lod;
+
+static const char *fscode =
+	"#extension GL_ARB_shader_texture_lod : require\n"
+	"uniform sampler2D tex;\n"
+	"uniform float lod;\n"
+	"void main() {\n"
+	"  gl_FragColor = texture2DLod(tex, gl_TexCoord[0].xy, lod);"
+	"}\n";
 
 void
 piglit_init(int argc, char **argv)
@@ -55,6 +65,7 @@ piglit_init(int argc, char **argv)
 	GLuint tex, fb;
 	GLenum status;
 	int i, dim;
+	GLuint fs, prog, loc_tex;
 
         for (i = 1; i < argc; i++) {
 		if (strcmp(argv[i], "-inplace") == 0)
@@ -63,11 +74,25 @@ piglit_init(int argc, char **argv)
 			no_bias = GL_TRUE;
 		else if (strcmp(argv[i], "-nolod") == 0)
 			no_lod = GL_TRUE;
+		else if (strcmp(argv[i], "-GL_ARB_shader_texture_lod"))
+			ARB_shader_texture_lod = GL_TRUE;
         }
 
 	piglit_require_extension("GL_EXT_framebuffer_object");
 	if (!GLEW_VERSION_1_4)
 		piglit_report_result(PIGLIT_SKIP);
+
+	if (ARB_shader_texture_lod) {
+		piglit_require_GLSL();
+		piglit_require_extension("GL_ARB_shader_texture_lod");
+
+		fs = piglit_compile_shader_text(GL_FRAGMENT_SHADER, fscode);
+		prog = piglit_link_simple_program(0, fs);
+		piglit_UseProgram(prog);
+		loc_tex = piglit_GetUniformLocation(prog, "tex");
+		loc_lod = piglit_GetUniformLocation(prog, "lod");
+		piglit_Uniform1i(loc_tex, 0);
+	}
 
 	glGenTextures(1, &tex);
 	glBindTexture(GL_TEXTURE_2D, tex);
@@ -127,8 +152,18 @@ piglit_init(int argc, char **argv)
 static void
 draw_quad(int x, int y, int w, int h, int level)
 {
-	float s = (w/(float)TEX_WIDTH) * (1<<level);
-	float t = (h/(float)TEX_HEIGHT) * (1<<level);
+	float s, t;
+
+	if (ARB_shader_texture_lod) {
+		float lod = level;
+		piglit_Uniform1fv(loc_lod, 1, &lod);
+
+		s = w/(float)TEX_WIDTH;
+		t = h/(float)TEX_HEIGHT;
+	} else {
+		s = (w/(float)TEX_WIDTH) * (1<<level);
+		t = (h/(float)TEX_HEIGHT) * (1<<level);
+	}
 
 	glBegin(GL_QUADS);
 
@@ -151,9 +186,8 @@ draw_quad(int x, int y, int w, int h, int level)
 enum piglit_result
 piglit_display(void)
 {
-	GLboolean pass = GL_TRUE;
 	int scale_to_level, baselevel, maxlevel, minlod, maxlod, bias, mipfilter;
-	int expected_level, x, y, i, c;
+	int expected_level, x, y, total, failed, c;
 	int start_bias, end_bias;
 	int start_min_lod, end_min_lod, end_max_lod;
 	unsigned char *pix, *p;
@@ -179,7 +213,8 @@ piglit_display(void)
 	glClearColor(0.5, 0.5, 0.5, 0.5);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	i = 0;
+	total = 0;
+	failed = 0;
 	for (scale_to_level = 0; scale_to_level <= LAST_LEVEL; scale_to_level++)
 		for (baselevel = 0; baselevel <= LAST_LEVEL; baselevel++)
 			for (maxlevel = baselevel; maxlevel <= LAST_LEVEL; maxlevel++)
@@ -198,9 +233,13 @@ piglit_display(void)
 								glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
 										mipfilter ? GL_NEAREST_MIPMAP_NEAREST : GL_NEAREST);
 
-								x = (i % (piglit_width/3)) * 3;
-								y = (i / (piglit_width/3)) * 3;
-								draw_quad(x, y, 3, 3, scale_to_level);
+								x = (total % (piglit_width/3)) * 3;
+								y = (total / (piglit_width/3)) * 3;
+
+								if (ARB_shader_texture_lod)
+									draw_quad(x, y, 3, 3, scale_to_level - baselevel);
+								else
+									draw_quad(x, y, 3, 3, scale_to_level);
 
 								if (mipfilter) {
 									if (no_lod) {
@@ -219,7 +258,7 @@ piglit_display(void)
 
                                                                 if (in_place_probing &&
                                                                     !piglit_probe_pixel_rgb(x, y, colors[expected_level])) {
-									pass = GL_FALSE;
+									failed++;
 									printf("  Expected mipmap level: %i\n", expected_level);
 									printf("  Scale to level: %i, baselevel: %i, maxlevel: %i, "
 									       "minlod: %i, maxlod: %i, bias: %i, mipfilter: %s\n",
@@ -227,14 +266,14 @@ piglit_display(void)
 									       no_lod ? LAST_LEVEL : maxlod, bias, mipfilter ? "yes" : "no");
                                                                 }
 
-								i++;
+								total++;
 							}
 
 	if (!in_place_probing) {
 		pix = malloc(piglit_width * piglit_height * 4);
 		glReadPixels(0, 0, piglit_width, piglit_height, GL_RGBA, GL_UNSIGNED_BYTE, pix);
 
-		i = 0;
+		total = 0;
 		for (scale_to_level = 0; scale_to_level <= LAST_LEVEL; scale_to_level++)
 			for (baselevel = 0; baselevel <= LAST_LEVEL; baselevel++)
 				for (maxlevel = baselevel; maxlevel <= LAST_LEVEL; maxlevel++)
@@ -257,13 +296,13 @@ piglit_display(void)
 									}
 									assert(expected_level <= 5);
 
-									x = (i % (piglit_width/3)) * 3;
-									y = (i / (piglit_width/3)) * 3;
+									x = (total % (piglit_width/3)) * 3;
+									y = (total / (piglit_width/3)) * 3;
 									p = pix + (y*piglit_width + x)*4;
 
 									for (c = 0; c < 3; c++) {
 										if (fabs(colors[expected_level][c] - (p[c]/255.0)) > 0.01) {
-											pass = GL_FALSE;
+											failed++;
 
 											printf("Probe at (%i,%i)\n", x, y);
 											printf("  Expected: %f %f %f\n", colors[expected_level][0],
@@ -277,12 +316,13 @@ piglit_display(void)
 											break;
 										}
 									}
-									i++;
+									total++;
 								}
 		free(pix);
 	}
+	printf("Summary: %i/%i passed\n", total-failed, total);
 
 	glutSwapBuffers();
 
-	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+	return !failed ? PIGLIT_PASS : PIGLIT_FAIL;
 }
