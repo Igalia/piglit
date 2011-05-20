e67530cb1b3aebeed2d26fd0a188b64b522b62f5
Eric Anholt <eric@anholt.net>
2011-05-20 12:52:43 -0700

    draw-copypixels-sync: Test synchronization of render -> blitter rings.
    Reviewed-by: Chad Versace <chad@chad-versace.us>

---
+++ tests/general/draw-copypixels-sync.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * @file draw-copypixels-sync.c
+ *
+ * Test repeated glCopyPixels of small draw calls results.  This
+ * stresses render to blitter syncing on the i965 driver.
+ */
+
+#include "piglit-util.h"
+#include "piglit-framework.h"
+
+int piglit_width = 64;
+int piglit_height = 64;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE | GLUT_ALPHA;
+
+enum piglit_result
+piglit_display(void)
+{
+	int x, y;
+	float green[4] = {0.0, 1.0, 0.0, 0.0};
+	bool pass = true;
+
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
+	glClearColor(1.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor4fv(green);
+
+	for (y = 0; y < piglit_height; y++) {
+		for (x = 0; x < piglit_width; x++) {
+			if ((x & 1) == 0) {
+				piglit_draw_rect(x, y, 1, 1);
+			} else {
+				glRasterPos2i(x, y);
+				glCopyPixels(x - 1, y, 1, 1, GL_COLOR);
+			}
+		}
+	}
+
+	pass = piglit_probe_rect_rgba(0, 0, piglit_width, piglit_height, green);
+
+	glutSwapBuffers();
+
+	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+}
+
+
+void
+piglit_init(int argc, char **argv)
+{
+}
