2cf8917e990f37da755e972733f210b57f50e30f
Brian Paul <brianp@vmware.com>
2011-06-22 07:20:04 -0600

    depthrange-clear: call piglit_ortho_projection() in piglit_display()

--- tests/general/depthrange-clear.c
+++ tests/general/depthrange-clear.c
@@ -86,6 +86,8 @@ piglit_display(void)
 	static float red[]   = {1.0, 0.0, 0.0, 0.0};
 	static float green[] = {0.0, 1.0, 0.0, 0.0};
 
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+
 	glEnable(GL_DEPTH_TEST);
 
 	glClearColor(0.0, 1.0, 0.0, 0.0);
@@ -129,17 +131,7 @@ piglit_display(void)
 }
 
 
-static void reshape(int width, int height)
-{
-	piglit_width = width;
-	piglit_height = height;
-
-	glViewport(0, 0, width, height);
-	piglit_ortho_projection(width, height, GL_FALSE);
-}
-
 void
 piglit_init(int argc, char **argv)
 {
-	reshape(piglit_width, piglit_height);
 }
