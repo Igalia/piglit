9d059906dc155129729e2138b192d51139045181
Paul Berry <stereotype441@gmail.com>
2011-08-09 08:41:27 -0700

    Add tests for gl_ClipDistance and gl_ClipVertex.
    This patch is a preparation for work I'm about to do to add
    gl_ClipDistance support to Mesa, and future work that I hope to do to
    fix bugs in gl_ClipVertex in Mesa.  It adds a thorough set of tests
    for the following behaviors of gl_ClipDistance and gl_ClipVertex:
    
    For gl_ClipVertex:
    - It behaves properly when equal to gl_Position.
    - It behaves properly when not equal to gl_Position.
    - It falls back to gl_Position when it is not set.
    - Its homogeneous coordinate is respected.
    - The homogeneous coordinate is respected when falling back to gl_Position.
    
    For gl_ClipDistance:
    - It works when all 6 clip planes are enabled.
    - Its behavior is properly affected by clip plane enable flags.
    - It can be explicitly or implicitly sized in the vertex shader.
    - It can be sized up to gl_MaxClipDistances but no higher.
    - In the fragment shader, it contains properly interpolated values.
    
    For the interaction between the two:
    - It is an error to assign to both gl_ClipDistance and gl_ClipVertex.
    
    In addition, this patch renames a few of the existing clipping tests
    so that their names more clearly reflect what they're testing.
    
    All of these tests have been validated on the proprietary nVidia
    driver for Linux, except the following:
    
    - fs-clip-distance-interpolated: this fails with the error message
      "gl_ClipDistance is not accessible in this profile".  Based on a
      google search for this error message, I suspect this is an nVidia
      driver bug.  If I replace the fragment shader's reference to
      gl_ClipDistance with a reference to a user-defined vertex shader
      output that contains the same data, the test passes.
    
    - vs-clip-based-on-position and
      vs-clip-based-on-position-homogeneity: these tests fail--no
      clipping occurs.  But they pass if I add the line "gl_ClipVertex =
      gl_Position;" to the end of the vertex shader.  The GLSL 1.30 spec
      clearly states that if neither gl_ClipVertex nor gl_ClipDistance is
      statically written by the vertex shader, then gl_Position should be
      used for clipping, so I conclude that this is an nVidia driver bug.
    
    - mixing-clip-distance-and-clip-vertex-disallowed: the nVidia driver
      fails to detect this error condition, in spite of clear language in
      the GLSL 1.30 spec.  I conclude that this is an nVidia driver bug.

---
+++ tests/spec/glsl-1.30/linker/clipping/mixing-clip-distance-and-clip-vertex-disallowed.c
@@ -0,0 +1,111 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * \file mixing-clip-distance-and-clip-vertex-disallowed.c
+ *
+ * From the GLSL 1.30 spec, section 7.1 (Vertex Shader Special
+ * Variables):
+ *
+ *   It is an error for a shader to statically write both
+ *   gl_ClipVertex and gl_ClipDistance.
+ *
+ * This test verifies that an error is generated if the shader
+ * contains writes to both variables, even if those writes would never
+ * both occur in the same render.
+ */
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+static const char vert[] =
+	"#version 130\n"
+	"uniform bool use_ClipDistance;\n"
+	"void main()\n"
+	"{\n"
+	"  gl_Position = vec4(0.0);\n"
+	"  if (use_ClipDistance) {\n"
+	"    gl_ClipDistance[0] = 1.0;\n"
+	"  } else {\n"
+	"    gl_ClipVertex = vec4(0.0);\n"
+	"  }\n"
+	"}\n";
+
+static const char frag[] =
+	"#version 130\n"
+	"void main()\n"
+	"{\n"
+	"  gl_FragColor = gl_Color;\n"
+	"}\n";
+
+enum piglit_result
+piglit_display(void)
+{
+	return PIGLIT_FAIL;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	const char *glsl_version_string;
+	float glsl_version;
+	GLint ok;
+	GLuint prog;
+	GLuint vs;
+	GLuint fs;
+
+
+	piglit_require_GLSL();
+
+	glsl_version_string = (char *)
+		glGetString(GL_SHADING_LANGUAGE_VERSION);
+	glsl_version = (glsl_version_string == NULL)
+		? 0.0 : strtod(glsl_version_string, NULL);
+	if (glsl_version <= 1.299999) {
+		printf("Test requires GLSL version >= 1.3.  "
+		       "Actual version is %.1f.\n",
+		       glsl_version);
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	vs = piglit_compile_shader_text(GL_VERTEX_SHADER, vert);
+	fs = piglit_compile_shader_text(GL_FRAGMENT_SHADER, frag);
+	prog = glCreateProgram();
+	glAttachShader(prog, vs);
+	glAttachShader(prog, fs);
+	glLinkProgram(prog);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	ok = piglit_link_check_status_quiet(prog);
+	if (ok) {
+		fprintf(stderr,
+			"Linking with a shader that accesses both "
+			"gl_ClipDistance and gl_ClipVertex succeeded when it "
+			"should have failed.\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	piglit_report_result(PIGLIT_PASS);
+}
