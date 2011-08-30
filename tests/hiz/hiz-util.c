03083d0e26052ed6ddac093ba2aad56ed400875b
Chad Versace <chad@chad-versace.us>
2011-08-30 08:28:34 -0700

    hiz: Use piglit_check_gl_error to check to GL errors

--- tests/hiz/hiz-util.c
+++ tests/hiz/hiz-util.c
@@ -95,7 +95,7 @@ hiz_make_fbo(const struct hiz_fbo_options *options)
 		                          GL_COLOR_ATTACHMENT0,
 		                          GL_RENDERBUFFER,
 		                          color_rb);
-		assert(glGetError() == 0);
+         	piglit_check_gl_error(0, PIGLIT_FAIL);
 	}
 
 	/* Bind depth attachment. */
@@ -109,7 +109,7 @@ hiz_make_fbo(const struct hiz_fbo_options *options)
 		                          GL_DEPTH_ATTACHMENT,
 		                          GL_RENDERBUFFER,
 		                          depth_rb);
-		assert(glGetError() == 0);
+         	piglit_check_gl_error(0, PIGLIT_FAIL);
 	}
 
 	/* Bind stencil attachment. */
@@ -123,7 +123,7 @@ hiz_make_fbo(const struct hiz_fbo_options *options)
 		                          GL_STENCIL_ATTACHMENT,
 		                          GL_RENDERBUFFER,
 		                          stencil_rb);
-		assert(glGetError() == 0);
+         	piglit_check_gl_error(0, PIGLIT_FAIL);
 	}
 
 	/* Bind depth/stencil attachment. */
@@ -137,7 +137,7 @@ hiz_make_fbo(const struct hiz_fbo_options *options)
 		                          GL_DEPTH_STENCIL_ATTACHMENT,
 		                          GL_RENDERBUFFER,
 		                          depth_stencil_rb);
-		assert(glGetError() == 0);
+         	piglit_check_gl_error(0, PIGLIT_FAIL);
 	}
 
 	fb_status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
@@ -177,7 +177,7 @@ hiz_delete_fbo(GLuint fbo)
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 	glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
 
-	assert(!glGetError());
+ 	piglit_check_gl_error(0, PIGLIT_FAIL);
 }
 
 /* ------------------------------------------------------------------------ */
@@ -468,7 +468,7 @@ hiz_run_test_stencil_test_common()
 	piglit_draw_rect(1 * dx, 1 * dy,   /* x, y */
 		         2 * dx, 2 * dy);  /* w, h */
 
-	assert(!glGetError());
+	piglit_check_gl_error(0, PIGLIT_FAIL);
 
 	return hiz_probe_color_buffer(expected_colors);
 }
@@ -596,7 +596,7 @@ hiz_run_test_stencil_read_common()
 	piglit_draw_rect(1 * dx, 1 * dy,   /* x, y */
 		         2 * dx, 2 * dy);  /* w, h */
 
-	assert(!glGetError());
+	piglit_check_gl_error(0, PIGLIT_FAIL);
 
 	if (!hiz_probe_color_buffer(expected_colors))
 		return false;
@@ -755,7 +755,7 @@ hiz_run_test_depth_stencil_test_fbo(const struct hiz_fbo_options *fbo_options)
 	piglit_draw_rect(1 * dx, 1 * dy,   /* x, y */
 		         2 * dx, 2 * dy);  /* w, h */
 
-	assert(!glGetError());
+ 	piglit_check_gl_error(0, PIGLIT_FAIL);
 
 	pass = hiz_probe_color_buffer(expected_colors);
 
