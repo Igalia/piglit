6a241f70b316c83860ad921c95344205aa402512
Yuanhan Liu <yuanhan.liu@linux.intel.com>
2011-11-24 09:52:08 +0800

    Add a simple testcase to test that GL_ELEMENT_ARRAY_BUFFER is per vao
    According opengl spec 4.2.pdf table 6.12 (Vertex Array Object State) at
    page 515: the element buffer object is listed in vertex array object.
    
    Add a testcase to test that.
    
    v2: fix a careless 'always-return-PIGLIT_PASS' fault.
    
    v3: use piglit_present_results() instead of glutSwapBuffers() (comments
        from Eric)
    
        Fix typos.
    
    Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Brian Paul <brianp@vmware.com>

---
+++ tests/general/vao-element-array-buffer.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * @file vao-element-array-buffer.c
+ *
+ * A simple test case to test that GL_ELEMENT_ARRAY_BUFFER is part of vao
+ *
+ */
+
+#include "piglit-util.h"
+
+int piglit_width = 100;
+int piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+static GLuint vao;
+
+enum piglit_result
+piglit_display(void)
+{
+	GLboolean pass = GL_TRUE;
+	GLfloat expected[4] = {1, 0, 0, 1};
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindVertexArray(vao);
+
+	glColor3f(1, 0, 0);
+	glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, NULL);
+	pass = piglit_probe_rect_rgba(0, 0, piglit_width, piglit_height, expected);
+
+	piglit_present_results();
+
+	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLuint vbo;
+	GLuint element;
+	GLfloat vertices[] = {
+		-1, -1, 0,
+		 1, -1, 0,
+		 1,  1, 0,
+		-1,  1, 0,
+	};
+	GLubyte indices[] = {0, 1, 2, 3};
+
+	piglit_require_extension("GL_ARB_vertex_array_object");
+
+	glClearColor(0, 0, 0, 1);
+
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &element);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexPointer(3, GL_FLOAT, 0, NULL);
+	glEnableClientState(GL_VERTEX_ARRAY);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
