a40460843b5aa1b9d5fd7e9ff40c1eabe9f44115
Ian Romanick <ian.d.romanick@intel.com>
2011-12-06 23:00:42 -0800

    glx_ext_import_context: Make an imported context current in two processes
    This test fails on NVIDIA's closed-source driver.
    
    This test fails on AMD's closed-source driver because it hits an
    assertion failure in XCB:
    
    xcb_io.c:515: _XReply: Assertion `!dpy->xcb->reply_data' failed.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>

---
+++ tests/spec/glx_ext_import_context/make-current-multi-process.c
@@ -0,0 +1,78 @@
+/* Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "piglit-util.h"
+#include "piglit-glx-util.h"
+#include "common.h"
+
+int main(int argc, char **argv)
+{
+	bool pass = true;
+	pid_t child;
+	int status;
+	Window win;
+
+	GLX_EXT_import_context_setup();
+	get_context_IDs();
+
+	win = piglit_get_glx_window(dpy, visinfo);
+	glXMakeCurrent(dpy, win, indirectCtx);
+
+	child = fork();
+	if (child == 0) {
+		GLXContext ctx;
+		Window child_win;
+
+		GLX_EXT_import_context_setup_for_child();
+
+		child_win = piglit_get_glx_window(dpy, visinfo);
+
+		/* The GLX_EXT_import_context spec says:
+		 *
+		 *     "If <context> is current to some other thread or
+		 *      process, then glXMakeCurrent will generate a BadAccess
+		 *      error."
+		 */
+		ctx = glXImportContextEXT(dpy, indirectID);
+		glXMakeCurrent(dpy, child_win, ctx);
+
+		XSync(dpy, 0);
+		pass = validate_glx_error_code(BadAccess, -1);
+
+		exit(pass ? 0 : 1);
+	}
+
+	/* The test passes if the child exited normally with a return value of
+	 * zero.
+	 */
+	waitpid(child, &status, 0);
+	pass = WIFEXITED(status) && (WEXITSTATUS(status) == 0);
+
+	GLX_EXT_import_context_teardown();
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+	return 0;
+}
