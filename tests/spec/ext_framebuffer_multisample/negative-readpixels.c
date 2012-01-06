cd5dc99533463dd30e45d6c25d4278d2274f01db
Eric Anholt <eric@anholt.net>
2012-01-05 19:33:50 -0800

    EXT_framebuffer_multisample/negative-readpixels: New test for mesa bug.
    v2: Leave testing SAMPLE_BUFFERS to a separate testcase.

---
+++ tests/spec/ext_framebuffer_multisample/negative-readpixels.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "piglit-util.h"
+
+/**
+ * @file negative-readpixels.c
+ *
+ * From the EXT_framebuffer_multisample spec:
+ *
+ *     "The error INVALID_OPERATION_EXT is generated if ReadPixels,
+ *      CopyPixels, CopyTex{Sub}Image*, CopyColor{Sub}Table, or
+ *      CopyConvolutionFilter* is called while
+ *      READ_FRAMEBUFFER_BINDING_EXT is non-zero, the read framebuffer
+ *      is framebuffer complete, and the value of SAMPLE_BUFFERS for
+ *      the read framebuffer is greater than zero."
+ */
+
+int piglit_width = 32;
+int piglit_height = 32;
+int piglit_window_mode = GLUT_DOUBLE | GLUT_RGB | GLUT_ALPHA;
+
+enum piglit_result
+piglit_display(void)
+{
+	/* UNREACHED */
+	return PIGLIT_FAIL;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLint max_samples;
+	GLuint rb, fb;
+	GLenum status;
+	bool pass = true;
+	float data[4];
+
+	piglit_require_extension("GL_EXT_framebuffer_multisample");
+
+	glGetIntegerv(GL_MAX_SAMPLES, &max_samples);
+
+	glGenFramebuffersEXT(1, &fb);
+	glBindFramebufferEXT(GL_FRAMEBUFFER, fb);
+
+	glGenRenderbuffersEXT(1, &rb);
+	glBindRenderbufferEXT(GL_RENDERBUFFER, rb);
+	glRenderbufferStorageMultisampleEXT(GL_RENDERBUFFER, max_samples,
+					    GL_RGBA, 1, 1);
+
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+				     GL_RENDERBUFFER, rb);
+
+	glDrawBuffer(GL_COLOR_ATTACHMENT0);
+	glReadBuffer(GL_COLOR_ATTACHMENT0);
+
+	status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER);
+	if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+		fprintf(stderr, "FBO incomplete\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	/* Finally, the actual test! */
+	glReadPixels(0, 0, 1, 1, GL_RGBA, GL_FLOAT, data);
+	if (!piglit_check_gl_error(GL_INVALID_OPERATION))
+		piglit_report_result(PIGLIT_FAIL);
+
+	glDeleteRenderbuffersEXT(1, &rb);
+	glDeleteFramebuffersEXT(1, &fb);
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+}
