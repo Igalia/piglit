ad09e9e9a1c1e5b277d57ad934a483077cc231e7
Eric Anholt <eric@anholt.net>
2012-01-07 00:55:15 -0800

    EXT_framebuffer_multisample/negative-copypixels: New test for spec behavior.
    v2: Leave testing SAMPLE_BUFFERS to a separate testcase.  Fix the
        error expected to be the spec text, not the typo in the Errors
        section of the spec.

---
+++ tests/spec/ext_framebuffer_multisample/negative-copypixels.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "piglit-util.h"
+
+/**
+ * @file negative-copypixels.c
+ *
+ * From the EXT_framebuffer_multisample spec:
+ *
+ *     "Finally, the behavior of several GL operations is specified
+ *      "as if the arguments were passed to CopyPixels."  These
+ *      operations include: CopyTex{Sub}Image*, CopyColor{Sub}Table,
+ *      and CopyConvolutionFilter*.  INVALID_FRAMEBUFFER_OPERATION_EXT
+ *      will be generated if an attempt is made to execute one of
+ *      these operations, or CopyPixels, while the object bound to
+ *      READ_FRAMEBUFFER_BINDING_EXT (section 4.4) is not "framebuffer
+ *      complete" (as defined in section 4.4.4.2).  INVALID_OPERATION
+ *      will be generated if the object bound to
+ *      READ_FRAMEBUFFER_BINDING_EXT is "framebuffer complete" and the
+ *      value of SAMPLE_BUFFERS is greater than zero."
+ *
+ * The Errors section says that these and ReadPixels report
+ * "INVALID_OPERATION_EXT", but that appears to be a typo.
+ */
+
+int piglit_width = 32;
+int piglit_height = 32;
+int piglit_window_mode = GLUT_DOUBLE | GLUT_RGB | GLUT_ALPHA;
+
+enum piglit_result
+piglit_display(void)
+{
+	/* UNREACHED */
+	return PIGLIT_FAIL;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLint max_samples;
+	GLuint rb, fb;
+	GLenum status;
+	bool pass = true;
+
+	piglit_require_extension("GL_EXT_framebuffer_multisample");
+
+	glGetIntegerv(GL_MAX_SAMPLES, &max_samples);
+
+	glGenFramebuffersEXT(1, &fb);
+	glBindFramebufferEXT(GL_FRAMEBUFFER, fb);
+
+	glGenRenderbuffersEXT(1, &rb);
+	glBindRenderbufferEXT(GL_RENDERBUFFER, rb);
+	glRenderbufferStorageMultisampleEXT(GL_RENDERBUFFER, max_samples,
+					    GL_RGBA, 1, 1);
+
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+				     GL_RENDERBUFFER, rb);
+
+	glDrawBuffer(GL_COLOR_ATTACHMENT0);
+	glReadBuffer(GL_COLOR_ATTACHMENT0);
+
+	status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER);
+	if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+		fprintf(stderr, "FBO incomplete\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	/* Finally, the actual test! */
+	glCopyPixels(0, 0, 1, 1, GL_COLOR);
+	if (!piglit_check_gl_error(GL_INVALID_FRAMEBUFFER_OPERATION))
+		piglit_report_result(PIGLIT_FAIL);
+
+	glDeleteRenderbuffersEXT(1, &rb);
+	glDeleteFramebuffersEXT(1, &fb);
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+}
