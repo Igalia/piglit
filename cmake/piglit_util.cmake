e505634142085823cc97801929a8262f990dea1b
Chad Versace <chad.versace@linux.intel.com>
2012-02-21 13:02:14 -0800

    cmake: Move utility functions into piglit_util.cmake
    Currently there is only one utility function, piglit_include_target_api.
    However, subsequent commits will add several more.
    
    I'm choosing to define all the utility functions in their own file,
    piglit_util.cmake, in order to keep things organized and the top
    CMakeLists uncluttered.
    
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

--- cmake/piglit_util.cmake
+++ cmake/piglit_util.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 Intel Corporation
+# Copyright 2012 Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -18,11 +18,32 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
-#
-# Authors:
-#     Chad Versace <chad.versace@intel.com>
 
-add_subdirectory(gl)
-add_subdirectory(gles1)
-add_subdirectory(gles2)
-add_subdirectory(no_api)
+# function piglit_include_target_api
+#
+# If the file CMakeLists.${piglit_target_api}.txt exists in the current source
+# directory, then include it.
+#
+function(piglit_include_target_api)
+
+    # Verify that the value of `piglit_target_api` is valid.
+    set(valid_api FALSE)
+
+    foreach(api "gl" "gles1" "gles2" "no_api")
+        if(piglit_target_api STREQUAL ${api})
+            set(valid_api TRUE)
+            break()
+        endif(piglit_target_api STREQUAL ${api})
+    endforeach(api)
+
+    if(NOT valid_api)
+        message(FATAL_ERROR "Invalid value for piglit_target_api: ${piglit_target_api}")
+    endif(NOT valid_api)
+
+    # Include CMakeLists.${piglit_target_api}.txt` if it exists.
+    set(api_file ${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.${piglit_target_api}.txt)
+    if(EXISTS ${api_file})
+        include(${api_file})
+    endif(EXISTS ${api_file})
+
+endfunction(piglit_include_target_api)
