Igalia/piglit

commit 4fbe147b5817b2ba0fe44fe9db96a2d05288aeea

framework: Fix filtering of excluded tests.
del dict[key] will raise a KeyError if the key isn't actually in the
dictionary.  This can happen if, say, a test didn't match the -t option
and subsequently matched a -x option.

Also, deleting dictionary items while iterating over them is apparently
not safe in Python 3, so we may as well just create a new dictionary
with only the entries we want.

This is simpler anyway.

Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>

framework/core.py
@@ -520,15 +520,12 @@ class TestProfile:
 		def matches_any_regexp(x, re_list):
 			return True in map(lambda r: r.search(x) != None, re_list)
 
-		# Filter out unwanted tests
-		for path in self.test_list.keys():
-			# Exclude tests that don't match any of the `filter' regexps (-t options)
-			if env.filter and not matches_any_regexp(path, env.filter):
-				del self.test_list[path]
+		def test_matches((path, test)):
+			return (matches_any_regexp(path, env.filter) and
+			        not matches_any_regexp(path, env.exclude_filter))
 
-			# Exclude tests that match an `exclude_filter' regexp (-x options)
-			if env.exclude_filter and matches_any_regexp(path, env.exclude_filter):
-				del self.test_list[path]
+		# Filter out unwanted tests
+		self.test_list = dict(filter(test_matches, self.test_list.items()))
 
 		# Queue up all the concurrent tests, so the pool is filled
 		# at the start of the test run.

commit 1ed2bf185c2992652b2694980fcf01c7f18102c1

framework: Fix lack-of-filtering of included tests.
Commit 4fbe147b5817b2ba0fe44fe9db96a2d05288aee introduced a regression
where not specifying a -t option would result in all tests being
excluded.

Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>

framework/core.py
@@ -521,7 +521,7 @@ class TestProfile:
 			return True in map(lambda r: r.search(x) != None, re_list)
 
 		def test_matches((path, test)):
-			return (matches_any_regexp(path, env.filter) and
+			return ((not env.filter or matches_any_regexp(path, env.filter)) and
 			        not matches_any_regexp(path, env.exclude_filter))
 
 		# Filter out unwanted tests