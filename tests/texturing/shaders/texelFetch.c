01f6bb7ceedafa4fc662b5448b4ac83ab985f94b
Kenneth Graunke <kenneth@whitecape.org>
2012-03-20 12:47:48 -0700

    Use nearest filtering in the texelFetch() and textureSize() tests.
    According to the OpenGL 3.0 specification, page 214, textures with
    internal integer format must use NEAREST for the mag filter and either
    NEAREST or NEAREST_MIPMAP_NEAREST for the min filter.  Otherwise, the
    texture should be incomplete.
    
    Both texelFetch() and textureSize() ignores the filter modes, so there's
    no harm in switching to nearest.  Obviously we need the texture to be
    complete.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- tests/texturing/shaders/texelFetch.c
+++ tests/texturing/shaders/texelFetch.c
@@ -245,8 +245,8 @@ generate_texture()
 
 	glGenTextures(1, &tex);
 	glBindTexture(target, tex);
-	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	if (swizzling)
