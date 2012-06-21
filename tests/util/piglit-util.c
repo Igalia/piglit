34ef043d488ce58b68971422b18f042e674f149d
Blaž Tomažič <blaz.tomazic@gmail.com>
2012-06-21 20:09:37 +0200

    util: Move file reading function to piglit-util
    Move function for reading files from shader-load.c to piglit-util.c.
    Delete shader-load.c because it had no other functionality.
    
    Signed-off-by: Blaž Tomažič <blaz.tomazic@gmail.com>
    Reviewed-by: Chad Versace <chad.versace@linux.intel.com>

--- tests/util/piglit-util.c
+++ tests/util/piglit-util.c
@@ -22,6 +22,7 @@
  */
 
 #if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #endif
 
@@ -37,6 +38,15 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #define USE_SETRLIMIT
+#endif
+
+#if defined(HAVE_FCNTL_H) && defined(HAVE_SYS_STAT_H) && defined(HAVE_SYS_TYPES_H) && defined(HAVE_UNISTD_H)
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+#else
+# define USE_STDIO
 #endif
 
 #include "piglit-util.h"
@@ -221,3 +231,106 @@ piglit_merge_result(enum piglit_result *all, enum piglit_result subtest)
 	}
 }
 
+char *piglit_load_text_file(const char *file_name, unsigned *size)
+{
+	char *text = NULL;
+
+#if defined(USE_STDIO)
+	FILE *fp;
+
+# ifdef HAVE_FOPEN_S
+	errno_t err;
+
+	if (file_name == NULL) {
+		return NULL;
+	}
+
+	err = fopen_s(&fp, file_name, "r");
+
+	if (err || (fp == NULL)) {
+		return NULL;
+	}
+# else
+	fp = fopen(file_name, "r");
+	if (fp == NULL) {
+		return NULL;
+	}
+# endif
+
+	if (fseek(fp, 0, SEEK_END) == 0) {
+		size_t len = (size_t) ftell(fp);
+		rewind(fp);
+
+		text = malloc(len + 1);
+		if (text != NULL) {
+			size_t total_read = 0;
+
+			do {
+				size_t bytes = fread(text + total_read, 1,
+						     len - total_read, fp);
+
+				total_read += bytes;
+				if (feof(fp)) {
+					break;
+				}
+
+				if (ferror(fp)) {
+					free(text);
+					text = NULL;
+					break;
+				}
+			} while (total_read < len);
+
+			if (text != NULL) {
+				text[total_read] = '\0';
+			}
+
+			if (size != NULL) {
+				*size = total_read;
+			}
+		}
+	}
+
+	fclose(fp);
+	return text;
+#else
+	struct stat st;
+	int fd = open(file_name, O_RDONLY);
+
+	if (fd < 0) {
+		return NULL;
+	}
+
+	if (fstat(fd, & st) == 0) {
+		ssize_t total_read = 0;
+
+		text = malloc(st.st_size + 1);
+		if (text != NULL) {
+			do {
+				ssize_t bytes = read(fd, text + total_read,
+						     st.st_size - total_read);
+				if (bytes < 0) {
+					free(text);
+					text = NULL;
+					break;
+				}
+
+				if (bytes == 0) {
+					break;
+				}
+
+				total_read += bytes;
+			} while (total_read < st.st_size);
+
+			text[total_read] = '\0';
+			if (size != NULL) {
+				*size = total_read;
+			}
+		}
+	}
+
+	close(fd);
+
+	return text;
+#endif
+}
