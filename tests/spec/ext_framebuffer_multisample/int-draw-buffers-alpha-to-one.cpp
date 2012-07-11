9083a6e0aba1615b16552f5b72979b068f58a106
Anuj Phogat <anuj.phogat@gmail.com>
2012-07-11 15:16:20 -0700

    msaa: Add test to verify sample-alpha-to-one with integer draw buffers
    When draw buffer zero is bound to an integer format buffer,`
    GL_SAMPLE_ALPHA_TO_ONE should have no effect. This should work properly
    even if there are other draw buffers that are not in integer format.
    
    Note: Test passes on Sandybridge with following patch applied on mesa.
    Patch is under review on mesa-dev mailing list:
    [PATCH] i965/msaa: Disable alpha-to-{coverage, one} when drawbuffer zero
    is in integer format
    
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

---
+++ tests/spec/ext_framebuffer_multisample/int-draw-buffers-alpha-to-one.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright © 2012 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "draw-buffers-common.h"
+
+/**
+ * \file int-draw-buffers-alpha-to-one.cpp
+ *
+ * Verify sample alpha to one with integer draw buffers.
+ *
+ * When draw buffer zero is bound to an integer format buffer,
+ * GL_SAMPLE_ALPHA_TO_ONE should have no effect. This should work properly
+ * even if there are other draw buffers that are not in integer format.
+ *
+ * This test operates by drawing a pattern in multisample FBO to generate
+ * reference and test images for all the draw buffers. Reference images are drawn
+ * to right half of window system draw buffer and test images to left half.
+ *
+ * Compare the left and right halves of window system frame buffer to verify
+ * the test image.
+ *
+ * Author: Anuj Phogat <anuj.phogat@gmail.com>
+ */
+
+PIGLIT_GL_TEST_MAIN(512 /*window_width*/,
+		    768 /*window_height*/,
+		    GLUT_DOUBLE | GLUT_RGBA | GLUT_ALPHA)
+
+/* At present fragment shader supports only fixed number of attachments (3) */
+static int num_attachments = 3;
+static int samples;
+
+void
+print_usage_and_exit(char *prog_name)
+{
+        printf("Usage: %s <num_samples>\n", prog_name);
+	piglit_report_result(PIGLIT_FAIL);
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+
+	if (argc < 2)
+		print_usage_and_exit(argv[0]);
+	{
+		char *endptr = NULL;
+		samples = strtol(argv[1], &endptr, 0);
+		if (endptr != argv[1] + strlen(argv[1]))
+			print_usage_and_exit(argv[0]);
+	}
+
+	piglit_require_gl_version(30);
+
+	int pattern_width = piglit_width / 2;
+	int pattern_height = piglit_height / num_attachments;
+
+	piglit_ortho_projection(pattern_width,
+				pattern_height,
+				GL_TRUE);
+	/* Skip the test if samples > GL_MAX_SAMPLES */
+	GLint max_samples;
+	glGetIntegerv(GL_MAX_SAMPLES, &max_samples);
+
+	if (samples > max_samples)
+		piglit_report_result(PIGLIT_SKIP);
+	/* Testing of integer formats other than GL_RGBA8I is not supported
+	 * by utility functions in draw-buffers-common.cpp.
+	 */
+	ms_fbo_and_draw_buffers_setup(samples,
+				      pattern_width,
+				      pattern_height,
+				      num_attachments,
+				      GL_COLOR_BUFFER_BIT,
+				      GL_RGBA8I);
+	shader_compile();
+}
+
+enum piglit_result
+piglit_display()
+{
+	bool pass = true;
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	allocate_data_arrays();
+
+	draw_reference_image(false /* sample_alpha_to_coverage */,
+			     true /* sample_alpha_to_one */);
+
+	draw_test_image(false /* sample_alpha_to_coverage */,
+			true /* sample_alpha_to_one */);
+
+	pass = piglit_check_gl_error(GL_NO_ERROR) && pass;
+
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+        pass = piglit_probe_rect_halves_equal_rgba(0, 0,
+						   piglit_width,
+						   piglit_height)
+	       && pass;
+
+	/* Free the memory allocated for data arrays */
+	free_data_arrays();
+
+	if (!piglit_automatic)
+		piglit_present_results();
+
+	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+}
