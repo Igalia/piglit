4e123c757d2e30e8c8eadc4a4ba40295fdf7b466
Anuj Phogat <anuj.phogat@gmail.com>
2012-07-23 12:18:12 -0700

    msaa: Add test to verify alpha-to-coverage when nothing is bound to draw buffer zero
    When nothing is bound to draw buffer zero, GL_SAMPLE_ALPHA_TO_COVERAGE still needs
    to work properly.
    
    Note: Test case passes using NVIDIA's proprietary linux drivers but fails on Mesa
    i965 drivers.
    
    Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

---
+++ tests/spec/ext_framebuffer_multisample/alpha-to-coverage-no-draw-buffer-zero.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright © 2012 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "draw-buffers-common.h"
+
+/**
+ * \file alpha-to-coverage-no-draw-buffer-zero.cpp
+ *
+ * Verify sample alpha to coverage with multiple draw buffers when nothing
+ * is bound to draw buffer zero.
+ *
+ * When nothing is bound to draw buffer zero, GL_SAMPLE_ALPHA_TO_COVERAGE
+ * still needs to work properly for rest of the draw buffers. The alpha value
+ * used to determine coverage should come from draw buffer zero.
+ *
+ * This test operates by drawing a pattern in multisample FBO to generate
+ * reference and test images for all the draw buffers. Reference images are
+ * drawn to right half of window system draw buffer and test images to left
+ * half.
+ *
+ * Compute the expected color values for all the draw buffers.
+ *
+ * Probe all the draw buffers blitted to downsampled FBO (resolve_fbo) and
+ * compare against expected color values.
+ *
+ * Author: Anuj Phogat <anuj.phogat@gmail.com>
+ */
+
+PIGLIT_GL_TEST_MAIN(512 /*window_width*/,
+		    768 /*window_height*/,
+		    GLUT_DOUBLE | GLUT_RGBA | GLUT_ALPHA)
+
+void
+print_usage_and_exit(char *prog_name)
+{
+        printf("Usage: %s <num_samples>\n", prog_name);
+	piglit_report_result(PIGLIT_FAIL);
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	int samples;
+	/* At present fragment shader supports only fixed number of
+	 * attachments (3)
+	 */
+	int num_attachments = 3;
+
+	if (argc < 2)
+		print_usage_and_exit(argv[0]);
+	{
+		char *endptr = NULL;
+		samples = strtol(argv[1], &endptr, 0);
+		if (endptr != argv[1] + strlen(argv[1]))
+			print_usage_and_exit(argv[0]);
+	}
+
+	piglit_require_gl_version(30);
+
+	int pattern_width = piglit_width / 2;
+	int pattern_height = piglit_height / num_attachments;
+
+	piglit_ortho_projection(pattern_width,
+				pattern_height,
+				GL_TRUE);
+
+	/* Skip the test if samples > GL_MAX_SAMPLES */
+	GLint max_samples;
+	glGetIntegerv(GL_MAX_SAMPLES, &max_samples);
+
+	if (samples > max_samples)
+		piglit_report_result(PIGLIT_SKIP);
+
+	ms_fbo_and_draw_buffers_setup(samples,
+				      pattern_width,
+				      pattern_height,
+				      num_attachments,
+				      GL_COLOR_BUFFER_BIT,
+				      GL_NONE /* color_buffer_zero_format */);
+	shader_compile();
+}
+
+enum piglit_result
+piglit_display()
+{
+	bool pass = true;
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	allocate_data_arrays();
+
+	/* Reference image drawn when sample_alpha_to_coverage is enabled,
+	 * doesn't represent an expected image. Reference image is drawn only
+	 * to visualize the image difference caused by enabling
+	 * sample_alpha_to_coverage
+	 */
+	draw_reference_image(true /* sample_alpha_to_coverage */,
+			     false /* sample_alpha_to_one */);
+
+	draw_test_image(true /* sample_alpha_to_coverage */,
+			false /* sample_alpha_to_one */);
+
+	pass = piglit_check_gl_error(GL_NO_ERROR) && pass;
+
+	/* Probe test images of all the draw_buffers blitted to resolve fbo
+	 * and compare with expected color values.  This method of verification
+	 * is appropriate for tests with sample-alpha-to-coverage enabled.
+	 * Possibility of dithering effect when the coverage value is not a
+	 * strict multiple of 1 / num_samples makes image compare (test /
+	 * reference image) unsuitable for this test.
+	 */
+	pass = probe_framebuffer_color() && pass;
+
+	/* Free the memory allocated for data arrays */
+	free_data_arrays();
+
+	if (!piglit_automatic)
+		piglit_present_results();
+
+	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+}
