165735b89d099f4ea4b635fc13b49c36e00bac6d
Matt Turner <mattst88@gmail.com>
2012-07-31 17:33:08 -0700

    egl_khr_create_context: Verify that the forward-compatible flag can be accepted for Desktop GL
    v2: - Ask for EGL_OPENGL_BIT and skip if not available.
        - Only check EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR if GL
          3.2+
    v3: - Only check EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR if GL
          3.0+
        - Skip if GL < 3.0.

---
+++ tests/egl/spec/egl_khr_create_context/valid-flag-forward-compatible-gl.c
@@ -0,0 +1,83 @@
+/* Copyright © 2012 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#include "piglit-util-gl-common.h"
+#include "common.h"
+
+int gl_version;
+
+static bool try_flag(int flag)
+{
+	const EGLint attribs[] = {
+		EGL_CONTEXT_FLAGS_KHR, flag,
+		EGL_NONE
+	};
+
+	ctx = eglCreateContext(egl_dpy, cfg, EGL_NO_CONTEXT, attribs);
+	if (ctx != NULL) {
+		/* Get GL version in order to know whether we can test
+		 * EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR.
+		 */
+		if (flag == 0) {
+			if (!eglMakeCurrent(egl_dpy, EGL_NO_SURFACE, EGL_NO_SURFACE, ctx)) {
+				fprintf(stderr, "eglMakeCurrent() failed\n");
+				piglit_report_result(PIGLIT_FAIL);
+			}
+
+			piglit_dispatch_default_init();
+
+			gl_version = piglit_get_gl_version();
+		}
+		eglDestroyContext(egl_dpy, ctx);
+		return true;
+	}
+
+	fprintf(stderr, "Failed to create context with flag %d\n", flag);
+	return false;
+}
+
+int main(int argc, char **argv)
+{
+	bool pass = true;
+
+	if (!EGL_KHR_create_context_setup(EGL_OPENGL_BIT)) {
+		fprintf(stderr, "Desktop GL not available.\n");
+		piglit_report_result(PIGLIT_SKIP);
+	}
+	eglBindAPI(EGL_OPENGL_API);
+
+	/* The EGL_KHR_create_context spec says:
+	 *
+	 *    "The default value of EGL_CONTEXT_FLAGS_KHR is zero."
+	 */
+	pass = pass && try_flag(0);
+	if (gl_version >= 30) {
+		pass = pass && try_flag(EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR);
+	} else {
+		piglit_report_result(PIGLIT_SKIP);
+	}
+
+	EGL_KHR_create_context_teardown();
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+
+	return EXIT_SUCCESS;
+}
