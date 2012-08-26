875b510417ab2010409d2396ab25104b4ad4b3d0
Chris Forbes <chrisf@ijw.co.nz>
2012-08-27 10:07:40 +1200

    convert version check to piglit_require_gl_version
    Reviewed-by: Chad Versace <chad.versace@linux.intel.com>

--- tests/shaders/glsl-arb-fragment-coord-conventions.c
+++ tests/shaders/glsl-arb-fragment-coord-conventions.c
@@ -219,10 +219,7 @@ static void key_func(unsigned char key, int x, int y)
 
 void piglit_init(int argc, char **argv)
 {
-	if (piglit_get_gl_version() < 20) {
-		printf("Requires OpenGL 2.0\n");
-		piglit_report_result(PIGLIT_SKIP);
-	}
+	piglit_require_gl_version(20);
 
 	piglit_require_extension("GL_ARB_fragment_coord_conventions");
 
