9ffd30f4180e827b0a20641ebcfba50f595c57ef
Blaž Tomažič <blaz.tomazic@gmail.com>
2012-08-29 22:18:22 +0200

    cmake: Build OpenCL utilities and framework
    Add BUILD_CL_TESTS option to cmake configuration and build OpenCL
    utilities and framework to library piglitutil_cl.
    
    Signed-off-by: Blaž Tomažič <blaz.tomazic@gmail.com>

--- cmake/Modules/FindOpenCL.cmake
+++ cmake/Modules/FindOpenCL.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2010-2012 Intel Corporation
+# Copyright © 2012 Blaž Tomažič <blaz.tomazic@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -16,23 +16,24 @@
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 
-# The no_api directory must precede the GL directories because it builds
-# glut_waffle, which the GL targets depend on.
-add_subdirectory(no_api)
+# This module defines the following variables:
+#
+#   OPENCL_FOUND
+#       True if OpenCL is installed.
+#
+#   OPENCL_INCLUDE_PATH
+#
+#   OPENCL_opencl_LIBRARY
+#       Path to OpenCL's library.
 
-add_subdirectory(gl)
+find_path(OPENCL_INCLUDE_PATH CL/opencl.h)
+find_library(OPENCL_opencl_LIBRARY OpenCL)
 
-if(BUILD_GLES1_TESTS)
-	add_subdirectory(gles1)
-endif(BUILD_GLES1_TESTS)
-
-if(BUILD_GLES2_TESTS)
-	add_subdirectory(gles2)
-endif(BUILD_GLES2_TESTS)
-
-if(BUILD_CL_TESTS)
-	add_subdirectory(cl)
-endif(BUILD_CL_TESTS)
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OpenCL
+    DEFAULT_MSG
+    OPENCL_opencl_LIBRARY OPENCL_INCLUDE_PATH
+    )
