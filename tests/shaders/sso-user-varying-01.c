e4b8494d113cb53f2696d802147046d480272576
Brian Paul <brianp@vmware.com>
2012-11-02 13:52:25 -0600

    shaders: remove config.window_width/height = 100 lines
    Reviewed-by: Chad Versace <chad.versace@linux.intel.com>

--- tests/shaders/sso-user-varying-01.c
+++ tests/shaders/sso-user-varying-01.c
@@ -33,8 +33,6 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.supports_gl_compat_version = 10;
 
-	config.window_width = 30;
-	config.window_height = 30;
 	config.window_visual = PIGLIT_GL_VISUAL_RGB | PIGLIT_GL_VISUAL_DOUBLE;
 
 PIGLIT_GL_TEST_CONFIG_END
