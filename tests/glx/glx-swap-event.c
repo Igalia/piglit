0ab10d39051cbd47b15658f1feb2c4c3b52fef6d
Marek Olšák <maraeo@gmail.com>
2012-11-23 19:55:48 +0100

    glx-swap-events: initialize GL dispatch
    Fixes a crash.

--- tests/glx/glx-swap-event.c
+++ tests/glx/glx-swap-event.c
@@ -533,6 +533,7 @@ main(int argc, char *argv[])
             piglit_report_result(PIGLIT_FAIL);
         }
     }
+    piglit_dispatch_default_init();
     event_loop(dpy, glxWin);
     
     glXDestroyContext(dpy, ctx);
