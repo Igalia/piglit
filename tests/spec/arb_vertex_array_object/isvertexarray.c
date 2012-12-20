92da9c3413dffa2e88d5e7518f15327f21577ab0
Matt Turner <mattst88@gmail.com>
2012-12-20 13:04:11 -0800

    arb_vertex_array_object: Add IsVertexArray() test

---
+++ tests/spec/arb_vertex_array_object/isvertexarray.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright © 2012 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "piglit-util-gl-common.h"
+
+/**
+ * @file isvertexarray.c
+ *
+ * Tests basic API support for glIsVertexArray().
+ *
+ * From the ARB_vertex_array_object spec:
+ *
+ *     "The command
+ *
+ *         void GenVertexArrays(sizei n, uint *arrays);
+ *
+ *      returns <n> previous unused vertex array object names in <arrays>. These
+ *      names are marked as used, for the purposes of GenVertexArrays only, but
+ *      they acquire array state only when they are first bound.
+ *
+ *      [...]
+ *
+ *      A vertex array object is created by binding a name returned by
+ *      GenVertexArrays with the command
+ *
+ *         void BindVertexArray(uint array);"
+ */
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+	config.supports_gl_compat_version = 10;
+
+	config.window_width = 32;
+	config.window_height = 32;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+enum piglit_result
+piglit_display(void)
+{
+	/* UNREACHED */
+	return PIGLIT_FAIL;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLuint id;
+
+	piglit_require_gl_version(15);
+	piglit_require_extension("GL_ARB_vertex_array_object");
+
+	glGenVertexArrays(1, &id);
+
+	if (glIsVertexArray(id)) {
+		fprintf(stderr, "id recognized incorrectly as a vertex array object.\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	glBindVertexArray(id);
+
+	if (!glIsVertexArray(id)) {
+		fprintf(stderr, "id not recognized correctly as a vertex array object.\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	piglit_report_result(PIGLIT_PASS);
+}
