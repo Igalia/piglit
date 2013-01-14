b34d8763d7414cde59d8cc64b1f6fb0d393e9d5f
Eric Anholt <eric@anholt.net>
2013-01-14 09:17:52 -0800

    fbo-blending-formats: Report subtest results.
    v2: Leave previous printfs in place, get the name for the enum.
    
    Reviewed-by: Carl Worth <cworth@cworth.org>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- tests/fbo/fbo-blending-formats.c
+++ tests/fbo/fbo-blending-formats.c
@@ -266,7 +266,9 @@ static enum piglit_result test_format(const struct format_desc *format)
 	status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
 	printf("Testing %s", format->name);
 	if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
-		printf(" - fbo incomplete (status = 0x%04x)\n", status);
+		printf(" - fbo incomplete (status = %s)\n",
+		       piglit_get_gl_enum_name(status));
+		piglit_report_subtest_result(PIGLIT_SKIP, format->name);
 		return PIGLIT_SKIP;
 	}
         printf("\n");
@@ -327,6 +329,7 @@ static enum piglit_result test_format(const struct format_desc *format)
 
 	if (!pass) {
 		piglit_present_results();
+		piglit_report_subtest_result(PIGLIT_FAIL, format->name);
 		return PIGLIT_FAIL;
 	}
 
@@ -377,6 +380,9 @@ static enum piglit_result test_format(const struct format_desc *format)
 	}
 
 	piglit_present_results();
+
+	piglit_report_subtest_result(pass ? PIGLIT_PASS : PIGLIT_FAIL,
+				     format->name);
 
 	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
 }
