cff9108fb5bc95039c9a96c791f92d0941bfd454
Eric Anholt <eric@anholt.net>
2013-01-14 09:28:33 -0800

    fbo-storage-*: Reindent to normal piglit style.
    Reviewed-by: Carl Worth <cworth@cworth.org>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- tests/fbo/fbo-storage-completeness.c
+++ tests/fbo/fbo-storage-completeness.c
@@ -35,9 +35,9 @@
 
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
-	config.supports_gl_compat_version = 10;
+config.supports_gl_compat_version = 10;
 
-	config.window_visual = PIGLIT_GL_VISUAL_RGB;
+config.window_visual = PIGLIT_GL_VISUAL_RGB;
 
 PIGLIT_GL_TEST_CONFIG_END
 
@@ -47,12 +47,12 @@ static const char *TestName = "fbo-storage-formats";
 static void
 check_error(int line)
 {
-   GLenum err = glGetError();
-   if (err) {
-      printf("%s: Unexpected error 0x%x at line %d\n",
-              TestName, err, line);
-      piglit_report_result(PIGLIT_FAIL);
-   }
+	GLenum err = glGetError();
+	if (err) {
+		printf("%s: Unexpected error 0x%x at line %d\n",
+		       TestName, err, line);
+		piglit_report_result(PIGLIT_FAIL);
+	}
 }
 
 
@@ -67,182 +67,190 @@ static GLboolean HaveExtension[MAX_EXT];
 
 struct format_info
 {
-   GLenum format;
-   GLuint extension;
+	GLenum format;
+	GLuint extension;
 };
 
 
 static const struct format_info Formats[] = {
-   { GL_RGB, 0 },
-   { GL_R3_G3_B2, 0 },
-   { GL_RGB4, 0 },
-   { GL_RGB5, 0 },
-   { GL_RGB8, 0 },
-   { GL_RGB10, 0 },
-   { GL_RGB12, 0 },
-   { GL_RGB16, 0 },
-   { GL_RGBA, 0 },
-   { GL_RGBA2, 0 },
-   { GL_RGBA4, 0 },
-   { GL_RGB5_A1, 0 },
-   { GL_RGBA8, 0 },
-   { GL_RGB10_A2, 0 },
-   { GL_RGBA12, 0 },
-   { GL_RGBA16, 0 },
-   { GL_STENCIL_INDEX, 0 },
-   { GL_STENCIL_INDEX1_EXT, 0 },
-   { GL_STENCIL_INDEX4_EXT, 0 },
-   { GL_STENCIL_INDEX8_EXT, 0 },
-   { GL_STENCIL_INDEX16_EXT, 0 },
-   { GL_DEPTH_COMPONENT, 0 },
-   { GL_DEPTH_COMPONENT16, 0 },
-   { GL_DEPTH_COMPONENT24, 0 },
-   { GL_DEPTH_COMPONENT32, 0 },
+	{ GL_RGB, 0 },
+	{ GL_R3_G3_B2, 0 },
+	{ GL_RGB4, 0 },
+	{ GL_RGB5, 0 },
+	{ GL_RGB8, 0 },
+	{ GL_RGB10, 0 },
+	{ GL_RGB12, 0 },
+	{ GL_RGB16, 0 },
+	{ GL_RGBA, 0 },
+	{ GL_RGBA2, 0 },
+	{ GL_RGBA4, 0 },
+	{ GL_RGB5_A1, 0 },
+	{ GL_RGBA8, 0 },
+	{ GL_RGB10_A2, 0 },
+	{ GL_RGBA12, 0 },
+	{ GL_RGBA16, 0 },
+	{ GL_STENCIL_INDEX, 0 },
+	{ GL_STENCIL_INDEX1_EXT, 0 },
+	{ GL_STENCIL_INDEX4_EXT, 0 },
+	{ GL_STENCIL_INDEX8_EXT, 0 },
+	{ GL_STENCIL_INDEX16_EXT, 0 },
+	{ GL_DEPTH_COMPONENT, 0 },
+	{ GL_DEPTH_COMPONENT16, 0 },
+	{ GL_DEPTH_COMPONENT24, 0 },
+	{ GL_DEPTH_COMPONENT32, 0 },
 
-   /* GL_ARB_framebuffer_object additions */
-   { GL_ALPHA, ARB_framebuffer_object },
-   { GL_ALPHA4, ARB_framebuffer_object },
-   { GL_ALPHA8, ARB_framebuffer_object },
-   { GL_ALPHA12, ARB_framebuffer_object },
-   { GL_ALPHA16, ARB_framebuffer_object },
-   { GL_LUMINANCE_ALPHA, ARB_framebuffer_object },
-   { GL_LUMINANCE, ARB_framebuffer_object },
-   { GL_INTENSITY, ARB_framebuffer_object },
+	/* GL_ARB_framebuffer_object additions */
+	{ GL_ALPHA, ARB_framebuffer_object },
+	{ GL_ALPHA4, ARB_framebuffer_object },
+	{ GL_ALPHA8, ARB_framebuffer_object },
+	{ GL_ALPHA12, ARB_framebuffer_object },
+	{ GL_ALPHA16, ARB_framebuffer_object },
+	{ GL_LUMINANCE_ALPHA, ARB_framebuffer_object },
+	{ GL_LUMINANCE, ARB_framebuffer_object },
+	{ GL_INTENSITY, ARB_framebuffer_object },
 
-   /* GL_ARB_texture_rg */
-   { GL_RED, ARB_texture_rg },
-   { GL_R8, ARB_texture_rg },
-   { GL_R16, ARB_texture_rg },
-   { GL_RG, ARB_texture_rg },
-   { GL_RG8, ARB_texture_rg },
-   { GL_RG16, ARB_texture_rg },
+	/* GL_ARB_texture_rg */
+	{ GL_RED, ARB_texture_rg },
+	{ GL_R8, ARB_texture_rg },
+	{ GL_R16, ARB_texture_rg },
+	{ GL_RG, ARB_texture_rg },
+	{ GL_RG8, ARB_texture_rg },
+	{ GL_RG16, ARB_texture_rg },
 #if 0
-   /* XXX also depend on texture_float, texture_integer extensions */
-   { GL_R16F, ARB_texture_rg },
-   { GL_R32F, ARB_texture_rg },
-   { GL_RG16F, ARB_texture_rg },
-   { GL_RG32F, ARB_texture_rg },
-   { GL_R8I, ARB_texture_rg },
-   { GL_R8UI, ARB_texture_rg },
-   { GL_R16I, ARB_texture_rg },
-   { GL_R16UI, ARB_texture_rg },
-   { GL_R32I, ARB_texture_rg },
-   { GL_R32UI, ARB_texture_rg },
-   { GL_RG8I, ARB_texture_rg },
-   { GL_RG8UI, ARB_texture_rg },
-   { GL_RG16I, ARB_texture_rg },
-   { GL_RG16UI, ARB_texture_rg },
-   { GL_RG32I, ARB_texture_rg },
-   { GL_RG32UI, ARB_texture_rg },
+	/* XXX also depend on texture_float, texture_integer extensions */
+	{ GL_R16F, ARB_texture_rg },
+	{ GL_R32F, ARB_texture_rg },
+	{ GL_RG16F, ARB_texture_rg },
+	{ GL_RG32F, ARB_texture_rg },
+	{ GL_R8I, ARB_texture_rg },
+	{ GL_R8UI, ARB_texture_rg },
+	{ GL_R16I, ARB_texture_rg },
+	{ GL_R16UI, ARB_texture_rg },
+	{ GL_R32I, ARB_texture_rg },
+	{ GL_R32UI, ARB_texture_rg },
+	{ GL_RG8I, ARB_texture_rg },
+	{ GL_RG8UI, ARB_texture_rg },
+	{ GL_RG16I, ARB_texture_rg },
+	{ GL_RG16UI, ARB_texture_rg },
+	{ GL_RG32I, ARB_texture_rg },
+	{ GL_RG32UI, ARB_texture_rg },
 #endif
 
-   /* GL_EXT_packed_depth_stencil */
-   { GL_DEPTH_STENCIL_EXT, EXT_packed_depth_stencil },
-   { GL_DEPTH24_STENCIL8_EXT, EXT_packed_depth_stencil }
+	/* GL_EXT_packed_depth_stencil */
+	{ GL_DEPTH_STENCIL_EXT, EXT_packed_depth_stencil },
+	{ GL_DEPTH24_STENCIL8_EXT, EXT_packed_depth_stencil }
 };
 
 
 static enum piglit_result
 test(void)
 {
-   GLuint fbo, rb;
-   int i;
-   int incomplete = -1, complete = -1;
+	GLuint fbo, rb;
+	int i;
+	int incomplete = -1, complete = -1;
 
-   /* clear out any errors */
-   while (glGetError())
-      ;
+	/* clear out any errors */
+	while (glGetError())
+		;
 
-   /* find a format which is incomplete */
-   for (i = 0; i < ARRAY_SIZE(Formats); i++) {
-      if (!HaveExtension[Formats[i].extension])
-          continue;
+	/* find a format which is incomplete */
+	for (i = 0; i < ARRAY_SIZE(Formats); i++) {
+		if (!HaveExtension[Formats[i].extension])
+			continue;
 
-      glGenFramebuffersEXT(1, &fbo);
-      glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
-      check_error(__LINE__);
+		glGenFramebuffersEXT(1, &fbo);
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+		check_error(__LINE__);
 
-      glGenRenderbuffersEXT(1, &rb);
-      check_error(__LINE__);
-      glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, rb);
+		glGenRenderbuffersEXT(1, &rb);
+		check_error(__LINE__);
+		glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, rb);
 
-      glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT,
-                                   GL_COLOR_ATTACHMENT0,
-                                   GL_RENDERBUFFER_EXT,
-                                   rb);
-      check_error(__LINE__);
+		glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT,
+					     GL_COLOR_ATTACHMENT0,
+					     GL_RENDERBUFFER_EXT,
+					     rb);
+		check_error(__LINE__);
 
-      glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, Formats[i].format,
-                               piglit_width, piglit_height);
-      if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) == GL_FRAMEBUFFER_COMPLETE_EXT)
-         complete = i;
-      else
-         incomplete = i;
+		glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, Formats[i].format,
+					 piglit_width, piglit_height);
+		if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) ==
+		    GL_FRAMEBUFFER_COMPLETE_EXT) {
+			complete = i;
+		} else {
+			incomplete = i;
+		}
 
-      glDeleteFramebuffersEXT(1, &fbo);
-      glDeleteRenderbuffersEXT(1, &rb);
+		glDeleteFramebuffersEXT(1, &fbo);
+		glDeleteRenderbuffersEXT(1, &rb);
 
-      if (incomplete != -1 && complete != -1)
-         break;
-   }
-   if (complete == -1) {
-      printf("Found no renderbuffer format which is framebuffer complete.\n");
-      return PIGLIT_FAIL;
-   }
-   if (incomplete == -1)
-      return PIGLIT_PASS;
+		if (incomplete != -1 && complete != -1)
+			break;
+	}
+	if (complete == -1) {
+		printf("Found no renderbuffer format which is framebuffer "
+		       "complete.\n");
+		return PIGLIT_FAIL;
+	}
+	if (incomplete == -1)
+		return PIGLIT_PASS;
 
-   glGenFramebuffersEXT(1, &fbo);
-   glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
-   check_error(__LINE__);
+	glGenFramebuffersEXT(1, &fbo);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+	check_error(__LINE__);
 
-   glGenRenderbuffersEXT(1, &rb);
-   check_error(__LINE__);
-   glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, rb);
+	glGenRenderbuffersEXT(1, &rb);
+	check_error(__LINE__);
+	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, rb);
 
-   glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT,
-                                GL_COLOR_ATTACHMENT0,
-                                GL_RENDERBUFFER_EXT,
-                                rb);
-   check_error(__LINE__);
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT,
+				     GL_COLOR_ATTACHMENT0,
+				     GL_RENDERBUFFER_EXT,
+				     rb);
+	check_error(__LINE__);
 
-   glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, Formats[complete].format,
-                            piglit_width, piglit_height);
-   if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) != GL_FRAMEBUFFER_COMPLETE_EXT) {
-      printf("The format which was previously framebuffer complete is now incomplete.\n");
-      return PIGLIT_FAIL;
-   }
+	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, Formats[complete].format,
+				 piglit_width, piglit_height);
+	if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) !=
+	    GL_FRAMEBUFFER_COMPLETE_EXT) {
+		printf("The format which was previously framebuffer complete "
+		       "is now incomplete.\n");
+		return PIGLIT_FAIL;
+	}
 
-   glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, Formats[incomplete].format,
-                            piglit_width, piglit_height);
-   if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) == GL_FRAMEBUFFER_COMPLETE_EXT) {
-      printf("The format which was previously framebuffer incomplete is now complete.\n");
-      return PIGLIT_FAIL;
-   }
+	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT,
+				 Formats[incomplete].format,
+				 piglit_width, piglit_height);
+	if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) ==
+	    GL_FRAMEBUFFER_COMPLETE_EXT) {
+		printf("The format which was previously framebuffer incomplete "
+		       "is now complete.\n");
+		return PIGLIT_FAIL;
+	}
 
-   glDeleteFramebuffersEXT(1, &fbo);
-   glDeleteRenderbuffersEXT(1, &rb);
+	glDeleteFramebuffersEXT(1, &fbo);
+	glDeleteRenderbuffersEXT(1, &rb);
 
-   return PIGLIT_PASS;
+	return PIGLIT_PASS;
 }
 
 
 enum piglit_result
 piglit_display(void)
 {
-   return test();
+	return test();
 }
 
 
 void
 piglit_init(int argc, char**argv)
 {
-   piglit_require_extension("GL_EXT_framebuffer_object");
+	piglit_require_extension("GL_EXT_framebuffer_object");
 
-   HaveExtension[0] = GL_TRUE;
-   HaveExtension[EXT_packed_depth_stencil] = piglit_is_extension_supported("GL_EXT_packed_depth_stencil");
-   HaveExtension[ARB_framebuffer_object] = piglit_is_extension_supported("GL_ARB_framebuffer_object");
-   HaveExtension[ARB_texture_rg] = piglit_is_extension_supported("GL_ARB_texture_rg");
+	HaveExtension[0] = GL_TRUE;
+	HaveExtension[EXT_packed_depth_stencil] = piglit_is_extension_supported("GL_EXT_packed_depth_stencil");
+	HaveExtension[ARB_framebuffer_object] = piglit_is_extension_supported("GL_ARB_framebuffer_object");
+	HaveExtension[ARB_texture_rg] = piglit_is_extension_supported("GL_ARB_texture_rg");
 
-   piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
 }
--- tests/fbo/fbo-storage-formats.c
+++ tests/fbo/fbo-storage-formats.c
@@ -34,9 +34,9 @@
 
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
-	config.supports_gl_compat_version = 10;
+config.supports_gl_compat_version = 10;
 
-	config.window_visual = PIGLIT_GL_VISUAL_RGB;
+config.window_visual = PIGLIT_GL_VISUAL_RGB;
 
 PIGLIT_GL_TEST_CONFIG_END
 
@@ -46,12 +46,12 @@ static const char *TestName = "fbo-storage-formats";
 static void
 check_error(int line)
 {
-   GLenum err = glGetError();
-   if (err) {
-      printf("%s: Unexpected error 0x%x at line %d\n",
-              TestName, err, line);
-      piglit_report_result(PIGLIT_FAIL);
-   }
+	GLenum err = glGetError();
+	if (err) {
+		printf("%s: Unexpected error 0x%x at line %d\n",
+		       TestName, err, line);
+		piglit_report_result(PIGLIT_FAIL);
+	}
 }
 
 
@@ -66,188 +66,191 @@ static GLboolean HaveExtension[MAX_EXT];
 
 struct format_info
 {
-   GLenum format;
-   GLuint extension;
+	GLenum format;
+	GLuint extension;
 };
 
 
 static const struct format_info Formats[] = {
-   { GL_RGB, 0 },
-   { GL_R3_G3_B2, 0 },
-   { GL_RGB4, 0 },
-   { GL_RGB5, 0 },
-   { GL_RGB8, 0 },
-   { GL_RGB10, 0 },
-   { GL_RGB12, 0 },
-   { GL_RGB16, 0 },
-   { GL_RGBA, 0 },
-   { GL_RGBA2, 0 },
-   { GL_RGBA4, 0 },
-   { GL_RGB5_A1, 0 },
-   { GL_RGBA8, 0 },
-   { GL_RGB10_A2, 0 },
-   { GL_RGBA12, 0 },
-   { GL_RGBA16, 0 },
-   { GL_STENCIL_INDEX, 0 },
-   { GL_STENCIL_INDEX1_EXT, 0 },
-   { GL_STENCIL_INDEX4_EXT, 0 },
-   { GL_STENCIL_INDEX8_EXT, 0 },
-   { GL_STENCIL_INDEX16_EXT, 0 },
-   { GL_DEPTH_COMPONENT, 0 },
-   { GL_DEPTH_COMPONENT16, 0 },
-   { GL_DEPTH_COMPONENT24, 0 },
-   { GL_DEPTH_COMPONENT32, 0 },
+	{ GL_RGB, 0 },
+	{ GL_R3_G3_B2, 0 },
+	{ GL_RGB4, 0 },
+	{ GL_RGB5, 0 },
+	{ GL_RGB8, 0 },
+	{ GL_RGB10, 0 },
+	{ GL_RGB12, 0 },
+	{ GL_RGB16, 0 },
+	{ GL_RGBA, 0 },
+	{ GL_RGBA2, 0 },
+	{ GL_RGBA4, 0 },
+	{ GL_RGB5_A1, 0 },
+	{ GL_RGBA8, 0 },
+	{ GL_RGB10_A2, 0 },
+	{ GL_RGBA12, 0 },
+	{ GL_RGBA16, 0 },
+	{ GL_STENCIL_INDEX, 0 },
+	{ GL_STENCIL_INDEX1_EXT, 0 },
+	{ GL_STENCIL_INDEX4_EXT, 0 },
+	{ GL_STENCIL_INDEX8_EXT, 0 },
+	{ GL_STENCIL_INDEX16_EXT, 0 },
+	{ GL_DEPTH_COMPONENT, 0 },
+	{ GL_DEPTH_COMPONENT16, 0 },
+	{ GL_DEPTH_COMPONENT24, 0 },
+	{ GL_DEPTH_COMPONENT32, 0 },
 
-   /* GL_ARB_framebuffer_object additions */
-   { GL_ALPHA, ARB_framebuffer_object },
-   { GL_ALPHA4, ARB_framebuffer_object },
-   { GL_ALPHA8, ARB_framebuffer_object },
-   { GL_ALPHA12, ARB_framebuffer_object },
-   { GL_ALPHA16, ARB_framebuffer_object },
-   { GL_LUMINANCE_ALPHA, ARB_framebuffer_object },
-   { GL_LUMINANCE, ARB_framebuffer_object },
-   { GL_INTENSITY, ARB_framebuffer_object },
+	/* GL_ARB_framebuffer_object additions */
+	{ GL_ALPHA, ARB_framebuffer_object },
+	{ GL_ALPHA4, ARB_framebuffer_object },
+	{ GL_ALPHA8, ARB_framebuffer_object },
+	{ GL_ALPHA12, ARB_framebuffer_object },
+	{ GL_ALPHA16, ARB_framebuffer_object },
+	{ GL_LUMINANCE_ALPHA, ARB_framebuffer_object },
+	{ GL_LUMINANCE, ARB_framebuffer_object },
+	{ GL_INTENSITY, ARB_framebuffer_object },
 
-   /* GL_ARB_texture_rg */
-   { GL_RED, ARB_texture_rg },
-   { GL_R8, ARB_texture_rg },
-   { GL_R16, ARB_texture_rg },
-   { GL_RG, ARB_texture_rg },
-   { GL_RG8, ARB_texture_rg },
-   { GL_RG16, ARB_texture_rg },
+	/* GL_ARB_texture_rg */
+	{ GL_RED, ARB_texture_rg },
+	{ GL_R8, ARB_texture_rg },
+	{ GL_R16, ARB_texture_rg },
+	{ GL_RG, ARB_texture_rg },
+	{ GL_RG8, ARB_texture_rg },
+	{ GL_RG16, ARB_texture_rg },
 #if 0
-   /* XXX also depend on texture_float, texture_integer extensions */
-   { GL_R16F, ARB_texture_rg },
-   { GL_R32F, ARB_texture_rg },
-   { GL_RG16F, ARB_texture_rg },
-   { GL_RG32F, ARB_texture_rg },
-   { GL_R8I, ARB_texture_rg },
-   { GL_R8UI, ARB_texture_rg },
-   { GL_R16I, ARB_texture_rg },
-   { GL_R16UI, ARB_texture_rg },
-   { GL_R32I, ARB_texture_rg },
-   { GL_R32UI, ARB_texture_rg },
-   { GL_RG8I, ARB_texture_rg },
-   { GL_RG8UI, ARB_texture_rg },
-   { GL_RG16I, ARB_texture_rg },
-   { GL_RG16UI, ARB_texture_rg },
-   { GL_RG32I, ARB_texture_rg },
-   { GL_RG32UI, ARB_texture_rg },
+	/* XXX also depend on texture_float, texture_integer extensions */
+	{ GL_R16F, ARB_texture_rg },
+	{ GL_R32F, ARB_texture_rg },
+	{ GL_RG16F, ARB_texture_rg },
+	{ GL_RG32F, ARB_texture_rg },
+	{ GL_R8I, ARB_texture_rg },
+	{ GL_R8UI, ARB_texture_rg },
+	{ GL_R16I, ARB_texture_rg },
+	{ GL_R16UI, ARB_texture_rg },
+	{ GL_R32I, ARB_texture_rg },
+	{ GL_R32UI, ARB_texture_rg },
+	{ GL_RG8I, ARB_texture_rg },
+	{ GL_RG8UI, ARB_texture_rg },
+	{ GL_RG16I, ARB_texture_rg },
+	{ GL_RG16UI, ARB_texture_rg },
+	{ GL_RG32I, ARB_texture_rg },
+	{ GL_RG32UI, ARB_texture_rg },
 #endif
 
-   /* GL_EXT_packed_depth_stencil */
-   { GL_DEPTH_STENCIL_EXT, EXT_packed_depth_stencil },
-   { GL_DEPTH24_STENCIL8_EXT, EXT_packed_depth_stencil }
+	/* GL_EXT_packed_depth_stencil */
+	{ GL_DEPTH_STENCIL_EXT, EXT_packed_depth_stencil },
+	{ GL_DEPTH24_STENCIL8_EXT, EXT_packed_depth_stencil }
 };
 
 
 static const GLenum InvalidFormats[] = {
-   GL_COLOR_INDEX,
-   GL_COLOR_INDEX1_EXT,
-   GL_COLOR_INDEX2_EXT,
-   GL_COLOR_INDEX4_EXT,
-   GL_COLOR_INDEX8_EXT,
-   GL_COLOR_INDEX12_EXT,
-   GL_COLOR_INDEX16_EXT,
+	GL_COLOR_INDEX,
+	GL_COLOR_INDEX1_EXT,
+	GL_COLOR_INDEX2_EXT,
+	GL_COLOR_INDEX4_EXT,
+	GL_COLOR_INDEX8_EXT,
+	GL_COLOR_INDEX12_EXT,
+	GL_COLOR_INDEX16_EXT,
 
-   GL_COMPRESSED_ALPHA,
-   GL_COMPRESSED_LUMINANCE,
-   GL_COMPRESSED_LUMINANCE_ALPHA,
-   GL_COMPRESSED_INTENSITY,
-   GL_COMPRESSED_RGB,
-   GL_COMPRESSED_RGBA,
+	GL_COMPRESSED_ALPHA,
+	GL_COMPRESSED_LUMINANCE,
+	GL_COMPRESSED_LUMINANCE_ALPHA,
+	GL_COMPRESSED_INTENSITY,
+	GL_COMPRESSED_RGB,
+	GL_COMPRESSED_RGBA,
 
-   GL_COMPRESSED_RGB_S3TC_DXT1_EXT,
-   GL_COMPRESSED_RGBA_S3TC_DXT1_EXT,
-   GL_COMPRESSED_RGBA_S3TC_DXT3_EXT,
-   GL_COMPRESSED_RGBA_S3TC_DXT5_EXT,
+	GL_COMPRESSED_RGB_S3TC_DXT1_EXT,
+	GL_COMPRESSED_RGBA_S3TC_DXT1_EXT,
+	GL_COMPRESSED_RGBA_S3TC_DXT3_EXT,
+	GL_COMPRESSED_RGBA_S3TC_DXT5_EXT,
 
-   GL_COMPRESSED_RED,
-   GL_COMPRESSED_RG,
+	GL_COMPRESSED_RED,
+	GL_COMPRESSED_RG,
 
-   GL_YCBCR_MESA,
+	GL_YCBCR_MESA,
 
-   GL_DUDV_ATI,
-   GL_DU8DV8_ATI
+	GL_DUDV_ATI,
+	GL_DU8DV8_ATI
 };
 
 
 static enum piglit_result
 test(void)
 {
-   GLuint fbo, rb;
-   int i;
-   GLboolean pass = GL_TRUE;
-   GLenum err;
+	GLuint fbo, rb;
+	int i;
+	GLboolean pass = GL_TRUE;
+	GLenum err;
 
-   glGenFramebuffersEXT(1, &fbo);
-   glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
-   check_error(__LINE__);
+	glGenFramebuffersEXT(1, &fbo);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+	check_error(__LINE__);
 
-   glGenRenderbuffersEXT(1, &rb);
-   check_error(__LINE__);
-   glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, rb);
+	glGenRenderbuffersEXT(1, &rb);
+	check_error(__LINE__);
+	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, rb);
 
-   glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT,
-                                GL_COLOR_ATTACHMENT0,
-                                GL_RENDERBUFFER_EXT,
-                                rb);
-   check_error(__LINE__);
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT,
+				     GL_COLOR_ATTACHMENT0,
+				     GL_RENDERBUFFER_EXT,
+				     rb);
+	check_error(__LINE__);
 
-   /* clear out any errors */
-   while (glGetError())
-      ;
+	/* clear out any errors */
+	while (glGetError())
+		;
 
-   /* test formats that should be accepted */
-   for (i = 0; i < ARRAY_SIZE(Formats); i++) {
-      if (!HaveExtension[Formats[i].extension])
-          continue;
+	/* test formats that should be accepted */
+	for (i = 0; i < ARRAY_SIZE(Formats); i++) {
+		if (!HaveExtension[Formats[i].extension])
+			continue;
 
-      glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, Formats[i].format,
-                               piglit_width, piglit_height);
-      err = glGetError();
-      if (err) {
-         printf("%s: glRenderbufferStorage failed for format 0x%x\n",
-                TestName, Formats[i].format);
-         pass = GL_FALSE;
-      }
-      printf("0x%04X: %s\n", Formats[i].format,
-             glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) == GL_FRAMEBUFFER_COMPLETE_EXT ?
-             "complete" : "incomplete");
-   }
+		glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, Formats[i].format,
+					 piglit_width, piglit_height);
+		err = glGetError();
+		if (err) {
+			printf("%s: glRenderbufferStorage failed for "
+			       "format 0x%x\n",
+			       TestName, Formats[i].format);
+			pass = GL_FALSE;
+		}
+		printf("0x%04X: %s\n", Formats[i].format,
+		       (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) ==
+			GL_FRAMEBUFFER_COMPLETE_EXT ?
+			"complete" : "incomplete"));
+	}
 
-   /* test formats that should fail */
-   for (i = 0; i < ARRAY_SIZE(InvalidFormats); i++) {
-      glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, InvalidFormats[i],
-                               piglit_width, piglit_height);
-      err = glGetError();
-      if (err != GL_INVALID_ENUM) {
-         printf("%s: glRenderbufferStorage erroneously accepted format 0x%x\n",
-                TestName, InvalidFormats[i]);
-         pass = GL_FALSE;
-      }
-   }
+	/* test formats that should fail */
+	for (i = 0; i < ARRAY_SIZE(InvalidFormats); i++) {
+		glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, InvalidFormats[i],
+					 piglit_width, piglit_height);
+		err = glGetError();
+		if (err != GL_INVALID_ENUM) {
+			printf("%s: glRenderbufferStorage erroneously "
+			       "accepted format 0x%x\n",
+			       TestName, InvalidFormats[i]);
+			pass = GL_FALSE;
+		}
+	}
 
-   return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
 }
 
 
 enum piglit_result
 piglit_display(void)
 {
-   return test();
+	return test();
 }
 
 
 void
 piglit_init(int argc, char**argv)
 {
-   piglit_require_extension("GL_EXT_framebuffer_object");
+	piglit_require_extension("GL_EXT_framebuffer_object");
 
-   HaveExtension[0] = GL_TRUE;
-   HaveExtension[EXT_packed_depth_stencil] = piglit_is_extension_supported("GL_EXT_packed_depth_stencil");
-   HaveExtension[ARB_framebuffer_object] = piglit_is_extension_supported("GL_ARB_framebuffer_object");
-   HaveExtension[ARB_texture_rg] = piglit_is_extension_supported("GL_ARB_texture_rg");
+	HaveExtension[0] = GL_TRUE;
+	HaveExtension[EXT_packed_depth_stencil] = piglit_is_extension_supported("GL_EXT_packed_depth_stencil");
+	HaveExtension[ARB_framebuffer_object] = piglit_is_extension_supported("GL_ARB_framebuffer_object");
+	HaveExtension[ARB_texture_rg] = piglit_is_extension_supported("GL_ARB_texture_rg");
 
-   piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
+	piglit_ortho_projection(piglit_width, piglit_height, GL_FALSE);
 }
