95396897adbc77246da357928d9b42e01aacc9c1
Brian Paul <brianp@vmware.com>
2013-01-21 16:35:39 -0700

    gl-2.0: remove explicit 100 x 100 window size
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

--- tests/spec/gl-2.0/vertex-program-two-side.c
+++ tests/spec/gl-2.0/vertex-program-two-side.c
@@ -48,9 +48,6 @@
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.supports_gl_compat_version = 10;
-
-	config.window_width = 100;
-	config.window_height = 100;
 	config.window_visual = PIGLIT_GL_VISUAL_RGB | PIGLIT_GL_VISUAL_ALPHA | PIGLIT_GL_VISUAL_DOUBLE;
 
 PIGLIT_GL_TEST_CONFIG_END
