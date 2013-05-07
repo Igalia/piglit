6e9442a7ac5c9372760dbf96f66580918ffde648
Vinson Lee <vlee@freedesktop.org>
2013-05-07 13:56:58 -0700

    amd_performance_monitor: Fix multi-statement macro 'report'.
    Fixes "Nesting level does not match indentation" defect reported by
    Coverity.
    
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- tests/spec/amd_performance_monitor/api.c
+++ tests/spec/amd_performance_monitor/api.c
@@ -113,8 +113,10 @@ find_invalid_counter(unsigned *counters, int num_counters)
 }
 
 #define report(pass) \
-    piglit_report_subtest_result((pass) ? PIGLIT_PASS : PIGLIT_FAIL, __FUNCTION__); \
-    return
+	do { \
+		piglit_report_subtest_result((pass) ? PIGLIT_PASS : PIGLIT_FAIL, __FUNCTION__); \
+		return; \
+	} while (0)
 
 /******************************************************************************/
 
