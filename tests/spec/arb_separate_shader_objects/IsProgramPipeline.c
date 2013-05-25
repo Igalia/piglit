7e0f8ee2f7ba480ce7cea035472adc3346e7ebbc
Gregory Hainaut <gregory.hainaut@gmail.com>
2013-05-25 12:29:28 +0200

    sso: new test IsProgramPipeline
    Check the pipeline object state creation.  glGenProgramPipelines only
    reserves the names.  The object does not "exist" until it is bound.
    
    Also take the opportunity to test negative value on
    glGenProgramPipelines and glDeleteProgramPipelines.
    
    This test fails on AMD's closed-source driver (Catalyst 13.4 on
    HD5770).  The test crashes when -1 is passed to glGenProgramPipelines.
    They are presumably treating the GLsizei as unsigned and generating a
    lot of names.  They also return GL_TRUE from glIsProgramPipeline
    before the object has been bound.
    
    This test fails on NVIDIA's closed-source driver (unknown version on
    GTX 650).  The test crashes in glGetProgramPipelineiv for a pipeline
    that has never been bound.  They are presumably dereferencing a NULL
    pointer in some internal data structure.  They also return GL_TRUE
    from glIsProgramPipeline before the object has been bound.
    
    V4: split the new test in a separate commit
    
    V5 (idr):
    * Use 'pass = foo() && pass;' idiom instead of 'pass &= foo();'.  C's
      short-circuit evaulation rules cause these to be different.
    * s/GLboolean/bool/g
    * Produce no output with -auto.
    * Add test to CMakeLists.gl.txt.
    * Trivial reformatting.
    
    V6 (idr):
    * Set config.supports_gl_compat_version = 20,
      config.supports_gl_core_version = 31, and remove explicit version
      checks.  Suggested by Paul.
    * Use piglit_reset_gl_error instead of open coding it.  Noticed by Ian.
    
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

---
+++ tests/spec/arb_separate_shader_objects/IsProgramPipeline.c
@@ -0,0 +1,136 @@
+/*
+ * Copyright © 2013 Gregory Hainaut <gregory.hainaut@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * \name IsProgramPipeline.c
+ * Verify correct behavior of glIsProgramPipeline relative to when a pipeline
+ * actually starts to exist.
+ *
+ * Also verify that glGenProgramPipelines and glDeleteProgramPipelines with
+ * negative counts function correctly.
+ *
+ * Section 2.11.4 (Program Pipeline Objects) of the OpenGL 4.1 spec says:
+ *
+ *     "The command
+ *
+ *         void GenProgramPipelines( sizei n, uint *pipelines );
+ *
+ *     returns n previously unused program pipeline object names in
+ *     pipelines. These names are marked as used, for the purposes of
+ *     GenProgramPipelines only, but they acquire state only when they are
+ *     first bound."
+ */
+
+#include "piglit-util-gl-common.h"
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+	config.supports_gl_compat_version = 20;
+	config.supports_gl_core_version = 31;
+
+	config.window_width = 32;
+	config.window_height = 32;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+static bool pass;
+
+enum piglit_result
+piglit_display(void)
+{
+	/* UNREACHED */
+	return PIGLIT_FAIL;
+}
+
+static void
+IsProgramPipeline(GLuint pipe, GLboolean expected)
+{
+	const GLboolean status = glIsProgramPipeline(pipe);
+
+	if (status != expected) {
+		pass = false;
+
+		fprintf(stderr,
+			"Pipeline %d has wrong IsProgramPipeline. "
+			"Expected %d, got %d\n",
+			pipe, expected, status);
+	}
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	GLuint id[4];
+	int i;
+	GLint dummy;
+
+	piglit_require_extension("GL_ARB_separate_shader_objects");
+
+	pass = true;
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	if (!piglit_automatic)
+		printf("glGenProgramPipelines with negative n value\n");
+
+	glGenProgramPipelines(-1, id);
+	pass = piglit_check_gl_error(GL_INVALID_VALUE) && pass;
+
+	if (!piglit_automatic)
+		printf("glGenProgramPipelines with correct n value\n");
+
+	glGenProgramPipelines(4, id);
+	pass = piglit_check_gl_error(GL_NO_ERROR) && pass;
+
+	for (i = 0; i < 4 ; i++) {
+		IsProgramPipeline(id[i], GL_FALSE);
+	}
+
+	glBindProgramPipeline(id[0]);
+	glUseProgramStages(id[1], GL_ALL_SHADER_BITS, 0);
+	glActiveShaderProgram(id[2], 0);
+	glGetProgramPipelineiv(id[3], GL_VERTEX_SHADER, &dummy);
+
+	/* Flush any errors.  The goal is to check that objects acquire a
+	 * state.
+	 */
+	piglit_reset_gl_error();
+
+	for (i = 0; i < 4 ; i++) {
+		IsProgramPipeline(id[i], GL_TRUE);
+	}
+
+	if (!piglit_automatic)
+		printf("glDeleteProgramPipelines with negative n value\n");
+
+	glDeleteProgramPipelines(-1, id);
+	pass = piglit_check_gl_error(GL_INVALID_VALUE) && pass;
+
+	if (!piglit_automatic)
+		printf("glDeleteProgramPipelines with correct n value\n");
+
+	glDeleteProgramPipelines(4, id);
+	pass = piglit_check_gl_error(GL_NO_ERROR) && pass;
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+}
