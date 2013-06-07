fb2fcdd33f6cecd6bbc63e6fe162667838ee3d44
Fabian Bieler <fabianbieler@fastmail.fm>
2013-06-07 20:12:56 +0200

    arb_geometry_shader4: Test valid input primitives.
    Test that glProgramParameter() triggers the required errors.
    
    The geometry shader input primitive type is limited to a subset
    of OpenGL primitive types.
    
    From the ARB_geometry_shader4 spec (section Errors):
    "The error INVALID_VALUE is generated by ProgramParameteriARB if <pname> is
    GEOMETRY_INPUT_TYPE_ARB and <value> is not one of POINTS, LINES,
    LINES_ADJACENCY_ARB, TRIANGLES or TRIANGLES_ADJACENCY_ARB."
    
    Signed-off-by: Fabian Bieler <fabianbieler@fastmail.fm>
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

---
+++ tests/spec/arb_geometry_shader4/execution/program-parameter/common.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright © 2013 The Piglit project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * \file common.h
+ *
+ * Common code for glProgramParameter testing.
+ */
+
+#include "piglit-util-gl-common.h"
+
+
+struct primitive_geom_info {
+	GLenum type;
+	GLenum error;
+};
+
+
+/* Primitive types passed as geometry shader input type and expected error. */
+static const struct primitive_geom_info primitives_in[] = {
+	{GL_POINTS, GL_NO_ERROR},
+
+	{GL_LINES, GL_NO_ERROR},
+	{GL_LINE_STRIP, GL_INVALID_VALUE},
+	{GL_LINE_LOOP, GL_INVALID_VALUE},
+
+	{GL_TRIANGLES, GL_NO_ERROR},
+	{GL_TRIANGLE_STRIP, GL_INVALID_VALUE},
+	{GL_TRIANGLE_FAN, GL_INVALID_VALUE},
+
+	{GL_LINES_ADJACENCY, GL_NO_ERROR},
+	{GL_LINE_STRIP_ADJACENCY, GL_INVALID_VALUE},
+
+	{GL_TRIANGLES_ADJACENCY, GL_NO_ERROR},
+	{GL_TRIANGLE_STRIP_ADJACENCY, GL_INVALID_VALUE},
+
+	{GL_QUADS, GL_INVALID_VALUE},
+	{GL_QUAD_STRIP, GL_INVALID_VALUE},
+	{GL_POLYGON, GL_INVALID_VALUE},
+};
+
+
+static const char vs_text[] =
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(0);\n"
+	"}\n";
+
+static const char gs_text[] =
+	"#extension GL_ARB_geometry_shader4: enable\n"
+	"uniform int vertex_count;\n"
+	"void main()\n"
+	"{\n"
+	"	for (int i = 0; i < vertex_count; i++) {\n"
+	"		gl_Position = vec4(0.0);\n"
+	"		EmitVertex();\n"
+	"	}\n"
+	"}\n";
+
+static const char fs_text[] =
+	"void main()\n"
+	"{\n"
+	"	gl_FragColor = vec4(1.0);\n"
+	"}\n";
+
+
+static GLuint
+create_shader(const char *const vs_text, const char *const gs_text,
+	      const char *const fs_text)
+{
+	const char *const varying = "var";
+	GLuint vs, gs, fs, prog;
+
+	prog = glCreateProgram();
+	vs = piglit_compile_shader_text(GL_VERTEX_SHADER, vs_text);
+	glAttachShader(prog, vs);
+	glDeleteShader(vs);
+	gs = piglit_compile_shader_text(GL_GEOMETRY_SHADER, gs_text);
+	glAttachShader(prog, gs);
+	glDeleteShader(gs);
+	if (fs_text) {
+		fs = piglit_compile_shader_text(GL_FRAGMENT_SHADER, fs_text);
+		glAttachShader(prog, fs);
+		glDeleteShader(fs);
+	} else {
+		glTransformFeedbackVaryings(prog, 1, &varying,
+				            GL_INTERLEAVED_ATTRIBS);
+	}
+
+	if (!piglit_check_gl_error(GL_NO_ERROR))
+		piglit_report_result(PIGLIT_FAIL);
+
+	return prog;
+}
+
