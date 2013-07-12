6c01796f0814530b30d16fe2c6b40b26e9464c81
Chad Versace <chad.versace@linux.intel.com>
2013-07-12 07:30:44 -0700

    hiz: Use default window size
    All the hiz tests requested a 400x400 window. Instead, don't specify
    window size and let Piglit choose it. This significantly speeds up their
    execution time.
    
    Signed-off-by: Chad Versace <chad.versace@linux.intel.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- tests/hiz/hiz-stencil-test-window-depth1.c
+++ tests/hiz/hiz-stencil-test-window-depth1.c
@@ -38,9 +38,6 @@
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.supports_gl_compat_version = 10;
-
-	config.window_width = 400;
-	config.window_height = 400;
 	config.window_visual = PIGLIT_GL_VISUAL_RGBA | PIGLIT_GL_VISUAL_STENCIL | PIGLIT_GL_VISUAL_DEPTH;
 
 PIGLIT_GL_TEST_CONFIG_END
