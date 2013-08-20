2002425f6ca39c5eaf4b1928bd0e66db9b0ebc75
Nicholas Mack <nichmack@gmail.com>
2013-08-20 10:41:51 -0700

    GL 3.2: Test that a query of QUADS_FOLLOW_PROVOKING_VERTEX_CONVENTION is invalid
    Note: GL 3.2 core spec refers to QUADS_FOLLOW_PROVOKING_VERTEX but this was removed
    in 3.3 core spec.
    
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

---
+++ tests/spec/arb_provoking_vertex/quads-follow-provoking-vertex.c
@@ -0,0 +1,61 @@
+/**
+ * Copyright © 2013 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * QUADS_FOLLOW_PROVOKING_VERTEX was erringly added to GL 3.2 core spec
+ *
+ *  This was removed from the core spec in 3.3.
+ *
+ *  Table 6.45 of GL 3.2 core spec includes QUADS_FOLLOW_PROVOKING_VERTEX
+ *  which can be queried with GetBooleanv() to see "Whether quads follow
+ *  provoking vertex convention"
+ *
+ */
+
+#include "piglit-util-gl-common.h"
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+        config.supports_gl_core_version = 32;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+void
+piglit_init(int argc, char **argv)
+{
+	bool pass = true;
+	GLboolean followsProvoking = false;
+
+	glGetBooleanv(GL_QUADS_FOLLOW_PROVOKING_VERTEX_CONVENTION,
+			 &followsProvoking);
+
+	pass = piglit_check_gl_error(GL_INVALID_ENUM) && pass;
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+}
+
+enum piglit_result
+piglit_display(void)
+{
+	return PIGLIT_FAIL;
+}
