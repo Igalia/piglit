bdef20291bd0616ef1a701448adce7cf1c4bcb0a
Nicholas Mack <nichmack@gmail.com>
2013-08-20 10:40:18 -0700

    GL 3.2: Test that the initial value of provoking vertex mode is LAST_VERTEX_CONVENTION
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

---
+++ tests/spec/arb_provoking_vertex/provoking-vertex-initial.c
@@ -0,0 +1,64 @@
+/**
+ * Copyright © 2013 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * Test that the intitial value of PROVOKING_VERTEX is LAST_VERTEX_CONVENTION
+ *
+ * Section 2.18(Flatshading) of OpenGL 3.2 Core says:
+ * "The initial value of the provoking vertex mode is LAST_VERTEX_CONVENTION."
+ *
+ */
+
+#include "piglit-util-gl-common.h"
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+	config.supports_gl_compat_version = 32;
+        config.supports_gl_core_version = 32;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+void
+piglit_init(int argc, char **argv)
+{
+	bool pass = true;
+	int ret = 0;
+
+	glGetIntegerv(GL_PROVOKING_VERTEX, &ret);
+	if(ret != GL_LAST_VERTEX_CONVENTION) {
+		printf("GL_PROVOKING_VERTEX was expected to be GL_LAST_VERTEX"
+			"_CONVENTION, but %s was returned.\n",
+			 piglit_get_gl_enum_name(ret));
+		pass = false;
+	}
+
+	pass = piglit_check_gl_error(GL_NO_ERROR) && pass;
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+}
+
+enum piglit_result
+piglit_display(void)
+{
+	return PIGLIT_FAIL;
+}
