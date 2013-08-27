1b0a9da910906c6728db73261dda6cad94c53974
Jacob Penner <jkpenner91@gmail.com>
2013-08-27 10:06:27 -0700

    GL 3.2: Test ReadPixels() works properly with layered framebuffers.
    Reviewed-by: Paul Berry <stereotype441@gmail.com>

---
+++ tests/spec/gl-3.2/layered-rendering/readpixels.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright © 2013 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+
+/** @file readpixel.c
+ *
+ * Section 4.4.7(Framebuffer Objects) From GL spec 3.2 core:
+ *
+ * When commands such as ReadPixels read from a layered framebuffer, the
+ * image at layer zero of the selected attachment is always used to obtain
+ * pixel values.
+ */
+
+#include "piglit-util-gl-common.h"
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+	config.supports_gl_compat_version = 32;
+	config.supports_gl_core_version = 32;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+static const float color[3][3] = {
+	{0.0, 1.0, 0.0},
+	{0.0, 0.0, 1.0}
+};
+
+void
+piglit_init(int argc, char **argv)
+{
+	int i, j;
+	bool pass = true;
+	GLuint fbo, texture;
+	float colorData[2][10*10*3];
+
+	/* fill in colorData */
+	for(j = 0; j < 2; j++)
+	for(i = 0; i < 10*10; i++) {
+		colorData[j][i*3+0] = color[j][0];
+		colorData[j][i*3+1] = color[j][1];
+		colorData[j][i*3+2] = color[j][2];
+	}
+
+	/* Create the Source framebuffer object */
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_3D, texture);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+	glTexImage3D(GL_TEXTURE_3D, 0, GL_RGB, 10, 10, 2, 0, GL_RGB,
+		     GL_FLOAT, colorData);
+
+	glGenFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	glFramebufferTexture3D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+			       GL_TEXTURE_3D, texture, 0, 0);
+
+	if(!piglit_check_gl_error(GL_NO_ERROR)) {
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	/* piglit_probe_rect_rgb internally calls ReadPixel(). Check that
+	 * the color probed is the same as the Zero layer of the texture.
+	 */
+	pass = piglit_probe_rect_rgb(0, 0, 10, 10, color[0]) && pass;
+
+	piglit_report_result(pass ? PIGLIT_PASS : PIGLIT_FAIL);
+}
+
+enum piglit_result
+piglit_display(void)
+{
+	/* UNREACHABLE */
+	return PIGLIT_FAIL;
+}
