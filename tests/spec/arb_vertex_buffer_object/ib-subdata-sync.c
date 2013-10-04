acdccd53b54931e2647d3b5d68728627b15cdb4c
Eric Anholt <eric@anholt.net>
2013-10-03 23:24:16 -0700

    GL_ARB_vertex_buffer_object: Test for glBufferSubData() on index buffers.
    We already have one for VBOs under general/
    
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

---
+++ tests/spec/arb_vertex_buffer_object/ib-subdata-sync.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright © 2013 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "piglit-util-gl-common.h"
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+	config.supports_gl_compat_version = 10;
+
+	config.window_visual = PIGLIT_GL_VISUAL_RGBA | PIGLIT_GL_VISUAL_DOUBLE;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+enum piglit_result
+piglit_display(void)
+{
+	float verts[] = {
+		-1, -1,
+		 0, -1,
+		 0,  1,
+		-1,  1,
+
+		 0, -1,
+		 1, -1,
+		 1,  1,
+		 0,  1,
+	};
+	uint32_t ib_1[] = {0, 1, 2, 3};
+	uint32_t ib_2[] = {4, 5, 6, 7};
+
+	bool pass = true;
+	size_t size = sizeof(ib_1);
+	GLuint vbo, ibo;
+	float green[] = {0, 1, 0, 0};
+
+	glColor4fv(green);
+
+	glGenBuffersARB(1, &vbo);
+	glBindBufferARB(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(2, GL_FLOAT, 0, NULL);
+
+	glGenBuffersARB(1, &ibo);
+	glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4096, NULL, GL_STREAM_DRAW);
+
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, size, ib_1);
+	glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, size, ib_2);
+	glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
+
+	pass = piglit_probe_rect_rgba(0, 0, piglit_width, piglit_height, green);
+
+	piglit_present_results();
+
+	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+}
+
+void
+piglit_init(int argc, char *argv[])
+{
+	piglit_require_extension("GL_ARB_vertex_buffer_object");
+}
