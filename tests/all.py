8ae12916d4dd8a80fc6047c01fe47e21d4c3c53e
Chris Forbes <chrisf@ijw.co.nz>
2013-10-13 20:21:01 +1300

    arb_draw_indirect: add test for DrawArraysIndirect with base instance
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>

--- tests/all.py
+++ tests/all.py
@@ -1592,6 +1592,7 @@ spec['ARB_draw_indirect'] = arb_draw_indirect
 add_concurrent_test(arb_draw_indirect, 'arb_draw_indirect-api-errors')
 add_concurrent_test(arb_draw_indirect, 'arb_draw_indirect-draw-arrays')
 add_concurrent_test(arb_draw_indirect, 'arb_draw_indirect-draw-elements')
+add_concurrent_test(arb_draw_indirect, 'arb_draw_indirect-draw-arrays-base-instance')
 
 # Group ARB_fragment_program
 arb_fragment_program = Group()
