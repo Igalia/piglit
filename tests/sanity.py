a0a4487c194f2f9e389263856c5654d6ca840cf6
Dylan Baker <baker.dylan.c@gmail.com>
2013-10-30 08:43:35 -0700

    tests/sanity.tests: Make sanity.tests importable
    This allows sanity.py to export a profile instance, meaning it can be
    used as an import instead of an execfile.
    
    Acked-by: Daniel Vetter <daniel@ffwll.ch>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Signed-off-by: Dylan Baker <baker.dylan.c@gmail.com>

--- tests/sanity.py
+++ tests/sanity.py
@@ -2,14 +2,11 @@
 # Minimal tests to check whether the installation is working
 #
 
-from framework.core import Group, TestProfile
+from framework.core import TestProfile
 from framework.gleantest import GleanTest
 
 __all__ = ['profile']
 
-glean = Group()
-glean['basic'] = GleanTest('basic')
-glean['readPixSanity'] = GleanTest('readPixSanity')
-
 profile = TestProfile()
-profile.tests['glean'] = glean
+profile.tests['glean/basic'] = GleanTest('basic')
+profile.tests['glean/readPixSanity'] = GleanTest('readPixSanity')
