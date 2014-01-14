1f8788ded09a1c3261db0ff182ec171dadec1725
Dylan Baker <baker.dylan.c@gmail.com>
2014-01-14 15:20:41 -0800

    Adds piglit framework test to test GleanTest.globalParams bug
    This patch adds a framework test to show the problem with
    GleanTest.globalParams. It makes use of nose, a python testing
    framework, which is simple to write tests for, and is in widespread
    usage for testing python code.
    
    Acked-by: Jose Fonseca <jfonseca@vmware.com>
    Tested-by: Michel Dänzer <michel.daenzer@amd.com>
    Signed-off-by: Dylan Baker <baker.dylan.c@gmail.com>

---
+++ framework/gleantest_tests.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2014 Intel Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+""" Tests for the glean class. Requires Nose """
+
+from framework.gleantest import GleanTest
+
+
+def test_globalParams_assignment():
+    """ Test to ensure that GleanTest.globalParams are correctly assigned
+
+    Specifically this tests for a bug where globalParams only affected
+    instances of GleanTest created after globalParams were set, so changing the
+    globalParams value had unexpected results.
+
+    If this test passes the GleanTest.command attributes will be the same in
+    the instance created before the globalParams assignment and the one created
+    after. A failure means the that globalParams are not being added to tests
+    initialized before it is set.
+
+    """
+    test1 = GleanTest('basic')
+    GleanTest.globalParams = ['--quick']
+    test2 = GleanTest('basic')
+    assert test1.command == test2.command
