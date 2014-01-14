257efacc3e1ceb6f6c5246e88e2c1db9cf7f3d43
José Fonseca <jfonseca@vmware.com>
2014-01-14 17:07:46 +0000

    tests/quick.py: Ensure glean receives the --quick parameter.
    Which was lost as result of commit 5a12f3b9f0d6d93da1ac7a72c9806b1a831a1744.
    
    Urgent. Trivial change.

--- tests/quick.py
+++ tests/quick.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+# XXX: These lines must be the first statements for the GleanTest.globalParams
+# assignment to be effective.  Do NOT add any import statement before.
 from framework.gleantest import GleanTest
+GleanTest.globalParams += ["--quick"]
+
 from tests.all import profile
 
 __all__ = ['profile']
-
-GleanTest.globalParams += ["--quick"]
 
 # These take too long
 del profile.tests['shaders']['glsl-fs-inline-explosion']
