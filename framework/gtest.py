6e5b8c40a1a973fa430e4b635c28541f4fb5458c
Tom Stellard <thomas.stellard@amd.com>
2014-04-23 07:08:45 -0700

    gtest: Use correct fuction signature for interpret_result
    This was changed in fb6c545d880791f8e3057bbd86dd5cdf1bf3a3d3
    and gtest.py was not updated.

--- framework/gtest.py
+++ framework/gtest.py
@@ -29,14 +29,15 @@ import re
 from framework.exectest import Test
 
 class GTest(Test):
-    def interpret_result(self, out, returncode, results):
+    def interpret_result(self):
         # Since gtests can have several subtets, if any of the subtests fail
         # then we need to report fail.
+        out = self.result['out']
         if len(re.findall('FAILED', out, re.MULTILINE)) > 0:
-            results['result'] = 'fail'
+            self.result['result'] = 'fail'
         elif len(re.findall('PASSED', out, re.MULTILINE)) > 0:
-            results['result'] = 'pass'
+            self.result['result'] = 'pass'
         else:
             #If we get here, then the test probably exited early.
-            results['result'] = 'fail'
+            self.result['result'] = 'fail'
         return out
