a962fea13ed48ac46bf1ad3aab2dcd5978e4ee93
Brian Paul <brianp@vmware.com>
2014-05-20 16:48:12 -0600

    arb_draw_elements_base_vertex: rename drawrangeelements test
    Reviewed-by: Matt Turner <mattst88@gmail.com>

--- tests/all.py
+++ tests/all.py
@@ -1581,7 +1581,7 @@ add_plain_test(arb_draw_elements_base_vertex, 'draw-elements-base-vertex-bounds'
 arb_draw_elements_base_vertex['draw-elements-base-vertex-neg-user_varrays'] = PiglitTest(['draw-elements-base-vertex-neg', '-auto', 'user_varrays'])
 add_plain_test(arb_draw_elements_base_vertex, 'arb_draw_elements_base_vertex-drawelements-instanced')
 arb_draw_elements_base_vertex['draw-elements-instanced-base-vertex-user_varrays'] = PiglitTest(['draw-elements-instanced-base-vertex', '-auto', 'user_varrays'])
-add_plain_test(arb_draw_elements_base_vertex, 'draw-range-elements-base-vertex')
+add_plain_test(arb_draw_elements_base_vertex, 'arb_draw_elements_base_vertex-drawrangeelements')
 add_plain_test(arb_draw_elements_base_vertex, 'multi-draw-elements-base-vertex')
 
 # Group ARB_draw_instanced
