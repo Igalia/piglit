c91b650bd86bc2acec25c47fb13b25c667e48497
Dylan Baker <dylanx.c.baker@intel.com>
2014-08-19 11:19:16 -0700

    framework: Merge programs_tests.py into core_tests.py
    Commit 9e0c3ee1 moved get_config() out of the programs/run.py into
    core.py, since the only tests in programs_tests.py are for get_config
    once they are moved into the more appropriate core module the
    programs_tests module is empty and can be deleted.
    
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- framework/tests/core_tests.py
+++ framework/tests/core_tests.py
@@ -23,8 +23,17 @@
 
 import os
 import collections
+import shutil
+import ConfigParser
+import textwrap
+import nose.tools as nt
 import framework.tests.utils as utils
 import framework.core as core
+
+
+def _reset_piglit_config():
+    """ Set core.PIGLIT_CONFIG back to pristine """
+    core.PIGLIT_CONFIG = ConfigParser.SafeConfigParser()
 
 
 def check_initialize(target):
@@ -111,3 +120,76 @@ def test_parse_listfile_tilde():
         results = core.parse_listfile(tfile)
 
     assert results[0] == os.path.expandvars("$HOME/foo")
+
+
+class TestGetConfig(utils.TestWithEnvClean):
+    CONF_FILE = textwrap.dedent("""
+    [nose-test]
+    ; a section for testing behavior
+    dir = foo
+    """)
+
+    def __unset_config(self):
+        self.defer(_reset_piglit_config)
+        self.add_teardown('XDG_CONFIG_HOME')
+        self.add_teardown('HOME')
+
+    def __move_local(self):
+        """ Move a local piglit.conf so it isn't overwritten """
+        if os.path.exists('piglit.conf'):
+            shutil.move('piglit.conf', 'piglit.conf.restore')
+            self.defer(shutil.move, 'piglit.conf.restore', 'piglit.conf')
+
+    def setup(self):
+        self.__unset_config()
+        self.__move_local()
+
+    def test_xdg_config_home(self):
+        """ get_config() finds $XDG_CONFIG_HOME/piglit.conf """
+        with utils.tempdir() as tdir:
+            os.environ['XDG_CONFIG_HOME'] = tdir
+            with open(os.path.join(tdir, 'piglit.conf'), 'w') as f:
+                f.write(TestGetConfig.CONF_FILE)
+            core.get_config()
+
+        nt.ok_(core.PIGLIT_CONFIG.has_section('nose-test'),
+               msg='$XDG_CONFIG_HOME not found')
+
+    def test_config_home_fallback(self):
+        """ get_config() finds $HOME/.config/piglit.conf """
+        with utils.tempdir() as tdir:
+            os.environ['HOME'] = tdir
+            os.mkdir(os.path.join(tdir, '.config'))
+            with open(os.path.join(tdir, '.config/piglit.conf'), 'w') as f:
+                f.write(TestGetConfig.CONF_FILE)
+            core.get_config()
+
+        nt.ok_(core.PIGLIT_CONFIG.has_section('nose-test'),
+               msg='$HOME/.config/piglit.conf not found')
+
+    def test_local(self):
+        """ get_config() finds ./piglit.conf """
+        with utils.tempdir() as tdir:
+            self.defer(os.chdir, os.getcwd())
+            os.chdir(tdir)
+
+            with open(os.path.join(tdir, 'piglit.conf'), 'w') as f:
+                f.write(TestGetConfig.CONF_FILE)
+
+            core.get_config()
+
+        nt.ok_(core.PIGLIT_CONFIG.has_section('nose-test'),
+               msg='./piglit.conf not found')
+
+    def test_piglit_root(self):
+        """ get_config() finds "piglit root"/piglit.conf """
+        with open('piglit.conf', 'w') as f:
+            f.write(TestGetConfig.CONF_FILE)
+        self.defer(os.unlink, 'piglit.conf')
+        self.defer(os.chdir, os.getcwd())
+        os.chdir('..')
+
+        core.get_config()
+
+        nt.ok_(core.PIGLIT_CONFIG.has_section('nose-test'),
+               msg='$PIGLIT_ROOT not found')
