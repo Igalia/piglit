Igalia/piglit

commit 4efb025fcac0b37d04ab759ab15055f27cea94f8

libpiglit: Set CMAKE SOVERSION for internal libraries to "0"
When packaging for piglit for debian, I see this type of message:
dpkg-shlibdeps: warning: Can't extract name and version from library name libpiglitutil.so

It it repeated several times for each piglit executable causing a
significant delay in the build time. Setting a soname version for
these internal libraries causes this message to go away.

Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

cmake/piglit_util.cmake
@@ -84,5 +84,6 @@ function(piglit_add_library name)
         install(TARGETS ${name} DESTINATION ${PIGLIT_INSTALL_LIBDIR}/lib)
     endif(WIN32)
     add_dependencies(${name} piglit_dispatch_gen)
+    set_target_properties(${name} PROPERTIES SOVERSION "0")
 
 endfunction(piglit_add_library)

commit 81481096dccb65b8b2601718123afe3e782849d2

libpiglit: Do not set CMAKE_SOVERSION on OpenBSD.
This patch fixes build regression on OpenBSD introduced with commit
4efb025fcac0b37d04ab759ab15055f27cea94f8 'libpiglit: Set CMAKE SOVERSION
for internal libraries to "0"'.

Linking CXX shared library ../../../../lib/libpiglitutil_gl.so
/usr/bin/ld: cannot find -llibpiglitutil.so.0
collect2: ld returned 1 exit status
target_api/gl/tests/util/CMakeFiles/piglitutil_gl.dir/build.make:515: recipe for target
'lib/libpiglitutil_gl.so.0' failed

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=84969
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Brian Paul <brianp@vmware.com>

cmake/piglit_util.cmake
@@ -84,6 +84,8 @@ function(piglit_add_library name)
         install(TARGETS ${name} DESTINATION ${PIGLIT_INSTALL_LIBDIR}/lib)
     endif(WIN32)
     add_dependencies(${name} piglit_dispatch_gen)
-    set_target_properties(${name} PROPERTIES SOVERSION "0")
+    if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD")
+        set_target_properties(${name} PROPERTIES SOVERSION "0")
+    endif()
 
 endfunction(piglit_add_library)