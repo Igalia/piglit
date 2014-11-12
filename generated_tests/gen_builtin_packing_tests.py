c61208d17601ff1a1b03abae0b144f577942b08d
Dylan Baker <dylanx.c.baker@intel.com>
2014-11-11 16:10:06 -0800

    gen_builtin_packing_tests.py: Fix imports
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    acked-by: Matt Turner <mattst88@gmail.com>

--- generated_tests/gen_builtin_packing_tests.py
+++ generated_tests/gen_builtin_packing_tests.py
@@ -1,18 +1,35 @@
 # coding=utf-8
+# Copyright (c) 2014 Intel Corporation
 
-import mako.template
-import mako.runtime
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import math
-import numpy as np
 import optparse
 import os
 import sys
-
 from collections import namedtuple
-from mako.template import Template
 from math import copysign, fabs, fmod, frexp, isinf, isnan, modf
-from numpy import int8, int16, int32, uint8, uint16, uint32, float32
 from textwrap import dedent
+
+import mako.runtime
+from mako.template import Template
+from numpy import int8, int16, uint8, uint16, uint32, float32
 
 # ----------------------------------------------------------------------------
 # Overview
