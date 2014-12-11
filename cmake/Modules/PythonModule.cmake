a55fbebd5e227a4a29c83e85d8b44154cdcbf145
Dylan Baker <dylanx.c.baker@intel.com>
2014-12-11 15:18:35 -0800

    cmake: Simplify finding python modules
    This patch adds 3 files, PythonModule.cmake, FindPythonMako.cmake and
    FindPythonNumpy.cmake.
    
    PythonModule provides a simple function for running python and parsing
    the version strings, each FindPython*.cmake then just calls that
    function, and then calls the find_package_handle_standard_args macro for
    simple, standard finding. This is much more in line with how the core
    cmake modules work, and makes is trivial to add additional python
    modules with all of the standard cmake awesomeness like version
    checking.
    
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

---
+++ cmake/Modules/PythonModule.cmake
@@ -0,0 +1,35 @@
+function(find_python_module MODULE PREFIX)
+	execute_process(
+		COMMAND ${PYTHON_EXECUTABLE} -c "import sys, ${MODULE}; sys.stdout.write(${MODULE}.__version__)"
+		OUTPUT_VARIABLE _version
+		RESULT_VARIABLE _status
+		ERROR_QUIET
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+
+	# Export version variables to parent scope. This is needed by
+	# find_package_handle_standard_args
+	set(${PREFIX}_VERSION_STRING ${_version} PARENT_SCOPE)
+
+	# A status returns 0 if everything is okay. And zero is false. To make
+	# checking in the outer scope less surprising
+	if (_status EQUAL 0)
+		set("${PREFIX}_STATUS" "success" PARENT_SCOPE)
+	endif (_status EQUAL 0)
+endfunction(find_python_module MODULE PREFIX)
+
+# This macro provides a simple way for basic python find modules to be
+# extremely simple without duplicate boilerplate
+macro(basic_python_module MODULE PREFIX)
+	find_python_module("${MODULE}" "${PREFIX}")
+
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(
+		"${PREFIX}"
+		REQUIRED_VARS "${PREFIX}_STATUS"
+		VERSION_VAR "${PREFIX}_VERSION_STRING"
+	)
+
+	# This isn't needed in the parent scope, just here.
+	unset("${PREFIX}_STATUS")
+endmacro(basic_python_module MODULE PREFIX)
