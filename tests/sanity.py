be2071c5d70133b7062e1b8be2b31a6f1d57a958
Dylan Baker <dylanx.c.baker@intel.com>
2014-12-29 16:28:24 -0800

    sanity.py: Make Test() command argument a list instead of string
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    Acked-by: Brian Paul <brianp@vmware.com>

--- tests/sanity.py
+++ tests/sanity.py
@@ -10,4 +10,4 @@ __all__ = ['profile']
 profile = TestProfile()
 
 profile.tests['spec/!OpenGL 1.0/gl-1.0-readpixsanity'] = \
-        PiglitGLTest('gl-1.0-readpixsanity', run_concurrent=True)
+    PiglitGLTest(['gl-1.0-readpixsanity'], run_concurrent=True)
