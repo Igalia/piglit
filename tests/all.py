dfb6b6d9a988ed40d957bf3a76b6a47a67cbd1b0
Martin Peres <martin.peres@linux.intel.com>
2015-01-22 13:38:31 +0200

    arb_direct_state_access: Added glTransformFeedbackBufferRange tests
    v2: review from Tapani
    - add the copyright for Intel
    - add some documentation
    - set the minimum GL version to 2.0
    - use piglit_build_simple_program_unlinked
    
    Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
    
    v3:
    - rewrite the test for GL 3.1 core (Ilia)
    - do not hardcode the tolerance (Laura)
    - delete the copyright from vmware as none of the code is left
    - improve the cleanup at the end of the test
    
    v4:
    - use the extern variable piglit_tolerance (Ilia & Laura)
    
    Signed-off-by: Martin Peres <martin.peres@linux.intel.com>
    Reviewed-by: Laura Ekstrand <laura@jlekstrand.net>

--- tests/all.py
+++ tests/all.py
@@ -3998,6 +3998,7 @@ arb_shader_atomic_counters['respecify-buffer'] = PiglitGLTest(['arb_shader_atomi
 
 spec['ARB_direct_state_access']['create-transformfeedbacks'] = PiglitGLTest(['arb_direct_state_access-create-transformfeedbacks'], run_concurrent=True)
 spec['ARB_direct_state_access']['transformfeedback-bufferbase'] = PiglitGLTest(['arb_direct_state_access-transformfeedback-bufferbase'], run_concurrent=True)
+spec['ARB_direct_state_access']['transformfeedback-bufferrange'] = PiglitGLTest(['arb_direct_state_access-transformfeedback-bufferrange'], run_concurrent=True)
 spec['ARB_direct_state_access']['dsa-textures'] = PiglitGLTest(['arb_direct_state_access-dsa-textures'], run_concurrent=True)
 spec['ARB_direct_state_access']['texturesubimage'] = PiglitGLTest(['arb_direct_state_access-texturesubimage'], run_concurrent=True)
 spec['ARB_direct_state_access']['bind-texture-unit'] = PiglitGLTest(['arb_direct_state_access-bind-texture-unit'], run_concurrent=True)
