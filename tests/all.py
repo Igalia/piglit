448427600ac3f75e1827cf9f5a0b3efc83a57c83
Martin Peres <martin.peres@linux.intel.com>
2015-02-13 19:34:33 +0200

    arb_direct_state_access: Added *NamedRenderbuffer* tests
    v2: review from Tapani
    - add some documentation
    - set the minimum GL version to 2.0
    
    v3:
    - set the minimum GL version to 3.1 core (Ilia)
    - do some clean up at the end of the test
    
    Signed-off-by: Martin Peres <martin.peres@linux.intel.com>
    Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

--- tests/all.py
+++ tests/all.py
@@ -4001,6 +4001,7 @@ spec['ARB_direct_state_access']['transformfeedback-bufferbase'] = PiglitGLTest([
 spec['ARB_direct_state_access']['transformfeedback-bufferrange'] = PiglitGLTest(['arb_direct_state_access-transformfeedback-bufferrange'], run_concurrent=True)
 spec['ARB_direct_state_access']['gettransformfeedback'] = PiglitGLTest(['arb_direct_state_access-gettransformfeedback'], run_concurrent=True)
 spec['ARB_direct_state_access']['create-renderbuffers'] = PiglitGLTest(['arb_direct_state_access-create-renderbuffers'], run_concurrent=True)
+spec['ARB_direct_state_access']['namedrenderbuffer'] = PiglitGLTest(['arb_direct_state_access-namedrenderbuffer'], run_concurrent=True)
 spec['ARB_direct_state_access']['dsa-textures'] = PiglitGLTest(['arb_direct_state_access-dsa-textures'], run_concurrent=True)
 spec['ARB_direct_state_access']['texturesubimage'] = PiglitGLTest(['arb_direct_state_access-texturesubimage'], run_concurrent=True)
 spec['ARB_direct_state_access']['bind-texture-unit'] = PiglitGLTest(['arb_direct_state_access-bind-texture-unit'], run_concurrent=True)
