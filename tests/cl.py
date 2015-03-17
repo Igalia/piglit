9199ea0ee54941dd79b7382a9850bc9d85536f8a
Dylan Baker <baker.dylan.c@gmail.com>
2015-03-17 13:56:57 -0700

    tests/cl.py: Remove accident duplicate test
    This test is exactly the same as another test added a few lines later.
    
    v2: - add this patch
    
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- tests/cl.py
+++ tests/cl.py
@@ -67,7 +67,6 @@ with profile.group_manager(PiglitCLTest, 'api') as g:
     g(['cl-api-create-program-with-source'], 'clCreateProgramWithSource')
     g(['cl-api-build-program'], 'clBuildProgram')
     g(['cl-api-compile-program'], 'clCompileProgram')
-    g(['cl-api-create-kernels-in-program'], 'clCreateKernelsInProgram')
     g(['cl-api-get-program-info'], 'clGetProgramInfo')
     g(['cl-api-get-program-build-info'], 'clGetProgramBuildInfo')
     g(['cl-api-retain_release-program'],
