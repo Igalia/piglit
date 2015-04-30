193f84fc0c4dfcbbbd8046410bb1813ba5442d64
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2015-04-30 12:50:49 +0200

    arb_shader_storage_buffer_object: Add new test for glDeleteBuffers() behavior
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>

--- tests/all.py
+++ tests/all.py
@@ -4037,6 +4037,7 @@ with profile.group_manager(
     g(['arb_shader_storage_buffer_object-rendering'], 'rendering')
     g(['arb_shader_storage_buffer_object-maxshaderstorageblocksize '], 'maxshaderstorageblocksize')
     g(['arb_shader_storage_buffer_object-getintegeri_v'], 'getintegeri_v')
+    g(['arb_shader_storage_buffer_object-deletebuffers'], 'deletebuffers')
 
 with profile.group_manager(
         PiglitGLTest,
