5055623f0da3a3b70db1f669b29e7d5aefce8bac
Kenneth Graunke <kenneth@whitecape.org>
2015-06-18 21:50:04 -0700

    Port fogcoord-dph.vpfp to shader_runner
    This commit was autogenerated by Python and Bash scripting.

--- tests/all.py
+++ tests/all.py
@@ -2605,7 +2605,6 @@ with profile.group_manager(
     g(['vp-address-04'], run_concurrent=False)
     g(['vp-bad-program'], run_concurrent=False)
     g(['vp-max-array'], run_concurrent=False)
-    add_vpfpgeneric(g, 'fogcoord-dph')
     add_vpfpgeneric(g, 'vp-arl-constant-array')
     add_vpfpgeneric(g, 'vp-arl-constant-array-huge')
     add_vpfpgeneric(g, 'vp-arl-constant-array-huge-varying')
