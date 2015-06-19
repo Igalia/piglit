1179508108577e8da429c004b589dbad92c5f5ba
Kenneth Graunke <kenneth@whitecape.org>
2015-06-18 21:50:04 -0700

    Port dataflow-bug.vpfp to shader_runner
    This commit was autogenerated by Python and Bash scripting.

--- tests/all.py
+++ tests/all.py
@@ -2608,7 +2608,6 @@ with profile.group_manager(
     g(['vp-address-04'], run_concurrent=False)
     g(['vp-bad-program'], run_concurrent=False)
     g(['vp-max-array'], run_concurrent=False)
-    add_vpfpgeneric(g, 'dataflow-bug')
     add_vpfpgeneric(g, 'fogcoord-dp3')
     add_vpfpgeneric(g, 'fogcoord-dph')
     add_vpfpgeneric(g, 'fogcoord-dp4')
