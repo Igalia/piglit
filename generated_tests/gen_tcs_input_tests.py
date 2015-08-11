baa07dff449844e94c0f505ddad9ba98ce21c528
Dylan Baker <baker.dylan.c@gmail.com>
2015-08-11 16:12:58 -0700

    gen_tcs_input_tests.py: fix imports
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    Reviewed-by: Vinson Lee <vlee@freedesktop.org>

--- generated_tests/gen_tcs_input_tests.py
+++ generated_tests/gen_tcs_input_tests.py
@@ -37,7 +37,10 @@ This script outputs, to stdout, the name of each file it generates.
 
 """
 
-import os, sys, random
+from __future__ import print_function, division, absolute_import
+import os
+import sys
+import random
 
 
 class Test(object):
