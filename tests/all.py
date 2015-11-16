fa67e513d6905cc9d057efe1c931b9c54de262b9
Alejandro Piñeiro <apinheiro@igalia.com>
2015-11-16 18:07:07 +0100

    arb_internalformat_query2: add test for COLOR_ENCODING
    From spec:
     "COLOR_ENCODING:
      <skip>
      Possible values for color buffers are LINEAR or SRGB, for linear or
      sRGB-encoded color components, respectively. For non-color formats
      (such as depth or stencil), or for unsupported resources, the value
      NONE is returned."
    
    This tests passes with NVIDIA proprietary drivers.

--- tests/all.py
+++ tests/all.py
@@ -3598,6 +3598,7 @@ with profile.group_manager(
     g(['arb_internalformat_query2-internalformat-type-checks'], 'misc. All INTERNALFORMAT_<X>_TYPE pname checks')
     g(['arb_internalformat_query2-image-format-compatibility-type'], 'misc. IMAGE_FORMAT_COMPATIBILITY_TYPE pname check')
     g(['arb_internalformat_query2-dimensions'], 'misc. dimensions related pname checks')
+    g(['arb_internalformat_query2-color-encoding'], 'misc. COLOR_ENCODING pname check')
 
 with profile.group_manager(
         PiglitGLTest, grouptools.join('spec', 'arb_map_buffer_range')) as g:
