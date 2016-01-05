b25af6876aa6dd9e7ba351cef586d13ad19c4f9d
Dylan Baker <baker.dylan.c@gmail.com>
2016-01-05 15:11:56 -0800

    framework/compat.py: Add a small module with some extra compat
    All of these changes are submitted to upstream six, and are ontrack for
    landing.
    
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    Acked-by: Jose Fonseca <jfonseca@vmware.com>

---
+++ framework/compat.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2016 Intel Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""A small library that adds a single compatability decorator for python 2/3.
+
+This function is pending upstreaming in six.
+
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import six
+
+
+def python_2_bool_compatible(class_):
+    """A decorator to fix __bool__/__nonzero__ name changes."""
+    if six.PY2:
+        try:
+            class_.__nonzero__ = class_.__bool__
+        except AttributeError:
+            raise ValueError(
+                "@python_2_bool_compatible cannot be applied to {} because "
+                "it doesn't define __bool__().".format(class_.__name__))
+    return class_
