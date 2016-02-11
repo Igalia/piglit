ce92183478cc9cf94d104ab3e30e414cc0c0302f
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-02-11 17:49:57 +0100

    arb_gpu_shader_fp64: test Uniform*{1,2,3,4}{d,v} within GS

--- tests/all.py
+++ tests/all.py
@@ -2110,6 +2110,7 @@ with profile.group_manager(
      g(['arb_gpu_shader_fp64-tf-interleaved-aligned'])
      g(['arb_gpu_shader_fp64-vs-getuniformdv'])
      g(['arb_gpu_shader_fp64-fs-getuniformdv'])
+     g(['arb_gpu_shader_fp64-gs-getuniformdv'])
      g(['arb_gpu_shader_fp64-wrong-type-setter'])
      g(['arb_gpu_shader_fp64-double_in_bool_uniform'])
 
