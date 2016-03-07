f3f7e0d362e7b469b5ab7472ec8618a56c3153ef
Andres Gomez <agomez@igalia.com>
2016-03-07 15:39:59 +0200

    arb_gpu_shader_fp64: Cleaned up code and added more tests to the non-flat interpolation generator
    * Now, it generates similar tests for the arb_gpu_shader_fp64
      extension in GLSL 1.50 and for the core feature introduced in 4.00

--- generated_tests/gen_flat_interpolation_qualifier_fp64.py
+++ generated_tests/gen_flat_interpolation_qualifier_fp64.py
@@ -24,7 +24,9 @@
 """Generate non-flat interpolation qualifier tests."""
 
 from __future__ import print_function, division, absolute_import
+import optparse
 import os
+import sys
 import itertools
 
 from templates import template_dir
@@ -32,9 +34,78 @@ from modules import utils
 
 TEMPLATES = template_dir(os.path.basename(os.path.splitext(__file__)[0]))
 
+def get_dir_name(ver):
+    """Returns the directory name to save tests given a GLSL version."""
 
-def generate(dirname, type_name, mode, additional_type_qualifier):
+    assert isinstance(ver, str)
+    if ver == '150':
+        feature_dir = 'arb_gpu_shader_fp64'
+    else:
+        feature_dir = 'glsl-' + ver[0] + '.' + ver[1:]
+
+    return os.path.join('spec', feature_dir, 'compiler',
+                        'flat_interpolation')
+
+
+def get_config(ver, mode):
+    """Returns the config string for a test given a
+       GLSL version and interpolation qualifier mode
+    """
+    assert isinstance(ver, str)
+    config = (' * expect_result: {0}\n'
+              ' * glsl_version: {1}\n'.format('pass' if mode == 'flat' else 'fail',
+                                              ver[0] + '.' + ver[1:]))
+    if ver == '150':
+        config += ' * require_extensions: GL_ARB_gpu_shader_fp64\n'
+    return config
+
+
+def get_preprocessor(ver):
+    """Returns the preprocessor string for a test given a
+       GLSL version
+    """
+    assert isinstance(ver, str)
+    preprocessor = '#version {0}\n'.format(ver)
+    if ver == '150':
+        preprocessor += '#extension GL_ARB_gpu_shader_fp64 : require\n'
+    return preprocessor
+
+
+def get_comments(ver):
+    """Returns the comments string for a test
+       given a GLSL version
+    """
+    assert isinstance(ver, str)
+    if ver == '150':
+        return (
+            ' *\n'
+            ' * GL_ARB_gpu_shader_fp64 spec states:\n'
+            ' *\n'
+            ' *    "Modifications to The OpenGL Shading Language Specification, Version 1.50\n'
+            ' *     (Revision 09)\n'
+            ' *    ...\n'
+            ' *        Modify Section 4.3.4, Inputs, p. 31\n'
+            ' *    ...\n'
+            ' *        (modify third paragraph, p. 32, allowing doubles as inputs and disallowing\n'
+            ' *        as non-flat fragment inputs) ... Fragment inputs can only be signed and\n'
+            ' *        unsigned integers and integer vectors, float, floating-point vectors,\n'
+            ' *        double, double-precision vectors, single- or double-precision matrices, or\n'
+            ' *        arrays or structures of these. Fragment shader inputs that are signed or\n'
+            ' *        unsigned integers, integer vectors, doubles, double-precision vectors, or\n'
+            ' *        double-precision matrices must be qualified with the interpolation\n'
+            ' *        qualifier flat.\n')
+
+    return ''
+
+
+def generate(type_name, mode, additional_type_qualifier, ver, names_only):
     """Generate GLSL parser tests."""
+
+    assert isinstance(type_name, str)
+    assert isinstance(mode, str)
+    assert isinstance(additional_type_qualifier, str)
+    assert isinstance(ver, str)
+    assert isinstance(names_only, bool)
     var_name = 'u'
     var_as_vec4 = 's.' + var_name if additional_type_qualifier == 'struct' else var_name
     if additional_type_qualifier == 'array':
@@ -48,45 +119,68 @@ def generate(dirname, type_name, mode, additional_type_qualifier):
         var_as_vec4 += '.xyzx'
 
     filename = os.path.join(
-        dirname,
+        get_dir_name(ver),
         '{0}-{1}-{2}{3}.frag'.format(mode, additional_type_qualifier, type_name,
                                      '' if mode == 'flat' else '-bad'))
 
     print(filename)
-    with open(filename, 'w') as test_file:
-        test_file.write(TEMPLATES.get_template(
-            '{0}_template.frag.mako'.format(additional_type_qualifier)).render_unicode(
-                expect_result='pass' if mode == 'flat' else 'fail',
-                type_name=type_name,
-                mode=mode,
-                interpolation_mode='' if mode == 'default' else mode + ' ',
-                var_name=var_name,
-                var_as_vec4=var_as_vec4))
+
+    if not names_only:
+        with open(filename, 'w') as test_file:
+            test_file.write(TEMPLATES.get_template(
+                '{0}_template.frag.mako'.format(additional_type_qualifier)).render_unicode(
+                    config=get_config(ver, mode),
+                    mode=mode,
+                    type_name=type_name,
+                    comments=get_comments(ver),
+                    preprocessor=get_preprocessor(ver),
+                    interpolation_mode='' if mode == 'default' else mode + ' ',
+                    var_name=var_name,
+                    var_as_vec4=var_as_vec4))
 
 
-def all_tests():
+def all_tests(names_only):
     """Creates all the combinations for flat qualifier tests."""
+
+    assert isinstance(names_only, bool)
     type_names = ['double', 'dvec2', 'dvec3', 'dvec4',
                   'dmat2', 'dmat2x3', 'dmat2x4',
                   'dmat3x2', 'dmat3', 'dmat3x4',
                   'dmat4x2', 'dmat4x3', 'dmat4']
     modes = ['flat', 'noperspective', 'smooth', 'default']
     additional_type_qualifiers = ['scalar', 'array', 'struct']
+    glsl_ver = ['150', '400']
+    for ver in glsl_ver:
+        utils.safe_makedirs(get_dir_name(ver))
 
-    for t_name, mode, a_t_qualifier in itertools.product(type_names,
-                                                         modes,
-                                                         additional_type_qualifiers):
-        yield t_name, mode, a_t_qualifier
+    for t_name, mode, a_t_qualifier, ver in itertools.product(type_names,
+                                                              modes,
+                                                              additional_type_qualifiers,
+                                                              glsl_ver):
+        yield t_name, mode, a_t_qualifier, ver, names_only
 
 
 def main():
     """Main function."""
-    dirname = os.path.join('spec', 'arb_gpu_shader_fp64', 'compiler',
-                           'flat_interpolation')
-    utils.safe_makedirs(dirname)
 
-    for args in all_tests():
-        generate(dirname, *args)
+    parser = optparse.OptionParser(
+        description="Generate non-flat interpolation qualifier tests with fp64 types",
+        usage="usage: %prog [-h] [--names-only]")
+    parser.add_option(
+        '--names-only',
+        dest='names_only',
+        action='store_true',
+        help="Don't output files, just generate a list of filenames to stdout")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 0:
+        # User gave extra args.
+        parser.print_help()
+        sys.exit(1)
+
+    for args in all_tests(bool(options.names_only)):
+        generate(*args)
 
 
 if __name__ == '__main__':
--- generated_tests/templates/gen_flat_interpolation_qualifier_fp64/struct_template.frag.mako
+++ generated_tests/templates/gen_flat_interpolation_qualifier_fp64/struct_template.frag.mako
@@ -1,30 +1,12 @@
 /* [config]
- * expect_result: ${expect_result}
- * glsl_version: 1.50
- * require_extensions: GL_ARB_gpu_shader_fp64
+${config}\
  * [end config]
  *
  * Declare a ${mode} interpolation ${type_name} in a struct fragment input.
- *
- * GL_ARB_gpu_shader_fp64 spec states:
- *
- *    "Modifications to The OpenGL Shading Language Specification, Version 1.50
- *     (Revision 09)
- *    ...
- *        Modify Section 4.3.4, Inputs, p. 31
- *    ...
- *        (modify third paragraph, p. 32, allowing doubles as inputs and disallowing
- *        as non-flat fragment inputs) ... Fragment inputs can only be signed and
- *        unsigned integers and integer vectors, float, floating-point vectors,
- *        double, double-precision vectors, single- or double-precision matrices, or
- *        arrays or structures of these. Fragment shader inputs that are signed or
- *        unsigned integers, integer vectors, doubles, double-precision vectors, or
- *        double-precision matrices must be qualified with the interpolation
- *        qualifier flat."
+${comments}\
  */
 
-#version 150
-#extension GL_ARB_gpu_shader_fp64 : require
+${preprocessor}\
 
 struct S {
     ${type_name} ${var_name};
