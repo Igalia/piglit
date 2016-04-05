7dd854a40aa53aa0d011a23f6bbab08df1d8af20
Alejandro Piñeiro <apinheiro@igalia.com>
2016-04-05 12:17:10 +0200

    arb_vertex_attrib_64bit: add test zero-vertex-attrib
    ARB_vertex_attrib_64bit spec specifies that calling GetVertexAttribLdv
    with pname CURRENT_VERTEX_ATTRIB and index 0 should raise a
    INVALID_OPERATION error.
    
    That is also true for the rest of GetVertexAttrib* methods depending
    on the spec you read, as some doesn't mention it explicitly, other do
    in core and compatibility and other just on compatibility. Current mesa
    code would only raise the error on compatibility mode, that would
    make this test somewhat useless.
    
    FIXME:
    Asked on piglit list about to ask spec clarification:
    https://lists.freedesktop.org/archives/piglit/2016-April/019348.html
    
    But it is possible that it would be better to just not add this test.

---
+++ tests/spec/arb_vertex_attrib_64bit/execution/zero-vertex-attrib.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2016 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+/**
+ * \file zero-vertex-attrib.c
+ *
+ * Test based on this paragraph of ARB_vertex_attrib_64bit spec:
+ * "  void GetVertexAttribLdv(uint index, enum pname, double *params);
+ * <skip>
+ *  The error INVALID_OPERATION is generated if index
+ *  is zero, as there is no current value for generic attribute zero."
+ *
+ * Also found on 4.1 core spec, section 6.1, page 338:
+ * "The error INVALID_OPERATION is generated if index is zero and
+ * pname is CURRENT_VERTEX_ATTRIB , since there is no current value
+ * for generic attribute zero."
+ *
+ */
+
+#include "piglit-util-gl.h"
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+	config.supports_gl_compat_version = 32;
+	config.supports_gl_core_version = 32;
+
+	config.window_visual = PIGLIT_GL_VISUAL_RGB | PIGLIT_GL_VISUAL_DOUBLE;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+#define CHECK_GL_INVALID_OPERATION \
+	if (glGetError() != GL_INVALID_OPERATION) return PIGLIT_FAIL; \
+	else printf("max-vertex-attrib test %d passed\n", ++test);
+
+enum piglit_result
+piglit_display(void)
+{
+	return PIGLIT_FAIL;
+}
+
+static GLboolean
+run_test(void)
+{
+        enum piglit_result result = PIGLIT_PASS;
+        GLdouble doublev[] = { 1.0, 1.0, 1.0, 1.0 };
+
+        glGetVertexAttribLdv(0, GL_CURRENT_VERTEX_ATTRIB, doublev);
+        if (glGetError () != GL_INVALID_OPERATION) {
+                fprintf(stderr, "GL_INVALID_OPERATION expected when calling "
+                        "GetVertexAttribLdv with index 0 and pname GL_CURRENT_VERTEX_ATTRIB.\n");
+                result = PIGLIT_FAIL;
+        }
+
+        return result;
+}
+
+void piglit_init(int argc, char **argv)
+{
+        piglit_require_gl_version(20);
+
+        piglit_require_extension("GL_ARB_vertex_attrib_64bit");
+
+        piglit_report_result(run_test());
+}
