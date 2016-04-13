1a695c7904ebf71ba53742c320977900f65cd4f4
Andres Gomez <agomez@igalia.com>
2016-04-13 13:41:50 +0300

    dir-locals.el: Added emacs setup based (v2)
    Based on current conventions at .editorconfig
    
    v2: Changed automatic deletion of trailing white spaces in python
        files upon saving for just highlighting them in any format, as
        suggested by Ilia Mirkin.
    
    Signed-off-by: Andres Gomez <agomez@igalia.com>
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+((nil . ((indent-tabs-mode . t)
+	 (tab-width . 8)
+	 (show-trailing-whitespace . t)))
+ (prog-mode .
+	    ((c-file-style . "linux")))
+ (cmake-mode .
+	     ((cmake-tab-width . 8)))
+ (python-mode .
+	      ((indent-tabs-mode . nil)
+	       (tab-width . 4)))
+ )
