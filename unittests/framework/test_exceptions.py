2b99b437aa58a24a2d1e14ede38085833df9f930
Dylan Baker <dylan@pnwbakers.com>
2016-07-06 14:03:37 -0700

    unittests: port exceptions_tests to pytest
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>

--- unittests/framework/test_exceptions.py
+++ unittests/framework/test_exceptions.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015 Intel Corporation
+# Copyright (c) 2015-2016 Intel Corporation
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,14 +24,15 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-import nose.tools as nt
+import pytest
 
-from . import utils
 from framework import exceptions
 
+# Good test names often are not PEP8
+# pylint: disable=invalid-name
 
-@nt.raises(SystemExit)
-@utils.nose.capture_stderr
+
+@pytest.mark.raises(exception=SystemExit)
 @exceptions.handler
 def test_handle_PiglitFatalError():
     """exceptions.handler: Handles PiglitFatalError"""
