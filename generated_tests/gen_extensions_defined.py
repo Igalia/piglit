6fa3371ce17c9b7771c9bbd0645f0ebf52a0b7f4
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2016-11-08 14:55:26 +0000

    gen_extensions_defined.py: add NV_image_formats
    This extensions is written against OpenGL ES 3.1 and 3.10 of the OpenGL ES
    Shading Language specification.
    
    v2: Fix missing 'GL_' prefix (Ilia)
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- generated_tests/gen_extensions_defined.py
+++ generated_tests/gen_extensions_defined.py
@@ -127,6 +127,7 @@ EXTENSIONS = [
     ("GL_EXT_texture_buffer", "310 es"),
     ("GL_OES_texture_buffer", "310 es"),
     ("GL_EXT_clip_cull_distance", "300 es"),
+    ("GL_NV_image_formats", "310 es"),
 ]
 EXTENSIONS = [(n, glsl.Version(v)) for n, v in EXTENSIONS]
 
