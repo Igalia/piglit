642f39fd3402840da9d2c45bb2a4bcd69ca8e1bf
Timothy Arceri <tarceri@itsqueeze.com>
2017-05-13 13:17:38 +1000

    tests: mark KHR_no_error incompatible tests
    There is still a huge number of tests to classify but this gives us
    a good number of tests to start testing with.

--- tests/spec/arb_es2_compatibility/arb_es2_compatibility-fixed-type.c
+++ tests/spec/arb_es2_compatibility/arb_es2_compatibility-fixed-type.c
@@ -32,6 +32,7 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 	config.window_width = 250;
 	config.window_height = 250;
 	config.window_visual = PIGLIT_GL_VISUAL_RGBA | PIGLIT_GL_VISUAL_DOUBLE;
+	config.khr_no_error_support = PIGLIT_HAS_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
 
