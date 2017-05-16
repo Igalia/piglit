3375cdb384ec8a2e83aa05fdd6e9414da8e7ca83
Timothy Arceri <tarceri@itsqueeze.com>
2017-05-16 21:40:29 +1000

    arb_draw_indirect: set KHR_no_error compatibility
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- tests/spec/arb_draw_indirect/draw-arrays-prim-restart.c
+++ tests/spec/arb_draw_indirect/draw-arrays-prim-restart.c
@@ -31,6 +31,7 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 	config.supports_gl_compat_version = 33;
 	config.supports_gl_core_version = 33;
 	config.window_visual = PIGLIT_GL_VISUAL_RGBA | PIGLIT_GL_VISUAL_DOUBLE;
+	config.khr_no_error_support = PIGLIT_NO_ERRORS;
 PIGLIT_GL_TEST_CONFIG_END
 
 static const GLchar *vstext =
