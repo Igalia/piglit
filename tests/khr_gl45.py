9a22906394a30405ba39fbe4a2964272d7771fa1
Andres Gomez <agomez@igalia.com>
2017-06-07 23:05:40 +0300

    tests: Added a couple of new test profiles for the Khronos CTS runner
    After:
    https://github.com/KhronosGroup/VK-GL-CTS/commit/af8c22a343ee2c230488f6de71b36dc3070b2024
    
    OpenGL GL*-CTS case lists were renamed to KHR-GL*.
    
    We want to keep the existing profiles so we are able to keep running
    the caselists from previous CTS releases and for branches in the open
    sourced repository created for API-specific release branches, as
    explained at:
    https://github.com/KhronosGroup/VK-GL-CTS/wiki/Contributing#branches
    
    Therefore, we add these 2 new test profiles to be able to run the
    newly named tests in the master branch of the opensourced Khronos CTS
    tests at:
    https://github.com/KhronosGroup/VK-GL-CTS
    
    Signed-off-by: Andres Gomez <agomez@igalia.com>
    Cc: Mark Janes <mark.a.janes@intel.com>
    Cc: Dylan Baker <dylanx.c.baker@intel.com>
    Cc: Marek Olšák <marek.olsak@amd.com>
    Acked-by: Dylan Baker <dylan@pnwbakers.com>
    Tested-by: Mark Janes <mark.a.janes@intel.com>
    Acked-by: Marek Olšák <marek.olsak@amd.com>

---
+++ tests/khr_gl45.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2017 Intel Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Piglit integration for the now open sourced Khronos CTS tests being
+developed at https://github.com/KhronosGroup/VK-GL-CTS
+
+This will run GL45 test cases only.
+
+This integration requires some configuration in piglit.conf, or the use of
+environment variables.
+
+In piglit.conf one should set the following:
+[khr_gl]:bin -- Path to the glcts binary
+[khr_gl]:extra_args -- any extra arguments to be passed to cts (optional)
+
+Alternatively (or in addition, since environment variables have precedence),
+one could set:
+PIGLIT_KHR_GL_BIN -- environment equivalent of [khr_gl]:bin
+PIGLIT_KHR_GL_EXTRA_ARGS -- environment equivalent of [khr_gl]:extra_args
+
+"""
+
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+import itertools
+
+from framework.test import deqp
+
+__all__ = ['profile']
+
+_KHR_BIN = deqp.get_option('PIGLIT_KHR_GL_BIN', ('khr_gl', 'bin'),
+                           required=True)
+
+_EXTRA_ARGS = deqp.get_option('PIGLIT_KHR_GL_EXTRA_ARGS', ('khr_gl', 'extra_args'),
+                              default='').split()
+
+
+class DEQPKHRTest(deqp.DEQPBaseTest):
+    deqp_bin = _KHR_BIN
+
+    @property
+    def extra_args(self):
+        return super(DEQPKHRTest, self).extra_args + \
+            [x for x in _EXTRA_ARGS if not x.startswith('--deqp-case')]
+
+profile = deqp.make_profile(  # pylint: disable=invalid-name
+    itertools.chain(
+        deqp.iter_deqp_test_cases(
+            deqp.gen_caselist_txt(_KHR_BIN, 'KHR-GL45-cases.txt', _EXTRA_ARGS)),
+    ),
+    DEQPKHRTest)
