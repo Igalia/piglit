16548b635de7f4d82b591cb3504266ab1547729b
Juan A. Suarez Romero <jasuarez@igalia.com>
2017-06-22 17:38:01 +0200

    Revert "arb_get_texture_sub_image-errors: Fix expected error values"
    In commit aec149c we test that glGetTextureSubImage() and
    glGetCompressedTextureSubImage() returns INVALID_VALUE when the texture
    is not the name of an existing texture object.
    
    Nevertheless, this is a bug in the OpenGL 4.5 spec itself, that has been
    fixed, but not released yet.
    
    In the fix the expected error value for this case is INVALID_OPERATION.
    
    This reverts commit aec149c836bcd29d4764c8f459fdc45613d18c8c.
    
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- tests/spec/arb_get_texture_sub_image/errors.c
+++ tests/spec/arb_get_texture_sub_image/errors.c
@@ -47,7 +47,7 @@ test_texture_id(void)
 			     8, 8, 1, /* size */
 			     GL_RGBA, GL_UNSIGNED_BYTE,
 			     sizeof(buffer), buffer);
-	if (!piglit_check_gl_error(GL_INVALID_VALUE))
+	if (!piglit_check_gl_error(GL_INVALID_OPERATION))
 		pass = false;
 
 	/* Test compressed get with bad texture ID */
@@ -55,7 +55,7 @@ test_texture_id(void)
 				       0, 0, 0, /* offset */
 				       8, 8, 1, /* size */
 				       sizeof(buffer), buffer);
-	if (!piglit_check_gl_error(GL_INVALID_VALUE))
+	if (!piglit_check_gl_error(GL_INVALID_OPERATION))
 		pass = false;
 
 	/* Test get with undefined texture */
