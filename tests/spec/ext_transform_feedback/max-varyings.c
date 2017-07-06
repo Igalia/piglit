5b459a9d81a68eff83a5badd633eb63c90567a22
Timothy Arceri <tarceri@itsqueeze.com>
2017-07-06 13:51:12 +1000

    ext_transform_feedback: set KHR_no_error status
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- tests/spec/ext_transform_feedback/max-varyings.c
+++ tests/spec/ext_transform_feedback/max-varyings.c
@@ -51,6 +51,7 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 	config.window_width = (2+MAX_VARYING*12);
 	config.window_height = (2+MAX_VARYING*12);
 	config.window_visual = PIGLIT_GL_VISUAL_RGB | PIGLIT_GL_VISUAL_DOUBLE;
+	config.khr_no_error_support = PIGLIT_NO_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
 
