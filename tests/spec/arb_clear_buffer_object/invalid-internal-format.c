7293527955ba16b3e645ed7d7aadf8060aaad7a5
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-08-09 18:39:04 +0200

    arb_clear_buffer_object: set KHR_no_error status
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- tests/spec/arb_clear_buffer_object/invalid-internal-format.c
+++ tests/spec/arb_clear_buffer_object/invalid-internal-format.c
@@ -46,6 +46,7 @@
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.supports_gl_compat_version = 15;
+	config.khr_no_error_support = PIGLIT_HAS_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
 
--- tests/spec/arb_clear_buffer_object/mapped.c
+++ tests/spec/arb_clear_buffer_object/mapped.c
@@ -35,6 +35,7 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.supports_gl_compat_version = 15;
 	config.supports_gl_core_version = 31;
+	config.khr_no_error_support = PIGLIT_HAS_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
 
