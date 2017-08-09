619e3caaee252e57ae6f64ddfed063907e99fd3f
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-08-09 16:59:35 +0200

    ext_texture_integer: set KHR_no_error status
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- tests/spec/ext_texture_integer/api-drawpixels.c
+++ tests/spec/ext_texture_integer/api-drawpixels.c
@@ -62,6 +62,7 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.supports_gl_compat_version = 10;
 	config.window_visual = PIGLIT_GL_VISUAL_RGBA | PIGLIT_GL_VISUAL_DOUBLE;
+	config.khr_no_error_support = PIGLIT_HAS_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
 
