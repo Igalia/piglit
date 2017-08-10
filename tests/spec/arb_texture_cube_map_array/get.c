27ad72ab3f63ba7a44659a4c80b64d511e480c0a
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-08-10 14:51:55 +0200

    arb_texture_cube_map_array: set KHR_no_error status
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- tests/spec/arb_texture_cube_map_array/get.c
+++ tests/spec/arb_texture_cube_map_array/get.c
@@ -36,6 +36,8 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 
     config.window_visual = PIGLIT_GL_VISUAL_DOUBLE | PIGLIT_GL_VISUAL_RGBA;
 
+    config.khr_no_error_support = PIGLIT_NO_ERRORS;
+
 PIGLIT_GL_TEST_CONFIG_END
 
 enum piglit_result
--- tests/spec/arb_texture_cube_map_array/teximage3d-invalid-values.c
+++ tests/spec/arb_texture_cube_map_array/teximage3d-invalid-values.c
@@ -39,6 +39,7 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
     config.window_width= 32;
     config.window_height = 32;
     config.window_visual = PIGLIT_GL_VISUAL_DOUBLE | PIGLIT_GL_VISUAL_RGBA;
+    config.khr_no_error_support = PIGLIT_HAS_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
 
