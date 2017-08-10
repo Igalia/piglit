90237f6f2a9abf79bcd95c8f0f52952a703b260f
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-08-10 13:49:23 +0200

    khr_debug: set KHR_no_error status
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- tests/spec/khr_debug/debug-object-label.c
+++ tests/spec/khr_debug/debug-object-label.c
@@ -36,6 +36,7 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 #endif
 
 	config.window_visual = PIGLIT_GL_VISUAL_RGBA | PIGLIT_GL_VISUAL_DOUBLE;
+	config.khr_no_error_support = PIGLIT_HAS_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
 
