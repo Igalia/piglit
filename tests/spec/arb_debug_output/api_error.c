26f889c24f73da506b994f347065960a7c577cdd
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-08-10 14:06:41 +0200

    arb_debug_output: set KHR_no_error status
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- tests/spec/arb_debug_output/api_error.c
+++ tests/spec/arb_debug_output/api_error.c
@@ -31,6 +31,8 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.window_visual = PIGLIT_GL_VISUAL_RGBA | PIGLIT_GL_VISUAL_DOUBLE;
 
+	config.khr_no_error_support = PIGLIT_HAS_ERRORS;
+
 PIGLIT_GL_TEST_CONFIG_END
 
 #define USER_PARAM ((void*)(intptr_t)12345678)
