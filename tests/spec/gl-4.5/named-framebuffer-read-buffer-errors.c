07069cc61093b69375b8f5c962a373085bdb683d
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-08-10 16:21:42 +0200

    gl-4.5: set KHR_no_error status
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- tests/spec/gl-4.5/named-framebuffer-read-buffer-errors.c
+++ tests/spec/gl-4.5/named-framebuffer-read-buffer-errors.c
@@ -59,6 +59,8 @@ PIGLIT_GL_TEST_CONFIG_BEGIN
 	config.window_visual = PIGLIT_GL_VISUAL_RGBA |
 		PIGLIT_GL_VISUAL_DOUBLE;
 
+	config.khr_no_error_support = PIGLIT_HAS_ERRORS;
+
 PIGLIT_GL_TEST_CONFIG_END
 
 static const GLenum table_17_4[] = {
