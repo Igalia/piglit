7ae111093f6486ce09b7709ae662e40adbf7df51
Alejandro Piñeiro <apinheiro@igalia.com>
2017-09-23 09:14:00 +0200

    framework: add --spirv option
    So when executing shader tests, they will be executed with -spirv.
    
    So for example:
    ./piglit run tests/shader.py -t ARB_explicit_uniform_location --spirv results/explicit_uniform_location-spirv
    
    Will run all the shader tests for that extension using -spirv.

--- framework/programs/run.py
+++ framework/programs/run.py
@@ -222,6 +222,10 @@ def _run_parser(input_):
                         type=path.realpath,
                         metavar="<Results Path>",
                         help="Path to results folder")
+    parser.add_argument("--spirv",
+                        action="store_true",
+                        help="Run shader runner tests with the -spirv option")
+
     return parser.parse_args(unparsed)
 
 
@@ -296,6 +300,7 @@ def run(input_):
     options.OPTIONS.sync = args.sync
     options.OPTIONS.deqp_mustpass = args.deqp_mustpass
     options.OPTIONS.process_isolation = args.process_isolation
+    options.OPTIONS.spirv = args.spirv
 
     # Set the platform to pass to waffle
     options.OPTIONS.env['PIGLIT_PLATFORM'] = args.platform
@@ -398,6 +403,7 @@ def resume(input_):
     options.OPTIONS.sync = results.options['sync']
     options.OPTIONS.deqp_mustpass = results.options['deqp_mustpass']
     options.OPTIONS.process_isolation = results.options['process_isolation']
+    options.OPTIONS.spirv = results.options['spirv']
 
     core.get_config(args.config_file)
 
