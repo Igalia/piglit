2589e514d54cab8f7849dde8169eda9fc384fcdd
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-03 13:03:41 -0400

    cl: Fix sinpi expected values
    account for fp32 rounding.
    sinpi(n) should return 0 for any integer n
    
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- generated_tests/gen_cl_math_builtins.py
+++ generated_tests/gen_cl_math_builtins.py
@@ -696,12 +696,11 @@ tests = {
         'arg_types' : [F, F],
         'function_type': 'ttt',
         'values' : [
-            [0.0, 0.0, sin(pi*pi/2), sin(pi*3*pi/2), sin(2*pi*pi),
-             sin(pi*1.12345), sin(pi*7), sin(pi*8), sin(pi*pow(2,20)),
-             sin(pi*pow(2,24)), sin(pi*pow(2,120)), float("nan")],#Result
+            [0.0, 0.0, sin(pi*pi/2), sin(pi*3*pi/2), sin(2* M_PI_F * pi),
+             sin(pi*1.12345), 0.0, 0.0, 0.0, 0.0, 0.0, float("nan")],#Result
             [0.0, 1.0, pi / 2,       3 * pi / 2,     2 * pi,
-             1.12345        , 7,         8,         pow(2,20),
-             pow(2,24),         pow(2,120),         float("nan")] #Arg0
+             1.12345        , 7.0, 8.0, pow(2,20), pow(2,24), pow(2,120),
+             float("nan")] #Arg0
         ],
         'tolerance' : 4
     },
