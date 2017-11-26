08f2bc02de11059b0a507241a7a74f5b081cea7a
Fabian Bieler <fabianbieler@fastmail.fm>
2017-11-26 22:15:45 +0100

    Move r300-readcache test from bugs to spec dir.
    Now it's called "read-cache-stress-test".
    
    Moved from opengl 1.1 to opengl 1.0 profile in all.py.
    
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- tests/spec/gl-1.0/read-cache-stress-test.c
+++ tests/spec/gl-1.0/read-cache-stress-test.c
@@ -1,7 +1,30 @@
-/**
- * @file r300-readcache.c
+/*
+ * Copyright © 2008 Nicolai Hähnle
  *
- * Test case for an odd problem in Radeon on-chip readcache.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * @file read-cache-stress-test.c
+ *
+ * Test case for an odd problem in Radeon R300 on-chip readcache.
  *
  * Basically, on some particular access patterns, the read cache misses the
  * fact that the framebuffer has changed, and a glReadPixels returns stale
@@ -19,7 +42,7 @@
 
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
-	config.supports_gl_compat_version = 10;
+	config.supports_gl_compat_version = 11;
 
 	config.window_visual = PIGLIT_GL_VISUAL_DOUBLE | PIGLIT_GL_VISUAL_RGB;
 	config.khr_no_error_support = PIGLIT_NO_ERRORS;
