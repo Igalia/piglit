31e484899c6ac577787ed6642184f57a61528729
Neil Roberts <nroberts@igalia.com>
2017-12-15 10:36:04 +0100

    arb_enhanced_layouts: Test XFB layout qualifiers via SPIR-V

---
+++ tests/spec/arb_enhanced_layouts/spirv/compile-spirv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+glslc="../../../../../shaderc/build/glslc/glslc"
+
+shaders=(
+	vs_two_sets_ifc_text \
+	vs_two_sets_named_ifc_text \
+	vs_two_sets_struct_text \
+	vs_two_sets_text \
+)
+
+for shader in "${shaders[@]}"; do
+    prefix=$(echo "$shader" | sed 's/_.*//')
+    case "$prefix" in
+	gs) stage=geom ;;
+	vs) stage=vert ;;
+	fs) stage=frag ;;
+	*) echo "unknown shader prefix: $prefix" >&2; exit 1;;
+    esac
+    "$glslc" --target-env=opengl -fshader-stage="$stage" \
+	     -o "$shader.spirv" \
+	     "$shader.glsl"
+done
