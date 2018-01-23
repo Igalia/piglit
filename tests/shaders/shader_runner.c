276b6e63b3d0a21ecffe525f133e9436da8bfec4
Alejandro Piñeiro <apinheiro@igalia.com>
2018-01-23 17:55:11 +0100

    shader_runner: debug info
    Adds the following debug info:
       * If running on GLSL or SPIR-V mode
       * On GLSL, if forcing no-names

--- tests/shaders/shader_runner.c
+++ tests/shaders/shader_runner.c
@@ -4276,12 +4276,20 @@ piglit_init(int argc, char **argv)
 	report_subtests = piglit_strip_arg(&argc, argv, "-report-subtests");
 	spirv_replaces_glsl = piglit_strip_arg(&argc, argv, "-spirv");
 	force_no_names = piglit_strip_arg(&argc, argv, "-force-no-names");
-	if (spirv_replaces_glsl)
+	if (spirv_replaces_glsl) {
+                printf("Running on SPIR-V mode\n");
 		force_no_names = true;
+	} else {
+                printf("Running on GLSL mode\n");
+        }
+
 	if (argc < 2) {
 		printf("usage: shader_runner <test.shader_test>\n");
 		exit(1);
 	}
+
+	if (force_no_names & !spirv_replaces_glsl)
+		fprintf(stderr, "Note: forcing names\n");
 
 	memcpy(default_piglit_tolerance, piglit_tolerance,
 	       sizeof(piglit_tolerance));
