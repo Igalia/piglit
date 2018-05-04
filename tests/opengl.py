38ce7a6e9472ddfea1d78518b5faae2f7b329732
Neil Roberts <nroberts@igalia.com>
2018-05-04 12:27:21 +0200

    arb_gl_spirv: Add a test for the SamplePosition builtin
    Tests whether setting OriginUpperLeft and OriginLowerLeft affects the
    SamplePosition builtin. It draws a grid of rectangles into a
    multi-sample framebuffer where each rectangle only covers the top half
    of the pixel. The fragment shaders store the sample position in the
    buffer. It then uses multisample texturing to read back the samples.
    Any samples written by the fragment shader should have the y position
    all greater than 0.5. It’s not clear whether changing the origin
    should affect this and there is an open spec issue about it, but for
    now this test assumes that it shouldn’t.

--- tests/opengl.py
+++ tests/opengl.py
@@ -4955,5 +4955,14 @@ with profile.test_list.group_manager(
         grouptools.join('spec', 'NV_image_formats')) as g:
     g(['nv_image_formats-gles3'])
 
+# Group ARB_gl_spirv
+with profile.test_list.group_manager(
+    PiglitGLTest,
+    grouptools.join('spec', 'ARB_gl_spirv')) as g:
+    g(['arb_gl_spirv-sample-position', 'OriginUpperLeft'],
+      'sample-position-upper-left')
+    g(['arb_gl_spirv-sample-position', 'OriginLowerLeft'],
+      'sample-position-lower-left')
+
 if platform.system() is 'Windows':
     profile.filters.append(lambda p, _: not p.startswith('glx'))
