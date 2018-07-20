Igalia/piglit

commit 1f0f2ad25ecbfdcb09ec764404ec365c6b6946c1

framework: Add filters class
This class works like a list, but with an extra method that allows it to
"reset" a filter. This will allow class based filters that need to be
reset to have deterministic behavior to do so.

Tested-by: Michel Dänzer <michel.daenzer@amd.com>

framework/profile.py
@@ -265,6 +265,42 @@ class TestDict(collections.MutableMapping):
         self.__allow_reassignment -= 1
 
 
+class Filters(collections.MutableSequence):
+
+    def __init__(self, iterable=None):
+        if iterable:
+            self.__container = list(iterable)
+        else:
+            self.__container = []
+
+    def __getitem__(self, index):
+        return self.__container[index]
+
+    def __setitem__(self, index, value):
+        self.__container[index] = value
+
+    def __delitem__(self, index):
+        del self.__container[index]
+
+    def __len__(self):
+        return len(self.__container)
+
+    def __add__(self, other):
+        return type(self)(itertools.chain(iter(self), iter(other)))
+
+    def insert(self, index, value):
+        self.__container.insert(index, value)
+
+    def run(self, iterable):
+        for f in self.__container:
+            if hasattr(f, 'reset'):
+                f.reset()
+
+        for k, v in iterable:
+            if all(f(k, v) for f in self.__container):
+                yield k, v
+
+
 def make_test(element):
     """Rebuild a test instance from xml."""
     def process(elem, opt):
@@ -309,7 +345,7 @@ class XMLProfile(object):
     def __init__(self, filename):
         self.filename = filename
         self.forced_test_list = []
-        self.filters = []
+        self.filters = Filters()
         self.options = {
             'dmesg': get_dmesg(False),
             'monitor': Monitoring(False),
@@ -369,7 +405,7 @@ class MetaProfile(object):
 
     def __init__(self, filename):
         self.forced_test_list = []
-        self.filters = []
+        self.filters = Filters()
         self.options = {
             'dmesg': get_dmesg(False),
             'monitor': Monitoring(False),
@@ -436,7 +472,7 @@ class TestProfile(object):
     def __init__(self):
         self.test_list = TestDict()
         self.forced_test_list = []
-        self.filters = []
+        self.filters = Filters()
         self.options = {
             'dmesg': get_dmesg(False),
             'monitor': Monitoring(False),

commit 57505c99e6d3acf24b1f41de4fc56f83ec94a58d

framework: deepcopy TestProfile's filters when copying it
This avoids modifications in the original filters object.

Detected through Travis CI.

Fixed also a couple of typos.

Fixes: 1f0f2ad25 ("framework: Add filters class")
Cc: Dylan Baker <dylanx.c.baker@intel.com>
Cc: Michel Dänzer <michel.daenzer@amd.com>
Signed-off-by: Andres Gomez <agomez@igalia.com>

framework/profile.py
@@ -501,11 +501,14 @@ class TestProfile(object):
         This method creates a copy with references to the original instance
         using copy.copy. This allows profiles to be "subclassed" by other
         profiles, without modifying the original.
+
+        copy.deepcopy is used for the filters so the original is
+        actually not modified in this case.
         """
         new = copy.copy(self)
         new.test_list = copy.copy(self.test_list)
         new.forced_test_list = copy.copy(self.forced_test_list)
-        new.filters = copy.copy(self.filters)
+        new.filters = copy.deepcopy(self.filters)
         return new
 
     def itertests(self):

unittests/framework/summary/test_feature.py
@@ -77,7 +77,7 @@ PROFILE.test_list['spec@gl-2.0@d'] = _maketest('fail')
 
 
 class TestFeatResult(object):
-    """Tests fro the FeatResult class."""
+    """Tests for the FeatResult class."""
 
     @pytest.fixture(scope='session')
     def feature(self, tmpdir_factory):

unittests/framework/utils.py
@@ -32,7 +32,7 @@ from framework.test.base import Test as _Test
 
 
 class Test(_Test):
-    """A Test dericed class with a stub interpret_result.
+    """A Test derived class with a stub interpret_result.
 
     This class provides a way to test the Test class.
     """