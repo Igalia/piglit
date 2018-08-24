ae9292b09ffdd3fa7deef8b4c4582ff0cb8583c3
Alejandro Piñeiro <apinheiro@igalia.com>
2018-08-24 11:22:56 +0200

    shader_test_spirv: automatic check for more unsupported extensions

--- generated_spv/shader_test_spirv.py
+++ generated_spv/shader_test_spirv.py
@@ -985,9 +985,16 @@ def process_shader_test(shader_test, config):
         # Pretty much inherently unsupported
         'GL_ARB_geometry_shader4',
         'GL_ARB_shader_subroutine',
+        'GL_ARB_compute_variable_group',
+        'GL_ARB_compute_variable_group_size',
+        'GL_EXT_shader_framebuffer_fetch',
+        'GL_EXT_shader_framebuffer_fetch_non_coherent',
+        'GL_NV_shader_atomic_float',
 
         # glslang limitation
         'GL_MESA_shader_integer_functions',
+        'GL_ARB_shader_atomic_counter_ops',
+        'GL_INTEL_shader_atomic_float_minmax'
     ])
 
     shaders = []
