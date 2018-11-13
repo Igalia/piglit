b84a94113eea238cd6eb8753899f177a3095587a
Neil Roberts <nroberts@igalia.com>
2018-11-13 21:35:47 +0100

    vulkan: Prefix group names for VkRunner tests with “vulkan”
    This will make it easier to distinguish tests written for Vulkan. It
    makes a bit of an inconsistency because the GL and CL tests don’t have
    any prefix. Ideally maybe we would add a prefix for those too, but
    changing the test names at this late stage would probably cause a lot
    of hassle.
    
    Adding the prefix will avoid problems if we eventually decide to merge
    the Vulkan profile into the “quick” profile because otherwise we risk
    having name collisions when the same thing is tested on both Vulkan
    and GL.
    
    Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
    Reviewed-by: Mark Janes <mark.a.janes@intel.com>
    Tested-by: Mark Janes <mark.a.janes@intel.com>

--- tests/vulkan.py
+++ tests/vulkan.py
@@ -22,6 +22,7 @@ for basedir in [TESTS_DIR, GENERATED_TESTS_DIR]:
     _basedir = os.path.join(basedir, 'vulkan')
     for dirpath, _, filenames in os.walk(_basedir):
         groupname = grouptools.from_path(os.path.relpath(dirpath, _basedir))
+        groupname = grouptools.join('vulkan', groupname)
         dirname = os.path.relpath(dirpath, os.path.join(basedir, '..'))
         for filename in filenames:
             testname, ext = os.path.splitext(filename)
