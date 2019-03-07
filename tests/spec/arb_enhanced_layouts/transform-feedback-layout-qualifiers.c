aa4f85235e8d2e811764fc5bf361b657b33a2db1
Alejandro Piñeiro <apinheiro@igalia.com>
2019-03-07 17:52:14 +0100

    arb_enhanced_layouts: revert testing SPIR-V shaders on arb_enhanced_layouts tests
    At that point, as shader_runner lacked xfb support, it was really
    convenient to add SPIR-V support on those tests. Now that we added XFB
    support on shader_runner, it is really easy to add new tests. So this
    is not needed, and makes thing more complex, so it is better to just
    go to the original state.
    
    FWIW, this reverts three commits:
     * "arb_enhanced_layouts: don't call glLinkProgram twice"
     * "arb_enhanced_layouts: Test doubles"
     * "arb_enhanced_layouts: Test XFB layout qualifiers via SPIR-V"
    
    Acked-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

--- tests/spec/arb_enhanced_layouts/transform-feedback-layout-qualifiers.c
+++ tests/spec/arb_enhanced_layouts/transform-feedback-layout-qualifiers.c
@@ -23,8 +23,6 @@
 
 #include "piglit-util-gl.h"
 
-#include "piglit-shader-test.h"
-
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
 	config.supports_gl_compat_version = 32;
@@ -38,15 +36,105 @@ static const char vs_pass_thru_text[] =
 	"  gl_Position = vec4(0.0);\n"
 	"}\n";
 
-#define VS_TWO_SETS_NAME "vs_two_sets.shader_source"
+static const char vs_two_sets_text[] =
+	"#version 150\n"
+	"#extension GL_ARB_enhanced_layouts: require\n"
+	"\n"
+	"layout(xfb_offset = 0) out float x1_out;\n"
+	"layout(xfb_offset = 4) out float x2_out[2];\n"
+	"layout(xfb_offset = 12) out vec3 x3_out;\n"
+	"layout(xfb_buffer = 2) out;\n"
+	"layout(xfb_offset = 0, xfb_buffer = 2) out float y1_out;\n"
+	"layout(xfb_offset = 4) out vec4 y2_out;\n"
+	"void main() {\n"
+	"  gl_Position = vec4(0.0);\n"
+	"  x1_out = 1.0;\n"
+	"  x2_out[0] = 2.0;\n"
+	"  x2_out[1] = 3.0;\n"
+	"  x3_out = vec3(4.0, 5.0, 6.0);\n"
+	"  y1_out = 7.0;\n"
+	"  y2_out = vec4(8.0, 9.0, 10.0, 11.0);\n"
+	"}";
 
-#define VS_TWO_SETS_IFC_NAME "vs_two_sets_ifc.shader_source"
+static const char vs_two_sets_ifc_text[] =
+	"#version 150\n"
+	"#extension GL_ARB_enhanced_layouts: require\n"
+	"\n"
+	"out block {\n"
+	"  layout(xfb_offset = 12) out float x1_out;\n"
+	"  layout(xfb_offset = 16) out vec2 x2_out;\n"
+	"  layout(xfb_buffer = 0) out vec3 not_captured;\n"
+	"  layout(xfb_offset = 0) out vec3 x3_out;\n"
+	"};"
+	"layout(xfb_buffer = 2) out;\n"
+	"layout(xfb_offset = 0) out block2 {\n"
+	"  float y1_out;\n"
+	"  vec4 y2_out;\n"
+	"};\n"
+	"void main() {\n"
+	"  gl_Position = vec4(0.0);\n"
+	"  x1_out = 4.0;\n"
+	"  x2_out = vec2(5.0, 6.0);\n"
+	"  x3_out = vec3(1.0, 2.0, 3.0);\n"
+	"  y1_out = 7.0;\n"
+	"  y2_out = vec4(8.0, 9.0, 10.0, 11.0);\n"
+	"}";
 
-#define VS_TWO_SETS_NAMED_IFC_NAME "vs_two_sets_named_ifc.shader_source"
+static const char vs_two_sets_named_ifc_text[] =
+	"#version 150\n"
+	"#extension GL_ARB_enhanced_layouts: require\n"
+	"\n"
+	"out block {\n"
+	"  layout(xfb_offset = 0) out float x1_out;\n"
+	"  layout(xfb_offset = 4) out vec2 x2_out;\n"
+	"  layout(xfb_buffer = 0) out vec3 not_captured;\n"
+	"  layout(xfb_offset = 12) out vec3 x3_out;\n"
+	"} x;"
+	"layout(xfb_buffer = 2) out;\n"
+	"layout(xfb_offset = 0) out block2 {\n"
+	"  float y1_out;\n"
+	"  vec4 y2_out;\n"
+	"} y;\n"
+	"void main() {\n"
+	"  gl_Position = vec4(0.0);\n"
+	"  x.x1_out = 1.0;\n"
+	"  x.x2_out = vec2(2.0, 3.0);\n"
+	"  x.x3_out = vec3(4.0, 5.0, 6.0);\n"
+	"  y.y1_out = 7.0;\n"
+	"  y.y2_out = vec4(8.0, 9.0, 10.0, 11.0);\n"
+	"}";
 
-#define VS_TWO_SETS_STRUCT_NAME "vs_two_sets_struct.shader_source"
-
-#define VS_DOUBLE_NAME "vs_double.shader_source"
+static const char vs_two_sets_struct_text[] =
+	"#version 150\n"
+	"#extension GL_ARB_enhanced_layouts: require\n"
+	"\n"
+	"struct Array {\n"
+	"  float x2_out;\n"
+	"};\n"
+	"struct AoA {\n"
+	"  Array x2_Array[2];\n"
+	"};\n"
+	"struct S {\n"
+	"  float x1_out;\n"
+	"  AoA x2_AoA[2];\n"
+	"  float x3_out;\n"
+	"};"
+	"layout(xfb_offset = 0) out S s1;\n"
+	"layout(xfb_offset = 0, xfb_buffer = 2) out struct S2 {\n"
+	"  float y1_out;\n"
+	"  vec4 y2_out;\n"
+	"} s2;\n"
+	"void main() {\n"
+	"  gl_Position = vec4(0.0);\n"
+	"  s1.x1_out = 1.0;\n"
+	"  s1.x2_AoA[0].x2_Array[0].x2_out = 2.0;\n"
+	"  s1.x2_AoA[0].x2_Array[1].x2_out = 3.0;\n"
+	"  s1.x2_AoA[1].x2_Array[0].x2_out = 4.0;\n"
+	"  s1.x2_AoA[1].x2_Array[1].x2_out = 5.0;\n"
+	"  s1.x3_out = 6.0;\n"
+	"  s2.y1_out = 7.0;\n"
+	"  s2.y2_out = vec4(8.0, 9.0, 10.0, 11.0);\n"
+	"}";
 
 static const char gs_text_two_sets_tmpl[] =
 	"#version 150\n"
@@ -79,116 +167,43 @@ static const char gs_text_two_sets_tmpl[] =
 	"  EndPrimitive();\n"
 	"}";
 
-struct test_config {
-	const char *shader_source_filename;
-	bool spirv;
-	GLint gs_invocation_n;
-	bool doubles;
-};
-
 #define BUF_1_FLOAT_N 6
 #define BUF_2_FLOAT_N 5
-#define BUF_DOUBLE_N 7
-
 
 static void
 print_usage_and_exit(const char *prog_name)
 {
-	printf("Usage: %s <subtest> [spirv]\n"
+	printf("Usage: %s <subtest>\n"
 	       "  where <subtest> is one of the following:\n"
 	       "    vs (vertex shader only)\n"
 	       "    vs_ifc (vertex shader only, with interface block)\n"
 	       "    vs_named_ifc (vertex shader only, with named interface block)\n"
 	       "    vs_struct (vertex shader only, with structs)\n"
-	       "    vs_double (vertex shader only, using doubles)\n"
 	       "    gs (with geometry shader invoked once per stage)\n"
 	       "    gs_max (with geometry shader invoked max times per "
-	       "stage)\n"
-	       "  add “spirv” to the command line to use SPIR-V shaders "
-	       "instead of GLSL. Only vs* tests suport SPIR-V shaders.\n",
-	       prog_name);
+	       "stage)\n", prog_name);
 	piglit_report_result(PIGLIT_FAIL);
 }
 
-static GLuint
-compile_spirv_program(GLenum shader_type,
-		      const unsigned spirv_asm_size,
-		      const char *spirv_asm)
-{
-	GLuint shader, prog;
-
-	shader = piglit_assemble_spirv(shader_type,
-				       spirv_asm_size,
-				       spirv_asm);
-
-	glSpecializeShader(shader,
-			   "main",
-			   0, /* numSpecializationConstants */
-			   NULL /* pConstantIndex */,
-			   NULL /* pConstantValue */);
-
-	prog = glCreateProgram();
-	glAttachShader(prog, shader);
-	glDeleteShader(shader);
-
-	glLinkProgram(prog);
-
-	return prog;
-}
-
 static void
-build_and_use_program(const struct test_config *config)
+build_and_use_program(unsigned gs_invocation_n, const char *vs_text)
 {
 	GLuint prog;
 
-	if (config->gs_invocation_n == 0) {
-		char filepath[4096];
-		char *source;
-		unsigned source_size;
-
-		piglit_join_paths(filepath,
-				  sizeof(filepath),
-				  6, /* num parts */
-				  piglit_source_dir(),
-				  "tests",
-				  "spec",
-				  "arb_enhanced_layouts",
-				  "shader_source",
-				  config->shader_source_filename);
-
-		piglit_load_source_from_shader_test(filepath,
-						    GL_VERTEX_SHADER,
-						    config->spirv,
-						    &source,
-						    &source_size);
-
-		if (config->spirv) {
-			prog = compile_spirv_program(GL_VERTEX_SHADER,
-						     source_size,
-						     source);
-		} else {
-			prog = piglit_build_simple_program_multiple_shaders(
-				GL_VERTEX_SHADER, source, 0);
-		}
-
-		free(source);
+	if (gs_invocation_n == 0) {
+		prog = piglit_build_simple_program_multiple_shaders(
+				GL_VERTEX_SHADER, vs_text, 0);
 	} else {
-		if (config->spirv) {
-			fprintf(stderr, "SPIR-V not supported for this subtest\n");
-			piglit_report_result(PIGLIT_FAIL);
-		}
-
 		char *gs_text;
 
-		(void)!asprintf(&gs_text,
-				gs_text_two_sets_tmpl,
-				config->gs_invocation_n);
+		(void)!asprintf(&gs_text, gs_text_two_sets_tmpl, gs_invocation_n);
 		prog = piglit_build_simple_program_multiple_shaders(
 				GL_VERTEX_SHADER, vs_pass_thru_text,
 				GL_GEOMETRY_SHADER, gs_text, 0);
 		free(gs_text);
 	}
 
+	glLinkProgram(prog);
 	if (!piglit_link_check_status(prog))
 		piglit_report_result(PIGLIT_FAIL);
 	if (!piglit_check_gl_error(GL_NO_ERROR))
@@ -197,11 +212,17 @@ build_and_use_program(const struct test_config *config)
 	glUseProgram(prog);
 }
 
-static void
-probe_queries(const GLuint *queries,
-	      unsigned primitive_n)
+static bool
+probe_buffers(const GLuint *xfb, const GLuint *queries, unsigned primitive_n)
 {
+	bool pass;
+	unsigned i;
 	GLuint query_result;
+	float *first;
+	float *second;
+
+	const unsigned first_n = primitive_n * BUF_1_FLOAT_N;
+	const unsigned second_n = primitive_n * BUF_2_FLOAT_N;
 
 	glGetQueryObjectuiv(queries[0], GL_QUERY_RESULT, &query_result);
 	if (query_result != primitive_n) {
@@ -216,20 +237,6 @@ probe_queries(const GLuint *queries,
 			primitive_n, query_result);
 		piglit_report_result(PIGLIT_FAIL);
 	}
-}
-
-
-static bool
-probe_buffers_float(const GLuint *xfb,
-		    unsigned primitive_n)
-{
-	bool pass;
-	unsigned i;
-	float *first;
-	float *second;
-
-	const unsigned first_n = primitive_n * BUF_1_FLOAT_N;
-	const unsigned second_n = primitive_n * BUF_2_FLOAT_N;
 
 	first = malloc(first_n * sizeof(float));
 	second = malloc(second_n * sizeof(float));
@@ -261,174 +268,79 @@ probe_buffers_float(const GLuint *xfb,
 	return pass;
 }
 
-static bool
-probe_buffers_double(const GLuint *xfb,
-		     unsigned primitive_n)
+static unsigned
+parse_args(int argc, char **argv, const char **vs_text)
 {
-	bool pass;
-	unsigned i, j;
-	double *first;
+	GLint gs_invocation_n;
 
-	const unsigned first_n = primitive_n * BUF_DOUBLE_N;
-
-	first = malloc(first_n * sizeof(double));
-
-	for (i = 0; i < primitive_n; ++i) {
-		for (j = 0; j < BUF_DOUBLE_N; j++)
-			first[i * BUF_DOUBLE_N + j] = i + j + 1;
-	}
-
-	pass = piglit_probe_buffer_doubles(xfb[0], GL_TRANSFORM_FEEDBACK_BUFFER,
-					   "first", 1, first_n, first);
-
-	free(first);
-
-	return pass;
-}
-
-static void
-parse_args(int argc, char **argv, struct test_config *config)
-{
-	int i, j;
-	bool option_was_handled = false;
-	static const struct {
-		const char *name;
-		const char *shader_source_filename;
-		unsigned gs_invocation_n;
-		bool doubles;
-	} test_types[] = {
-		{
-			"vs",
-			"vs_two_sets.shader_source",
-			0,
-			false
-		},
-		{
-			"vs_ifc",
-			"vs_two_sets_ifc.shader_source",
-			0,
-			false
-		},
-		{
-			"vs_named_ifc",
-			"vs_two_sets_named_ifc.shader_source",
-			0,
-			false
-		},
-		{
-			"vs_struct",
-			"vs_two_sets_struct.shader_source",
-			0,
-			false
-		},
-		{
-			"vs_double",
-			"vs_double.shader_source",
-			0,
-			true,
-		},
-		{
-			"gs",
-			NULL,
-			1,
-			false
-		},
-		{
-			"gs_max",
-			NULL,
-			INT_MAX,
-			false
-		}
-	};
-
-	memset(config, 0, sizeof *config);
-
-	for (i = 1; i < argc; i++) {
-		for (j = 0; j < ARRAY_SIZE(test_types); j++) {
-			if (!strcmp(argv[i], test_types[j].name)) {
-				config->shader_source_filename =
-					test_types[j].shader_source_filename,
-				config->gs_invocation_n =
-					test_types[j].gs_invocation_n;
-				config->doubles =
-					test_types[j].doubles;
-				option_was_handled = true;
-				goto option_handled;
-			}
-		}
-
-		if (!strcmp(argv[i], "spirv")) {
-			config->spirv = true;
-			printf("Running on SPIR-V mode\n");
-		}
-
-	option_handled:
-		continue;
-	}
-
-	if (!option_was_handled)
+	if (argc != 2)
 		print_usage_and_exit(argv[0]);
+
+	if (strcmp(argv[1], "vs") == 0) {
+		*vs_text = vs_two_sets_text;
+		return 0;
+	}
+
+	if (strcmp(argv[1], "vs_ifc") == 0) {
+		*vs_text = vs_two_sets_ifc_text;
+		return 0;
+	}
+
+	if (strcmp(argv[1], "vs_named_ifc") == 0) {
+		*vs_text = vs_two_sets_named_ifc_text;
+		return 0;
+	}
+
+	if (strcmp(argv[1], "vs_struct") == 0) {
+		*vs_text = vs_two_sets_struct_text;
+		return 0;
+	}
+
+	piglit_require_extension("GL_ARB_gpu_shader5");
+
+	if (strcmp(argv[1], "gs") == 0)
+		return 1;
+
+	if (strcmp(argv[1], "gs_max") != 0)
+		print_usage_and_exit(argv[0]);
+
+	glGetIntegerv(GL_MAX_GEOMETRY_SHADER_INVOCATIONS, &gs_invocation_n);
+	if (gs_invocation_n <= 0) {
+		printf("Maximum amount of geometry shader invocations "
+		       "needs to be positive (%u).\n", gs_invocation_n);
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	return gs_invocation_n;
 }
 
 void
 piglit_init(int argc, char **argv)
 {
 	bool pass;
-	unsigned primitive_n;
+	unsigned primitive_n, gs_invocation_n;
+	const char *vs_text;
 	GLuint queries[2];
 	GLuint xfb[2];
 	GLuint vao;
-	struct test_config config;
 
 	piglit_require_GLSL_version(150);
 	piglit_require_extension("GL_ARB_transform_feedback3");
 	piglit_require_extension("GL_ARB_enhanced_layouts");
 
-	parse_args(argc, argv, &config);
-
-	if (config.gs_invocation_n > 0) {
-		piglit_require_extension("GL_ARB_gpu_shader5");
-
-		if (config.gs_invocation_n == INT_MAX) {
-			glGetIntegerv(GL_MAX_GEOMETRY_SHADER_INVOCATIONS,
-				      &config.gs_invocation_n);
-			if (config.gs_invocation_n <= 0) {
-				printf("Maximum amount of geometry shader "
-				       "invocations needs to be positive "
-				       "(%u).\n",
-				       config.gs_invocation_n);
-				piglit_report_result(PIGLIT_FAIL);
-			}
-		}
-	}
-
-	if (config.spirv) {
-		piglit_require_extension("GL_ARB_gl_spirv");
-
-		if (config.gs_invocation_n > 0) {
-			printf("Geometry shader invocations is not supported "
-			       "with SPIR-V\n");
-			piglit_report_result(PIGLIT_FAIL);
-		}
-	}
-
-	if (config.doubles)
-	   piglit_require_GLSL_version(450);
+	gs_invocation_n = parse_args(argc, argv, &vs_text);
 
 	/* Zero invocations means the feedback is produced by vertex shader */
-	primitive_n = config.gs_invocation_n ? config.gs_invocation_n : 1;
+	primitive_n = gs_invocation_n ? gs_invocation_n : 1;
 
-	build_and_use_program(&config);
+	build_and_use_program(gs_invocation_n, vs_text);
 
 	/* Set up the transform feedback buffers. */
 	glGenBuffers(ARRAY_SIZE(xfb), xfb);
 	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, xfb[0]);
 	glBufferData(GL_TRANSFORM_FEEDBACK_BUFFER,
-		     primitive_n *
-		     MAX2(BUF_1_FLOAT_N * sizeof(float),
-			  BUF_DOUBLE_N * sizeof(double)),
-		     NULL,
-		     GL_STREAM_READ);
+		primitive_n * BUF_1_FLOAT_N * sizeof(float), NULL,
+		GL_STREAM_READ);
 	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 2, xfb[1]);
 	glBufferData(GL_TRANSFORM_FEEDBACK_BUFFER,
 		primitive_n * BUF_2_FLOAT_N * sizeof(float), NULL,
@@ -459,11 +371,7 @@ piglit_init(int argc, char **argv)
 	if (!piglit_check_gl_error(GL_NO_ERROR))
 		piglit_report_result(PIGLIT_FAIL);
 
-	probe_queries(queries, primitive_n);
-	if (config.doubles)
-		pass = probe_buffers_double(xfb, primitive_n);
-	else
-		pass = probe_buffers_float(xfb, primitive_n);
+	pass = probe_buffers(xfb, queries, primitive_n);
 
 	glDeleteBuffers(2, xfb);
 	glDeleteQueries(2, queries);
