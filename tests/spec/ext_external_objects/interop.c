0737702cfb3c919471270e58962ab55d9b2f6cfc
Eleni Maria Stea <estea@igalia.com>
2020-04-13 20:30:28 +0300

    ext_external_objects: Helper structs that map the Vk and GL formats
    Added a struct that maps the Vulkan and OpenGl formats and stores
    other properties about them. Even if we can query that information
    it is useful to have our mapping with thee expected format to compare
    it.
    
    v2:
    * Fixed indentation in enum fragment_type
    
    Co-authored-by: Juan A. Suarez Romero <jasuarez@igalia.com>
    Co-authored-by: Eleni Maria Stea <estea@igalia.com>
    
    Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

--- tests/spec/ext_external_objects/interop.c
+++ tests/spec/ext_external_objects/interop.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright © 2020 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Author:
+ *    Eleni Maria Stea <estea@igalia.com>
+ *    Juan A. Suarez Romero <jasuarez@igalia.com>
+ */
+
 #include <piglit-util-gl.h>
 #include <sized-internalformats.h>
 #include "interop.h"
@@ -56,7 +83,9 @@ gl_gen_tex_from_mem_obj(const struct vk_image_props *props,
 			GLuint mem_obj, uint32_t offset,
 			GLuint *tex)
 {
+	GLint filter;
 	GLuint target = gl_get_target(props);
+	const struct sized_internalformat *format = get_sized_internalformat(tex_storage_format);
 
 	glGenTextures(1, tex);
 	glBindTexture(target, *tex);
@@ -87,8 +116,18 @@ gl_gen_tex_from_mem_obj(const struct vk_image_props *props,
 		return false;
 	}
 
-	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	switch (get_channel_type(format, 1)) {
+	case GL_INT:
+	case GL_UNSIGNED_INT:
+		filter = GL_NEAREST;
+		break;
+	default:
+		filter = GL_LINEAR;
+		break;
+	}
+
+	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, filter);
+	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, filter);
 
 	return glGetError() == GL_NO_ERROR;
 }
--- tests/spec/ext_external_objects/interop.h
+++ tests/spec/ext_external_objects/interop.h
@@ -1,3 +1,30 @@
+/*
+ * Copyright © 2020 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Author:
+ *    Eleni Maria Stea <estea@igalia.com>
+ *    Juan A. Suarez Romero <jasuarez@igalia.com>
+ */
+
 #ifndef INTEROPERABILITY_H
 #define INTEROPERABILITY_H
 
@@ -7,6 +34,27 @@
 struct gl_ext_semaphores {
 	GLuint vk_frame_done;
 	GLuint gl_frame_ready;
+};
+
+enum fragment_type {
+	FLOAT_FS = 0,
+	INT_FS,
+	UINT_FS,
+};
+
+struct format_mapping {
+	char *name;
+	GLenum glformat;
+	VkFormat vkformat;
+	enum fragment_type fs_type;
+
+	uint32_t rbits;
+	uint32_t gbits;
+	uint32_t bbits;
+	uint32_t abits;
+
+	VkImageTiling tiling;
+	VkImageUsageFlagBits usage;
 };
 
 GLuint
