579e4b9d03ef4111b9dbea7ed9684eee24cd2d25
Serge Martin <edb@sigluy.net>
2020-05-12 22:38:24 +0200

    cl: clGetKernelInfo check returned values
    also add CL_KERNEL_ATTRIBUTES support
    
    Part-of: <https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/271>

--- tests/cl/api/get-kernel-info.c
+++ tests/cl/api/get-kernel-info.c
@@ -44,10 +44,90 @@ PIGLIT_CL_API_TEST_CONFIG_BEGIN
 	config.run_per_platform = true;
 	config.create_context = true;
 
-	config.program_source =  "kernel void dummy_kernel() {}";
+	config.program_source =  "kernel __attribute__((work_group_size_hint(1, 1, 1))) void dummy_kernel(int i) {}\n";
 
 PIGLIT_CL_API_TEST_CONFIG_END
 
+
+static bool
+check_size(size_t expected_size, size_t actual_size, enum piglit_result *result) {
+	if (expected_size != actual_size) {
+		printf(": failed, expected and actual size differ. Expect %lu, got %lu",
+		       expected_size, actual_size);
+		piglit_merge_result(result, PIGLIT_FAIL);
+		return false;
+	}
+
+	return true;
+}
+
+static bool
+check_string(char* expected, char* actual_value, enum piglit_result *result) {
+	if (strcmp(expected, actual_value)) {
+		printf(": failed, expected and actual string differ. Expect '%s', got '%s'",
+		       expected, actual_value);
+		piglit_merge_result(result, PIGLIT_FAIL);
+		return false;
+	}
+
+	return true;
+}
+
+static void
+check_info(const struct piglit_cl_api_test_env* env,
+	   cl_kernel_info kind, void* param_value, size_t param_value_size,
+	   enum piglit_result *result) {
+	switch (kind) {
+		case CL_KERNEL_FUNCTION_NAME:
+			if (check_size(strlen("dummy_kernel") + 1, param_value_size, result)) {
+				check_string("dummy_kernel", param_value, result);
+			}
+			break;
+		case CL_KERNEL_NUM_ARGS:
+			if (check_size(sizeof(cl_uint), param_value_size, result)) {
+				if (*(cl_uint*)param_value != 1) {
+					printf(": failed, expected and actual value differ. Expect '%d', got '%d'",
+					1, *(cl_uint*)param_value);
+					piglit_merge_result(result, PIGLIT_FAIL);
+				}
+			}
+			break;
+		case CL_KERNEL_REFERENCE_COUNT:
+			if (check_size(sizeof(cl_uint), param_value_size, result)) {
+				if (*(cl_uint*)param_value < 1) {
+					printf(": failed, expected and actual value differ. Expect >='%d', got '%d'",
+					1, *(cl_uint*)param_value);
+					piglit_merge_result(result, PIGLIT_FAIL);
+				}
+			}
+			break;
+		case CL_KERNEL_CONTEXT:
+			if (check_size(sizeof(cl_context), param_value_size, result)) {
+				if (*(cl_context*)param_value != env->context->cl_ctx) {
+					printf(": failed, expected and actual context differ");
+					piglit_merge_result(result, PIGLIT_FAIL);
+				}
+			}
+			break;
+		case CL_KERNEL_PROGRAM:
+			if (check_size(sizeof(cl_program), param_value_size, result)) {
+				if (*(cl_program*)param_value != env->program) {
+					printf(": failed, expected and actual program differ");
+					piglit_merge_result(result, PIGLIT_FAIL);
+				}
+			}
+			break;
+		case CL_KERNEL_ATTRIBUTES:
+			if (check_size(strlen("work_group_size_hint(1,1,1)") + 1, param_value_size, result)) {
+				check_string("work_group_size_hint(1,1,1)", param_value, result);
+			}
+			break;
+
+		default:
+			printf(": WARN unchecked value");
+			piglit_merge_result(result, PIGLIT_WARN);
+	}
+}
 
 enum piglit_result
 piglit_cl_test(const int argc,
@@ -63,10 +143,10 @@ piglit_cl_test(const int argc,
 
 	size_t param_value_size;
 	void* param_value;
-	
+
 	int num_kernel_infos = PIGLIT_CL_ENUM_NUM(cl_kernel_info, env->version);
 	const cl_kernel_info* kernel_infos = PIGLIT_CL_ENUM_ARRAY(cl_kernel_info);
-	
+
 	kernel = clCreateKernel(env->program,
 	                        "dummy_kernel",
 	                        &errNo);
@@ -79,7 +159,7 @@ piglit_cl_test(const int argc,
 
 	/*** Normal usage ***/
 	for(i = 0; i < num_kernel_infos; i++) {
-		printf("%s ", piglit_cl_get_enum_name(kernel_infos[i]));
+		printf("%s", piglit_cl_get_enum_name(kernel_infos[i]));
 
 		errNo = clGetKernelInfo(kernel,
 		                        kernel_infos[i],
@@ -88,7 +168,7 @@ piglit_cl_test(const int argc,
 		                        &param_value_size);
 		if(!piglit_cl_check_error(errNo, CL_SUCCESS)) {
 			fprintf(stderr,
-			        "Failed (error code: %s): Get size of %s.\n",
+			        "failed (error code: %s): Get size of %s.\n",
 			        piglit_cl_get_error_name(errNo),
 			        piglit_cl_get_enum_name(kernel_infos[i]));
 			piglit_merge_result(&result, PIGLIT_FAIL);
@@ -103,13 +183,14 @@ piglit_cl_test(const int argc,
 		                        NULL);
 		if(!piglit_cl_check_error(errNo, CL_SUCCESS)) {
 			fprintf(stderr,
-			        "Failed (error code: %s): Get value of %s.\n",
+			        "failed (error code: %s): Get value of %s.\n",
 			        piglit_cl_get_error_name(errNo),
 			        piglit_cl_get_enum_name(kernel_infos[i]));
 			piglit_merge_result(&result, PIGLIT_FAIL);
 		}
 
-		//TODO: output returned values
+		check_info(env, kernel_infos[i], param_value, param_value_size, &result);
+
 		printf("\n");
 		free(param_value);
 	}
