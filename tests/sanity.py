2195deac0d42e8f57ee7e2558e5aeadf76bf9595
Timothy Arceri <tarceri@itsqueeze.com>
2020-06-23 10:54:48 +1000

    glsl-1.10: move algebraic tests to glsl-1.10 dir
    Part-of: <https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/323>

--- tests/sanity.py
+++ tests/sanity.py
@@ -73,7 +73,7 @@ profile = TestProfile()
 shader_tests = (
     'fast_color_clear/all-colors.shader_test',
     'shaders/arb_texture_gather-miplevels.shader_test',
-    'shaders/glsl-algebraic-add-zero.shader_test',
+    'spec/glsl-1.10/execution/algebraic/glsl-algebraic-add-zero.shader_test',
     'shaders/glsl-derivs-varyings.shader_test',
     'shaders/glsl-fs-max-3.shader_test',
     'shaders/glsl-fs-texture2d-branching.shader_test',
