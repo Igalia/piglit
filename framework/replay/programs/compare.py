faed7620ec69a63af78a5c7cb097b80ba0fe8e85
Andres Gomez <agomez@igalia.com>
2020-07-29 23:24:04 +0300

    framework/replay: add programs module
    v2:
      - Updated the README.md
      - Improved the documentation of the calls parameter
        for the dump command.
      - Added missing parameter in the dump program inner call.
    
    v3:
      - Make it compatible with python 3.6.
    
    v4:
      - Directly return the inner function return value in the compare
        program.
    
    v5:
      - Remove extra empty line (Dylan).
    
    Signed-off-by: Andres Gomez <agomez@igalia.com>
    Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/353>

---
+++ framework/replay/programs/compare.py
@@ -0,0 +1,108 @@
+# coding=utf-8
+#
+# Copyright © 2020 Valve Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
+
+import argparse
+
+from framework import exceptions
+from framework.replay import compare_replay
+from framework.replay import options
+from framework.programs import parsers as piglit_parsers
+from . import parsers
+
+
+__all__ = ['compare']
+
+
+def _from_yaml(args):
+    options.OPTIONS.device_name = args.device_name
+    options.OPTIONS.keep_image = args.keep_image
+    options.OPTIONS.download['force'] = args.force_download
+    options.OPTIONS.db_path = args.db_path
+    options.OPTIONS.results_path = args.output
+
+    return compare_replay.from_yaml(args.yaml_file)
+
+
+def _trace(args):
+    options.OPTIONS.device_name = args.device_name
+    options.OPTIONS.keep_image = args.keep_image
+    options.OPTIONS.set_download_url(args.download_url)
+    options.OPTIONS.download['force'] = args.force_download
+    options.OPTIONS.db_path = args.db_path
+    options.OPTIONS.results_path = args.output
+
+    return compare_replay.trace(args.file_path, args.expected_checksum)
+
+
+@exceptions.handler
+def compare(input_):
+    """ Parser for replayer compare command """
+    unparsed = piglit_parsers.parse_config(input_)[1]
+
+    try:
+        # Set the parent of the config to add the -f/--config message
+        parser = argparse.ArgumentParser(parents=[piglit_parsers.CONFIG])
+        # The "required" keyword is only available since python >= 3.7
+        subparsers = parser.add_subparsers(dest='command', required=True)
+    except TypeError:
+        parser = argparse.ArgumentParser(parents=[piglit_parsers.CONFIG])
+        # Add a destination due to
+        # https://github.com/python/cpython/pull/3027#issuecomment-330910633
+        subparsers = parser.add_subparsers(dest='command')
+
+    parser_trace = subparsers.add_parser(
+        'trace',
+        parents=[parsers.DEVICE,
+                 parsers.KEEP_IMAGE,
+                 parsers.DOWNLOAD_URL,
+                 parsers.DOWNLOAD_FORCE,
+                 parsers.DB_PATH,
+                 parsers.RESULTS_PATH],
+        help=('Compares a specific trace given a checksum and a device.'))
+    parser_trace.add_argument(
+        'file_path',
+        help=('the relative path to the trace file inside the db path. '
+              'If not present and given that an URL has been provided '
+              'for its download, the relative path to the file in such URL.'))
+    parser_trace.add_argument(
+        'expected_checksum',
+        help=('the expected checksum value to obtain '
+              'when replaying a trace in a specific device'))
+    parser_trace.set_defaults(func=_trace)
+
+    parser_yaml = subparsers.add_parser(
+        'yaml',
+        parents=[parsers.DEVICE,
+                 parsers.KEEP_IMAGE,
+                 parsers.YAML,
+                 parsers.DOWNLOAD_FORCE,
+                 parsers.DB_PATH,
+                 parsers.RESULTS_PATH],
+        help=('Compares from a traces description file listing traces '
+              'and their checksums for a given device.'))
+    parser_yaml.set_defaults(func=_from_yaml)
+
+    args = parser.parse_args(unparsed)
+
+    return args.func(args)
---
+++ framework/replay/programs/query.py
@@ -0,0 +1,119 @@
+# coding=utf-8
+#
+# Copyright (c) 2019 Collabora Ltd
+# Copyright © 2020 Valve Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
+
+import argparse
+
+from framework import exceptions
+from framework.replay import query_traces_yaml as qty
+from . import parsers
+
+
+__all__ = ['query']
+
+
+def _traces_db_download_url(args):
+    y = qty.load_yaml(args.yaml_file)
+
+    url = qty.download_url(y) or ""
+
+    print(url)
+
+
+def _traces(args):
+    y = qty.load_yaml(args.yaml_file)
+
+    t_list = qty.traces(y,
+                        trace_types=args.trace_types,
+                        device_name=args.device_name,
+                        checksum=args.checksum)
+
+    if args.checksum:
+        print('\n'.join(((t['path'] + '\n' + t['checksum'])
+                         for t in t_list)))
+    else:
+        print('\n'.join((t['path'] for t in t_list)))
+
+
+def _checksum(args):
+    y = qty.load_yaml(args.yaml_file)
+
+    traces = y['traces']
+    try:
+        trace = next(t for t in traces if t['path'] == args.file_path)
+    except StopIteration:
+        print("")
+        return
+
+    print(qty.trace_checksum(trace, args.device_name))
+
+
+@exceptions.handler
+def query(input_):
+    """ Parser for replayer query command """
+    try:
+        parser = argparse.ArgumentParser(parents=[parsers.YAML])
+        # The "required" keyword is only available since python >= 3.7
+        subparsers = parser.add_subparsers(dest='command', required=True)
+    except TypeError:
+        parser = argparse.ArgumentParser(parents=[parsers.YAML])
+        # Add a destination due to
+        # https://github.com/python/cpython/pull/3027#issuecomment-330910633
+        subparsers = parser.add_subparsers(dest='command')
+
+    parser_checksum = subparsers.add_parser(
+        'checksum',
+        parents=[parsers.DEVICE],
+        help=('Outputs, if available, the checksum that a specific device '
+              'should produce for a specified trace file.'))
+    parser_checksum.add_argument(
+        'file_path',
+        help=('the path to a trace file.'))
+    parser_checksum.set_defaults(func=_checksum)
+
+    parser_traces = subparsers.add_parser(
+        'traces',
+        parents=[parsers.DEVICE],
+        help=('Outputs the trace files filtered by the optional arguments.'))
+    parser_traces.add_argument(
+        '-t', '--trace-types',
+        required=False,
+        default=None,
+        help=('a comma separated list of trace types to look for '
+              'in recursive dir walks. '
+              'If none are provide, all types are used by default'))
+    parser_traces.add_argument(
+        '-c', '--checksum',
+        action='store_true',
+        help='whether to print the checksum below every trace')
+    parser_traces.set_defaults(func=_traces)
+
+    parser_traces_db_download_url = subparsers.add_parser(
+        'traces_db_download_url',
+        help=('Outputs, if available, the download URL.'))
+    parser_traces_db_download_url.set_defaults(func=_traces_db_download_url)
+
+    args = parser.parse_args(input_)
+
+    args.func(args)
