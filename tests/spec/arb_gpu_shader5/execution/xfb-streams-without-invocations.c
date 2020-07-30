bba9aa1543b25453f3c8be8bbe7df5a8b70591ce
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-07-30 14:06:44 -0400

    arb_gpu_shader5: fix gl version requirement for xfb-streams-without-invocations
    this uses a spirv shader which explicitly requires 4.50
    
    Reviewed-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/354>

--- tests/spec/arb_gpu_shader5/execution/xfb-streams-without-invocations.c
+++ tests/spec/arb_gpu_shader5/execution/xfb-streams-without-invocations.c
@@ -36,8 +36,8 @@
 
 PIGLIT_GL_TEST_CONFIG_BEGIN
 
-	config.supports_gl_compat_version = 32;
-	config.supports_gl_core_version = 32;
+	config.supports_gl_compat_version = 45;
+	config.supports_gl_core_version = 45;
 	config.khr_no_error_support = PIGLIT_NO_ERRORS;
 
 PIGLIT_GL_TEST_CONFIG_END
