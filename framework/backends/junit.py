Igalia/piglit

commit 733dddbc35adbd9cbd46d8bfed2a17a3525eca3d

framework/backends/junit.py: restore time from stderr
This allows time to be fully restored, but will fallback to the more
basic time element for restoration if this isn't parsable.

Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
Reviewed-by: Mark Janes <mark.a.janes@intel.com>

framework/backends/junit.py
@@ -255,6 +255,9 @@ def _load(results_file):
             name = name[:-1]
 
         result.result = test.attrib['status']
+
+        # This is the fallback path, we'll try to overwrite this with the value
+        # in stderr
         result.time = results.TimeAttribute(end=float(test.attrib['time']))
         result.err = test.find('system-err').text
 
@@ -263,6 +266,15 @@ def _load(results_file):
         out = test.find('system-out').text.split('\n')
         result.command = out[0]
         result.out = '\n'.join(out[1:])
+
+        # Try to get the values in stderr for time
+        if 'time start' in result.err:
+            for line in result.err.split('\n'):
+                if line.startswith('time start:'):
+                    result.time.start = float(line[len('time start: '):])
+                elif line.startswith('time end:'):
+                    result.time.end = float(line[len('time end: '):])
+                    break
 
         run_result.tests[name] = result
 

framework/tests/junit_backends_tests.py
@@ -47,7 +47,11 @@ _XML = """\
     <testsuite name="piglit" tests="1">
       <testcase classname="piglit.foo.bar" name="a-test" status="pass" time="1.12345">
         <system-out>this/is/a/command\nThis is stdout</system-out>
-        <system-err>this is stderr</system-err>
+        <system-err>this is stderr
+
+time start: 1.0
+time end: 4.5
+        </system-err>
       </testcase>
     </testsuite>
   </testsuites>
@@ -234,11 +238,17 @@ class TestJUnitLoad(utils.StaticDirectory):
         nt.assert_is_instance(self.xml().tests[self.testname].result,
                               status.Status)
 
-    def test_time(self):
-        """backends.junit._load: Time is loaded correctly."""
+    def test_time_start(self):
+        """backends.junit._load: Time.start is loaded correctly."""
         time = self.xml().tests[self.testname].time
         nt.assert_is_instance(time, results.TimeAttribute)
-        nt.assert_equal(time.total, 1.12345)
+        nt.eq_(time.start, 1.0)
+
+    def test_time_end(self):
+        """backends.junit._load: Time.end is loaded correctly."""
+        time = self.xml().tests[self.testname].time
+        nt.assert_is_instance(time, results.TimeAttribute)
+        nt.eq_(time.end, 4.5)
 
     def test_command(self):
         """backends.junit._load: command is loaded correctly."""
@@ -253,7 +263,9 @@ class TestJUnitLoad(utils.StaticDirectory):
     def test_err(self):
         """backends.junit._load: stderr is loaded correctly."""
         test = self.xml().tests[self.testname].err
-        nt.assert_equal(test, 'this is stderr')
+        nt.eq_(
+            test, 'this is stderr\n\ntime start: 1.0\ntime end: 4.5\n        ')
+
 
     @utils.no_error
     def test_load_file(self):

commit 5fafccc4c5cbb35c555756b0f2c6b640b4fb7c09

framework: fix loading time start/end in junit test results
v2:
  - Added fixes line.

Fixes: 733dddbc3 ("framework/backends/junit.py: restore time from stderr")
Signed-off-by: Andres Gomez <agomez@igalia.com>
Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
Part-of: <https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/423>

framework/backends/junit.py
@@ -45,6 +45,9 @@ __all__ = [
 
 _JUNIT_SPECIAL_NAMES = ('api', 'search')
 
+_PID_STR = "pid: "
+_START_TIME_STR = "start time: "
+_END_TIME_STR = "end time: "
 
 def junit_escape(name):
     name = name.replace('.', '_')
@@ -85,8 +88,10 @@ class JUnitWriter(object):
         """Adds the 'system-err' element."""
         err = etree.SubElement(element, 'system-err')
         err.text = data.err
-        err.text += '\n\npid: {}\nstart time: {}\nend time: {}\n'.format(
-            data.pid, data.time.start, data.time.end)
+        err.text += '\n\n{}{}\n{}{}\n{}{}\n'.format(
+            _PID_STR, data.pid,
+            _START_TIME_STR, data.time.start,
+            _END_TIME_STR, data.time.end)
 
         if data.result in ['fail', 'dmesg-warn', 'dmesg-fail']:
             if expected_result == "failure":
@@ -410,14 +415,14 @@ def _load(results_file):
 
         # Try to get the values in stderr for time and pid
         for line in result.err.split('\n'):
-            if line.startswith('time start:'):
-                result.time.start = float(line[len('time start: '):])
+            if line.startswith(_START_TIME_STR):
+                result.time.start = float(line[len(_START_TIME_STR):])
                 continue
-            elif line.startswith('time end:'):
-                result.time.end = float(line[len('time end: '):])
+            elif line.startswith(_END_TIME_STR):
+                result.time.end = float(line[len(_END_TIME_STR):])
                 continue
-            elif line.startswith('pid:'):
-                result.pid = json.loads(line[len('pid: '):])
+            elif line.startswith(_PID_STR):
+                result.pid = json.loads(line[len(_PID_STR):])
                 continue
 
 

unittests/framework/backends/test_junit.py
@@ -54,8 +54,8 @@ _XML = """\
         <system-err>this is stderr
 
 pid: [1934]
-time start: 1.0
-time end: 4.5
+start time: 1.0
+end time: 4.5
         </system-err>
       </testcase>
     </testsuite>
@@ -150,8 +150,8 @@ class TestProtectedLoad(object):
                 this is stderr
 
                 pid: [1934]
-                time start: 1.0
-                time end: 4.5""")
+                start time: 1.0
+                end time: 4.5""")
             assert result.tests[self.testname].err.strip() == expected
 
         def test_totals(self, result):